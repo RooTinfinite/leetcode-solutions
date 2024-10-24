6b1f0b6d07ee0c1f83b915b25415ef950dc6efbc
Piotr M <filo74b@gmail.com>
2024-10-24 21:48:03 +0200

    Time: 39 ms (56.51%) | Memory: 55.7 MB (74.27%) - LeetSync

---
+++ solutions/1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.ts
@@ -0,0 +1,20 @@
+function maxUniqueSplit(s: string): number {
+  let res = 1
+  let strings = new Set()
+  function search(s: string) {
+    if (!strings.has(s)) {
+      strings.add(s)
+      res = Math.max(res, strings.size)
+      strings.delete(s)
+    }
+    for (let i = 1; i < s.length; i++) {
+      const sub = s.substring(0, i)
+      if (strings.has(sub)) continue
+      strings.add(sub)
+      search(s.substring(i))
+      strings.delete(sub)
+    }
+  }
+  search(s)
+  return res
+};
