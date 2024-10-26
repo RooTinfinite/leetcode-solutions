c713fd6bd443ac55176283b645e73bc8f9efd735
Piotr M <filo74b@gmail.com>
2024-10-26 03:58:23 +0200

    Time: 159 ms (100.00%) | Memory: 74.2 MB (22.05%) - LeetSync

--- solutions/2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.py
+++ solutions/2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.py
@@ -1,53 +1,45 @@
 class Solution:
     def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
-        # Initialize arrays to store heights and node information
-        heights = [0] * 50000  # Heights of leaf nodes
-        d = [0] * 100001      # Depth of each node
-        l = [0] * 100001      # Left index for each node
-        r = [0] * 100001      # Right index for each node
-        len_leaves = 0        # Counter for leaf nodes
+        heights = [0] * 50000
+        d = [0] * 100001
+        l = [0] * 100001
+        r = [0] * 100001
+        len_leaves = 0
         
         def search(p: TreeNode, h: int) -> None:
             nonlocal len_leaves
-            d[p.val] = h  # Store current node's depth
+            d[p.val] = h
             
-            # If leaf node found
             if not p.left and not p.right:
-                heights[len_leaves] = h    # Store leaf height
-                l[p.val] = r[p.val] = len_leaves  # Both indices same for leaf
+                heights[len_leaves] = h
+                l[p.val] = r[p.val] = len_leaves
                 len_leaves += 1
                 return
             
-            l[p.val] = len_leaves  # Store left index for current node
+            l[p.val] = len_leaves
             
-            # Recursively process left and right subtrees
             if p.left:
                 search(p.left, h + 1)
             if p.right:
                 search(p.right, h + 1)
                 
-            r[p.val] = len_leaves - 1  # Store right index for current node
+            r[p.val] = len_leaves - 1
         
-        # Process the tree starting from root
         search(root, 0)
         
-        n = len_leaves  # Total number of leaf nodes
-        maxl = [0] * n  # Max heights from left
-        maxr = [0] * n  # Max heights from right
+        n = len_leaves
+        maxl = [0] * n
+        maxr = [0] * n
         
-        # Build prefix and suffix maximum arrays
         for i in range(1, n):
-            maxl[i] = max(maxl[i-1], heights[i-1])  # Max height from left
-            maxr[n-i-1] = max(maxr[n-i], heights[n-i])  # Max height from right
+            maxl[i] = max(maxl[i-1], heights[i-1])
+            maxr[n-i-1] = max(maxr[n-i], heights[n-i])
         
-        ret = []  # Result list
+        ret = []
         
-        # Process each query
         for query in queries:
-            # Find maximum height excluding current node's subtree
-            maxxl = maxl[l[query]]  # Max height to the left
-            maxxr = maxr[r[query]]  # Max height to the right
-            # Result is max of (max left height, max right height, current depth-1)
+            maxxl = maxl[l[query]]
+            maxxr = maxr[r[query]]
             ret.append(max(max(maxxl, maxxr), d[query]-1))
         
         return ret
