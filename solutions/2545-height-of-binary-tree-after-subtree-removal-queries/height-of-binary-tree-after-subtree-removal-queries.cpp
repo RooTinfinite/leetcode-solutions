810e8bdbee38ed42121add02bdbeb001589b6af5
Piotr M <filo74b@gmail.com>
2024-10-26 02:03:08 +0200

    Time: 40 ms (95.26%) | Memory: 221.7 MB (70.93%) - LeetSync

--- solutions/2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.cpp
+++ solutions/2545-height-of-binary-tree-after-subtree-removal-queries/height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -1,53 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    // Array to store the maximum height of the tree after removing each node
-    int maxHeightAfterRemoval[100001];
-    int currentMaxHeight = 0;
-
-    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
-        traverseLeftToRight(root, 0);
-        currentMaxHeight = 0;  // Reset for the second traversal
-        traverseRightToLeft(root, 0);
-
-        // Process queries and build the result vector
-        int queryCount = queries.size();
-        vector<int> queryResults(queryCount);
-        for (int i = 0; i < queryCount; i++) {
-            queryResults[i] = maxHeightAfterRemoval[queries[i]];
+    vector<int>dfsTree;
+    int d[300005];
+    int st[300005];
+    int en[300005];
+    void dfs(TreeNode *root, int dep) {
+        if(root == NULL) {
+            return;
         }
 
-        return queryResults;
+        d[root->val] = dep;
+        st[root->val] = (int)dfsTree.size();
+        dfsTree.push_back(root->val);
+        dfs(root->left,dep+1);
+        dfs(root->right,dep+1);
+        en[root->val] = (int)dfsTree.size();
+        dfsTree.push_back(root->val);
+
     }
-
-private:
-    // Left to right traversal
-    void traverseLeftToRight(TreeNode* node, int currentHeight) {
-        if (node == nullptr) return;
-
-        // Store the maximum height if this node were removed
-        maxHeightAfterRemoval[node->val] = currentMaxHeight;
-
-        // Update the current maximum height
-        currentMaxHeight = max(currentMaxHeight, currentHeight);
-
-        // Traverse left subtree first, then right
-        traverseLeftToRight(node->left, currentHeight + 1);
-        traverseLeftToRight(node->right, currentHeight + 1);
-    }
-
-    // Right to left traversal
-    void traverseRightToLeft(TreeNode* node, int currentHeight) {
-        if (node == nullptr) return;
-
-        // Update the maximum height if this node were removed
-        maxHeightAfterRemoval[node->val] =
-            max(maxHeightAfterRemoval[node->val], currentMaxHeight);
-
-        // Update the current maximum height
-        currentMaxHeight = max(currentHeight, currentMaxHeight);
-
-        // Traverse right subtree first, then left
-        traverseRightToLeft(node->right, currentHeight + 1);
-        traverseRightToLeft(node->left, currentHeight + 1);
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        vector<int>pref;
+        vector<int>suf;
+        dfs(root,0);
+        int n = (int)dfsTree.size();
+        for(int i = 0 ; i < n ; i++) {
+            pref.push_back(d[dfsTree[i]]);
+            suf.push_back(pref.back());
+        }
+        for(int i = 1 ; i < n ; i++) {
+            pref[i] = max(pref[i],pref[i-1]);
+        }
+        for(int i = n-2 ; i >= 0 ; i--) {
+            suf[i] = max(suf[i],suf[i+1]);
+        }
+        vector<int>ans;
+        for(auto &x : queries) {
+            int ST = st[x];
+            int EN = en[x];
+            int res = 0;
+            if(ST-1 >= 0) {
+                res = max(res,pref[ST-1]);
+            }
+            if(EN+1 < n) {
+                res = max(res,suf[EN+1]);
+            }
+            ans.push_back(res);
+        }
+        return ans;
     }
 };
