d8dec9711cea803d981c558dcc7186069e86c31a
Piotr M <filo74b@gmail.com>
2024-10-29 03:09:40 +0100

    Time: 0 ms (100.00%) | Memory: 8.5 MB (75.73%) - LeetSync

--- solutions/9-palindrome-number/palindrome-number.cpp
+++ solutions/9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+    bool isPalindrome(int x) 
+    {
+        if(x < 0) return false;
+        long long int temp = x, answer = 0;
+        while(x)
+        {
+            int digit = x % 10;
+            answer = (answer * 10) + digit;
+            x = x / 10;
         }
-
-        long long reversed = 0;
-        long long temp = x;
-
-        while (temp != 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
-        }
-
-        return (reversed == x);
+        
+        if(temp == answer) return true;
+        return false;
     }
 };
