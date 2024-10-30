443938a901a31a8ab78f06c7ff20c0dacc108dae
Piotr M <filo74b@gmail.com>
2024-10-30 02:35:02 +0100

    Time: 17 ms (86.05%) | Memory: 17.1 MB (6.27%) - LeetSync

--- solutions/1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ solutions/1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -1,42 +1,33 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        # Get length of input array
-        n = len(nums)
         
-        # First pass: Calculate longest increasing subsequence from right to left
-        t, suf = [], [0] * n  # t is temp array for binary search, suf stores lengths
-        for i in range(n - 1, 0, -1):
-            # Find position where nums[i] would fit in sorted array t
-            p = bisect_left(t, nums[i])
-            if p < len(t):
-                # If position exists, update suffix array and t
-                suf[i] = p
-                t[p] = nums[i]
+        # two pass + one pass
+        size = len(nums)
+        left = [0] * size
+        right = [0] * size
+        
+
+        dp = []
+        for i in range(size):
+            idx = bisect_left(dp, nums[i])
+            if idx == len(dp):
+                dp.append(nums[i])
             else:
-                # If position doesn't exist, append to t
-                suf[i] = len(t)
-                t.append(nums[i])
-        
-        # Second pass: Calculate longest increasing subsequence from left to right
-        # and find maximum mountain length
-        t, pre, res = [], 0, 0  # Reset t, pre tracks current LIS length
-        for i, x in enumerate(nums):
-            # Find position where current number would fit
-            p = bisect_left(t, x)
-            if p < len(t):
-                # If position exists, update prefix length and t
-                pre = p
-                t[p] = x
+                dp[idx] = nums[i]
+            left[i] = len(dp)
+
+        dp = []
+        for i in range(size - 1, -1, -1):
+            idx = bisect_left(dp, nums[i])
+            if idx == len(dp):
+                dp.append(nums[i])
             else:
-                # If position doesn't exist, append to t
-                pre = len(t)
-                t.append(x)
-            
-            # Check if we can form a mountain (need at least 1 element on each side)
-            if pre >= 1 and suf[i] >= 1:
-                # Update maximum mountain length (pre + suf[i] + 1)
-                res = max(res, pre + suf[i] + 1)
-        
-        # Return minimum removals needed
-        # Total length minus maximum mountain length
-        return n - res
+                dp[idx] = nums[i]
+            right[i] = len(dp)
+
+        minRemove = size
+        for i in range(size):
+            if left[i] > 1 and right[i] > 1:
+                minRemove = min(minRemove, size - left[i] - right[i] + 1)
+
+        return minRemove
