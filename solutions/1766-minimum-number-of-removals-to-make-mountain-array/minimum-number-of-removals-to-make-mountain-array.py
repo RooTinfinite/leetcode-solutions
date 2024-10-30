cf90f217aa982ada35b5405aa719d58d5c8dba59
Piotr M <filo74b@gmail.com>
2024-10-30 02:30:21 +0100

    Time: 12 ms (95.35%) | Memory: 17 MB (13.17%) - LeetSync

--- solutions/1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ solutions/1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -1,31 +1,25 @@
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        N = len(nums)
-        right = [0]*N 
-        tails = [-float(inf)] 
-        for i in range(N-1, -1, -1):
-            n = nums[i]
-            l = bisect_left(tails, n)
-            if l < len(tails):
-                tails[l] = n
+        n = len(nums)
+        t, suf = [], [0] * n
+        for i in range(n - 1, 0, -1):
+            p = bisect_left(t, nums[i])
+            if p < len(t):
+                suf[i] = p
+                t[p] = nums[i]
             else:
-                tails.append(n) 
-            right[i] = l
-        tails = [-float(inf)]
-        ans = N
-        for i, n in enumerate(nums):
-            l = bisect_left(tails, n)
-            r = right[i]
-            if l > 1 and r > 1:
-                ans = min(ans, N-(l+r-1))
-            if l < len(tails):
-                tails[l] = n
+                suf[i] = len(t)
+                t.append(nums[i])
+        t, pre, res = [], 0, 0
+        for i, x in enumerate(nums):
+            p = bisect_left(t, x)
+            if p < len(t):
+                pre = p
+                t[p] = x
             else:
-                tails.append(n)
-        return ans
-
-with open("user.out", "w") as f:
-    inputs = map(loads, stdin)
-    for nums in inputs:
-        print(Solution().minimumMountainRemovals(nums),file=f)
-exit(0)
+                pre = len(t)
+                t.append(x)
+            if pre >= 1 and suf[i] >= 1:
+                res = max(res, pre + suf[i] + 1)
+        return n - res
+        
