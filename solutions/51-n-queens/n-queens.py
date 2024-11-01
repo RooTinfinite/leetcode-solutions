b9d7c06384591d68283ab61c88a5467e38dad4b1
Piotr M <filo74b@gmail.com>
2024-11-01 22:16:42 +0100

    Time: 8 ms (84.37%) | Memory: 17.2 MB (26.55%) - LeetSync

--- solutions/51-n-queens/n-queens.py
+++ solutions/51-n-queens/n-queens.py
@@ -1,41 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def isSafePlace(board, row, col):
-            # Check column
-            for i in range(n):
-                if board[i][col] == 'Q':
-                    return False
-            
-            # Check upper left diagonal
-            i, j = row-1, col-1
-            while i >= 0 and j >= 0:
-                if board[i][j] == 'Q':
-                    return False
-                i -= 1
-                j -= 1
-            
-            # Check upper right diagonal
-            i, j = row-1, col+1
-            while i >= 0 and j < n:
-                if board[i][j] == 'Q':
-                    return False
-                i -= 1
-                j += 1
-                
-            return True
-        
-        def backtrack(row):
-            if row == n:
-                output.append([''.join(row) for row in board])
+        def backtrack(r):
+            if r == n:
+                copy = board[:]
+                sol = []
+                for c in copy:
+                    sol.append("".join(c[:]))
+                ans.append(sol)
                 return
-            
-            for col in range(n):
-                if isSafePlace(board, row, col):
-                    board[row][col] = 'Q'
-                    backtrack(row + 1)
-                    board[row][col] = '.'
+                
+
+            for c in range(n):
+                if c in placedCol or r + c in placedPos or r - c in placedNeg: continue
+
+                board[r][c] = "Q"
+                placedCol.add(c)
+                placedPos.add(r + c)
+                placedNeg.add(r - c)
+
+                backtrack(r + 1)
+
+                board[r][c] = "."
+                placedCol.remove(c)
+                placedPos.remove(r + c)
+                placedNeg.remove(r - c)
+
+        board = [["."] * n for _ in range(n)]
         
-        output = []
-        board = [['.' for _ in range(n)] for _ in range(n)]
+        placedCol = set()
+        placedPos = set()
+        placedNeg = set()
+        ans = []
         backtrack(0)
-        return output
+        return ans
