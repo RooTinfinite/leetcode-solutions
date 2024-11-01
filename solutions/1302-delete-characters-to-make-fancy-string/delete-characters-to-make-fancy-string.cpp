beff1f9a56737840e37e518dd898457591f0323a
Piotr M <filo74b@gmail.com>
2024-11-01 05:12:41 +0100

    Time: 17 ms (89.61%) | Memory: 42.7 MB (77.67%) - LeetSync

--- solutions/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ solutions/1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        if (s.length() < 3) return s;
+        string ret;
+        int cnt = 1;
+        char prev = '\0';
         
-        string result;
-        result.reserve(s.length()); // Pre-allocate memory
-        
-        // Add first two characters
-        result.push_back(s[0]);
-        result.push_back(s[1]);
-        
-        // Check for three consecutive characters
-        for (int i = 2; i < s.length(); i++) {
-            if (s[i] != s[i-1] || s[i] != s[i-2]) {
-                result.push_back(s[i]);
+        for (char c : s) {
+            if (c == prev) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            
+            prev = c;
+            
+            if (cnt < 3) {
+                ret += c;
             }
         }
         
-        return result;
+        return ret;
     }
 };
