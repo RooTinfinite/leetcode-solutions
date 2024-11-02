ed4572542712071e10f5846dfed9275f6429fbaa
Piotr M <filo74b@gmail.com>
2024-11-02 22:37:27 +0100

    Time: 1 ms (98.68%) | Memory: 48.1 MB (22.63%) - LeetSync

--- solutions/1-two-sum/two-sum.cs
+++ solutions/1-two-sum/two-sum.cs
@@ -1,23 +1,17 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        // Hash table to store number->index mapping
         Dictionary<int, int> ht = new Dictionary<int, int>();
         
-        // Iterate through the array
         for (int i = 0; i < nums.Length; i++) {
             int tmp = nums[i];
             
-            // Check if complement (target - current number) exists in hash table
             if (ht.ContainsKey(target - tmp)) {
-                // If found, return indices of both numbers
                 return new int[] {ht[target - tmp], i};
             }
             
-            // Store current number and its index in hash table
             ht[tmp] = i;
         }
         
-        // Return empty array if no solution found
         return new int[0];
     }
 }
