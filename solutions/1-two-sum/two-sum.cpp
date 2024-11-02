fc00f51da4c7307f1da6e33e28ffb2bbc10f546c
Piotr M <filo74b@gmail.com>
2024-11-02 18:20:31 +0100

    Time: 0 ms (100.00%) | Memory: 14.5 MB (20.51%) - LeetSync

--- solutions/1-two-sum/two-sum.cpp
+++ solutions/1-two-sum/two-sum.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    // Function to find two numbers in array that add up to target
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Hash table to store number->index mapping
         unordered_map<int, int> ht;
+        
+        // Variable to store current number
         int tmp;
+
+        // Iterate through the array
         for (int i = 0; i < nums.size(); ++i) {
             tmp = nums[i];
+            
+            // Check if complement (target - current number) exists in hash table
             if (ht.find(target-tmp) != ht.end()) {
-                return {ht[target-tmp],i};
+                // If found, return indices of both numbers
+                return {ht[target-tmp], i};
             }
+            
+            // Store current number and its index in hash table
             ht[tmp] = i;
         }
+        
+        // Return empty vector if no solution found
         return {};
     }
 };
