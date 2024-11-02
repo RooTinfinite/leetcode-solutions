41a8e2cf7ee8b385d533b9780c064a5363205cd9
Piotr M <filo74b@gmail.com>
2024-11-02 22:39:39 +0100

    Time: 3 ms (76.05%) | Memory: 53 MB (31.99%) - LeetSync

---
+++ solutions/1-two-sum/two-sum.ts
@@ -0,0 +1,17 @@
+// TypeScript
+
+function twoSum(nums: number[], target: number): number[] {
+    const ht = new Map<number, number>();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        
+        if (ht.has(target - num)) {
+            return [ht.get(target - num)!, i];
+        }
+        
+        ht.set(num, i);
+    }
+    
+    return [];
+}
