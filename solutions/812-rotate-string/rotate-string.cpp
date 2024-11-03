bb1fe704daba2bd21ad756d48bc41b70d5f32ff7
Piotr M <filo74b@gmail.com>
2024-11-03 01:22:18 +0100

    Time: 0 ms (100.00%) | Memory: 7.6 MB (55.81%) - LeetSync

--- solutions/812-rotate-string/rotate-string.cpp
+++ solutions/812-rotate-string/rotate-string.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.length() != goal.length()) return false;
-        string doubled = s + s;
-        return doubled.find(goal) != string::npos;
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).find(goal) != string::npos;
     }
 };
