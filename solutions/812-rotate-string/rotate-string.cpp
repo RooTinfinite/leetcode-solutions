5d9c4026d13ee8bb5371856e5bb8957d45134320
Piotr M <filo74b@gmail.com>
2024-11-03 01:04:45 +0100

    Time: 0 ms (100.00%) | Memory: 7.5 MB (83.69%) - LeetSync

--- solutions/812-rotate-string/rotate-string.cpp
+++ solutions/812-rotate-string/rotate-string.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        // Check if the lengths are different
-        if (s.length() != goal.length()) return false;
+        int n=s.length();
+        int m=goal.length();
 
-        // Create a new string by concatenating 's' with itself
-        string doubledString = s + s;
+        if(n!=m){
+            return false;
+        }
+        int i=0;
 
-        // Use find to search for 'goal' in 'doubledString'
-        // If find returns an index that is less than the
-        // length of doubledString, 'goal' is a substring
-        return doubledString.find(goal) < doubledString.length();
+        while(i<n){
+
+            if(s==goal){
+                return true;
+            }
+
+            char a=s[0];
+            for( int j=0; j<n-1; j++){
+                s[j]=s[j+1];
+            }
+            s[n-1]=a;
+            i++;
+
+        }
+        return false;
+        
     }
 };
