27bed681c1aaafbca174b79a0c821a854ad3c0f8
Piotr M <filo74b@gmail.com>
2024-11-04 04:20:43 +0100

    Time: 18 ms (80.34%) | Memory: 27.7 MB (91.92%) - LeetSync

--- solutions/3451-string-compression-iii/string-compression-iii.cpp
+++ solutions/3451-string-compression-iii/string-compression-iii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     string compressedString(string word) {
-        string comp = "";
-
-        // pos tracks our position in the input string
-        int pos = 0;
-
-        // Process until we reach end of string
-        while (pos < word.length()) {
-            int consecutiveCount = 0;
-
-            char currentChar = word[pos];
-
-            // Count consecutive occurrences (maximum 9)
-            while (pos < word.length() && consecutiveCount < 9 &&
-                   word[pos] == currentChar) {
-                consecutiveCount++;
-                pos++;
+        string temp;
+        char last=word[0];
+        int cnt=1;
+        for(int i=1; i<word.size(); i++){
+            if(last==word[i]){
+                if(cnt<9){
+                    ++cnt;
+                    continue;
+                }else{
+                    temp.push_back(cnt+'0');
+                    temp.push_back(last);
+                    cnt=1;
+                }
+            }else{
+                temp.push_back(cnt+'0');
+                temp.push_back(last);
+                cnt=1;
+                last=word[i];
             }
-
-            // Append count followed by character to result
-            comp += to_string(consecutiveCount) + currentChar;
         }
-
-        return comp;
+        temp.push_back(cnt+'0');
+        temp.push_back(last);
+        return temp;
     }
 };
