41d87ba5caccfea9a7cc62210375ad32e2891d55
Piotr M <filo74b@gmail.com>
2024-11-04 04:33:20 +0100

    Time: 7 ms (96.58%) | Memory: 27.8 MB (83.84%) - LeetSync

--- solutions/3451-string-compression-iii/string-compression-iii.cpp
+++ solutions/3451-string-compression-iii/string-compression-iii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     string compressedString(string word) {
-        string temp;
-        char last=word[0];
-        int cnt=1;
-        for(int i=1; i<word.size(); i++){
-            if(last==word[i]){
-                if(cnt<9){
-                    ++cnt;
-                    continue;
-                }else{
-                    temp.push_back(cnt+'0');
-                    temp.push_back(last);
-                    cnt=1;
-                }
-            }else{
-                temp.push_back(cnt+'0');
-                temp.push_back(last);
-                cnt=1;
-                last=word[i];
+        string comp = "";
+        int cnt = 1, n = word.size();
+        char ch = word[0];
+        for(int i=1;i<n;i++){
+            if(word[i] == ch && cnt < 9)cnt++;
+            else{
+                comp.push_back(cnt+'0');
+                comp.push_back(ch);
+                ch = word[i];
+                cnt = 1;
             }
         }
-        temp.push_back(cnt+'0');
-        temp.push_back(last);
-        return temp;
+        comp.push_back(cnt+'0');
+        comp.push_back(ch);
+        return comp;
     }
 };
