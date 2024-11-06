5e88c4b712ad1b58dd20963243f91098bedfdd83
Piotr M <filo74b@gmail.com>
2024-11-06 01:10:33 +0100

    Time: 4 ms (85.45%) | Memory: 16.7 MB (23.35%) - LeetSync

--- solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,31 +1,21 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
-        # Store pairs of (min, max) values for each group of numbers with same bit count
-        mm = []
-        
-        # Initialize first group with first number
-        mm.append([nums[0], nums[0]])
-        
-        # Iterate through array starting from second element
-        for i in range(1, len(nums)):
-            # Get bit count of current and previous numbers
-            cur = bin(nums[i]).count('1')
-            prev = bin(nums[i-1]).count('1')
-            
-            # If bit counts are different, start a new group
-            if cur != prev:
-                mm.append([nums[i], nums[i]])
-            
-            # Update min and max values of current group
-            mm[-1][0] = min(mm[-1][0], nums[i])
-            mm[-1][1] = max(mm[-1][1], nums[i])
-        
-        # Check if groups can be sorted
-        # For array to be sortable, max of previous group should be less than min of next group
-        for i in range(1, len(mm)):
-            if mm[i-1][1] > mm[i][0]:
-                return False
-        
-        # If all groups can be sorted, return true
-        return True
+        groups = []
+        curr = []
+        currBits = None
+        for num in nums:
+            if not currBits:
+                currBits = num.bit_count()
+            if num.bit_count() == currBits:
+                curr.append(num)
+            else:
+                currBits = num.bit_count()
+                groups.append(curr)
+                curr = [num]
+        if curr:
+            groups.append(curr)
 
+        out = []
+        for g in groups:
+            out += sorted(g)
+        return out == sorted(nums)
