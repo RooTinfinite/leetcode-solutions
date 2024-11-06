627fa7dd8aea110d31af61bffafd02040fe36ea1
Piotr M <filo74b@gmail.com>
2024-11-06 01:31:21 +0100

    Time: 1 ms (100.00%) | Memory: 44.2 MB (76.10%) - LeetSync

--- solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,53 +1,51 @@
-class Solution {
-    public boolean canSortArray(int[] nums) {
-        // Initialize list to store groups of numbers with same bit count
-        List<List<Integer>> groups = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        Integer currBits = null;
-        
-        // Iterate through each number in input array
-        for (int num : nums) {
-            // If this is start of first group, set initial bit count
-            if (currBits == null) {
-                currBits = Integer.bitCount(num);
-            }
-            
-            // If current number has same bit count as current group
-            if (Integer.bitCount(num) == currBits) {
-                curr.add(num);
-            } else {
-                // Different bit count found
-                currBits = Integer.bitCount(num);
-                groups.add(new ArrayList<>(curr));
-                curr.clear();
-                curr.add(num);
-            }
-        }
-        
-        // Add final group if exists
-        if (!curr.isEmpty()) {
-            groups.add(new ArrayList<>(curr));
-        }
-        
-        // Build output array by sorting each group internally
-        List<Integer> out = new ArrayList<>();
-        for (List<Integer> g : groups) {
-            Collections.sort(g);
-            out.addAll(g);
-        }
-        
-        // Create sorted version of original array for comparison
-        int[] sortedNums = nums.clone();
-        Arrays.sort(sortedNums);
-        
-        // Compare sorted output with sorted input
-        int i = 0;
-        for (int num : out) {
-            if (num != sortedNums[i++]) {
-                return false;
+class Solution 
+{
+    public boolean canSortArray(int[] nums) 
+    {
+        int n = nums.length;
+
+        // Copy the original array to values
+        int[] values = Arrays.copyOf(nums, n);
+
+        // First Pass: Iterate from left to right
+        // Goal: Move the maximum value of each segment as far right as possible
+        for (int i = 0; i < n - 1; i++) 
+        {
+            if (values[i] <= values[i + 1]) continue;
+            else 
+            {
+                // Count the number of set bits using Integer.bitCount
+                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i + 1])) 
+                {
+                    // Swap them if they have the same number of set bits
+                    int temp = values[i];
+                    values[i] = values[i + 1];
+                    values[i + 1] = temp;
+                } 
+                else return false; // Return false if they cannot be swapped
             }
         }
-        
+
+        // Second Pass: Iterate from right to left
+        // Goal: Move the minimum value of each segment as far left as possible
+        for (int i = n - 1; i >= 1; i--) 
+        {
+            if (values[i] >= values[i - 1]) continue;
+            else 
+            {
+                // Count the number of set bits using Integer.bitCount
+                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i - 1])) 
+                {
+                    // Swap them if they have the same number of set bits
+                    int temp = values[i];
+                    values[i] = values[i - 1];
+                    values[i - 1] = temp;
+                } 
+                else return false; // Return false if they cannot be swapped
+            }
+        }
+
+        // If both passes complete without returning false, the array can be sorted
         return true;
     }
 }
