68500ea98548ffbce7db67b5f07e76c0f8c5269e
Piotr M <filo74b@gmail.com>
2024-11-06 01:38:34 +0100

    Time: 1 ms (100.00%) | Memory: 43.7 MB (98.74%) - LeetSync

--- solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
+++ solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.java
@@ -1,51 +1,45 @@
-class Solution 
-{
-    public boolean canSortArray(int[] nums) 
-    {
-        int n = nums.length;
+class Solution {
+    // Determines if an array can be sorted by swapping adjacent elements with same number of set bits
 
-        // Copy the original array to values
+    public boolean canSortArray(int[] nums) {
+        int n = nums.length;
+        
+        // Create a copy of input array to work with
         int[] values = Arrays.copyOf(nums, n);
 
-        // First Pass: Iterate from left to right
-        // Goal: Move the maximum value of each segment as far right as possible
-        for (int i = 0; i < n - 1; i++) 
-        {
+        // Forward pass - check and swap from left to right
+        for (int i = 0; i < n - 1; i++) {
+            // Skip if elements are already in order
             if (values[i] <= values[i + 1]) continue;
-            else 
-            {
-                // Count the number of set bits using Integer.bitCount
-                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i + 1])) 
-                {
-                    // Swap them if they have the same number of set bits
+            else {
+                // Check if adjacent elements have same number of set bits
+                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i + 1])) {
+                    // Swap elements if they have same number of set bits
                     int temp = values[i];
                     values[i] = values[i + 1];
                     values[i + 1] = temp;
                 } 
-                else return false; // Return false if they cannot be swapped
+                else return false; // Cannot swap elements with different number of set bits
             }
         }
 
-        // Second Pass: Iterate from right to left
-        // Goal: Move the minimum value of each segment as far left as possible
-        for (int i = n - 1; i >= 1; i--) 
-        {
+        // Backward pass - check and swap from right to left
+        for (int i = n - 1; i >= 1; i--) {
+            // Skip if elements are already in order
             if (values[i] >= values[i - 1]) continue;
-            else 
-            {
-                // Count the number of set bits using Integer.bitCount
-                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i - 1])) 
-                {
-                    // Swap them if they have the same number of set bits
+            else {
+                // Check if adjacent elements have same number of set bits
+                if (Integer.bitCount(values[i]) == Integer.bitCount(values[i - 1])) {
+                    // Swap elements if they have same number of set bits
                     int temp = values[i];
                     values[i] = values[i - 1];
                     values[i - 1] = temp;
                 } 
-                else return false; // Return false if they cannot be swapped
+                else return false; // Cannot swap elements with different number of set bits
             }
         }
 
-        // If both passes complete without returning false, the array can be sorted
+        // Array can be sorted if we reach here
         return true;
     }
 }
