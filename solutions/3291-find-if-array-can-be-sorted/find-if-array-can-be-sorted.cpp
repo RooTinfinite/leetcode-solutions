e4bc64ee9d70bc3b20a72a0150aaf6c81542c364
Piotr M <filo74b@gmail.com>
2024-11-06 01:44:13 +0100

    Time: 0 ms (100.00%) | Memory: 31.6 MB (42.29%) - LeetSync

--- solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ solutions/3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        vector<pair<int,int>>mm;
-        mm.push_back({nums[0],nums[0]});
-        for(int i = 1 ; i < nums.size() ;i++)
-        {
-            int cur = __builtin_popcount(nums[i]);
-            int prev = __builtin_popcount(nums[i-1]);
-            if(cur != prev)
-            {   
-                mm.push_back({nums[i],nums[i]});
+        int n = nums.size();
+        
+        // Create a copy of input array to work with
+        vector<int> values = nums;
+
+        // Forward pass - check and swap from left to right
+        for (int i = 0; i < n - 1; i++) {
+            // Skip if elements are already in order
+            if (values[i] <= values[i + 1]) continue;
+            else {
+                // Check if adjacent elements have same number of set bits
+                if (__builtin_popcount(values[i]) == __builtin_popcount(values[i + 1])) {
+                    // Swap elements if they have same number of set bits
+                    swap(values[i], values[i + 1]);
+                } 
+                else return false; // Cannot swap elements with different number of set bits
             }
-            mm.back().first = min(mm.back().first,nums[i]);
-            mm.back().second = max(mm.back().second,nums[i]);
         }
-        for(int i = 1 ; i < mm.size() ; i++)
-        {
-            if(mm[i-1].second > mm[i].first)
-                return false;
+
+        // Backward pass - check and swap from right to left
+        for (int i = n - 1; i >= 1; i--) {
+            // Skip if elements are already in order
+            if (values[i] >= values[i - 1]) continue;
+            else {
+                // Check if adjacent elements have same number of set bits
+                if (__builtin_popcount(values[i]) == __builtin_popcount(values[i - 1])) {
+                    // Swap elements if they have same number of set bits
+                    swap(values[i], values[i - 1]);
+                } 
+                else return false; // Cannot swap elements with different number of set bits
+            }
         }
+
+        // Array can be sorted if we reach here
         return true;
     }
 };
