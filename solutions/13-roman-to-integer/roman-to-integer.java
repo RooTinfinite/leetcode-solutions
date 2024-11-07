6348ba55b44ba056d18034e996c0a6663672ada5
Piotr M <filo74b@gmail.com>
2024-11-08 00:31:57 +0100

    Time: 5 ms (62.68%) | Memory: 44.6 MB (54.88%) - LeetSync

--- solutions/13-roman-to-integer/roman-to-integer.java
+++ solutions/13-roman-to-integer/roman-to-integer.java
@@ -1,24 +1,25 @@
 class Solution {
-    private static int calcScale(char c, char a1, char a2) {
-        return (c == a1 || c == a2) ? -1 : 1;
-    }
-    
     public int romanToInt(String s) {
-        int result = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
         
-        for (int n = 0; n < s.length(); n++) {
-            char nextChar = (n + 1 < s.length()) ? s.charAt(n + 1) : '\0';
-            
-            switch (s.charAt(n)) {
-                case 'M': result += 1000; break;
-                case 'D': result += 500; break;
-                case 'C': result += 100 * calcScale(nextChar, 'M', 'D'); break;
-                case 'L': result += 50; break;
-                case 'X': result += 10 * calcScale(nextChar, 'C', 'L'); break;
-                case 'V': result += 5; break;
-                case 'I': result += 1 * calcScale(nextChar, 'X', 'V'); break;
+        int n = s.length();
+        int result = roman.get(s.charAt(n-1));
+        
+        for(int i = n-2; i >= 0; i--) {
+            if(roman.get(s.charAt(i)) < roman.get(s.charAt(i+1))) {
+                result -= roman.get(s.charAt(i));
+            } else {
+                result += roman.get(s.charAt(i));
             }
         }
+        
         return result;
     }
 }
