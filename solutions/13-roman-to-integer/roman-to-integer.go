dfd2cff278260195138ae51f3d9e9f84685ed5c7
Piotr M <filo74b@gmail.com>
2024-11-08 00:33:42 +0100

    Time: 3 ms (43.37%) | Memory: 4.5 MB (34.69%) - LeetSync

--- solutions/13-roman-to-integer/roman-to-integer.go
+++ solutions/13-roman-to-integer/roman-to-integer.go
@@ -1,35 +1,19 @@
-func calcScale(c byte, a1 byte, a2 byte) int {
-    if c == a1 || c == a2 {
-        return -1
-    }
-    return 1
-}
-
 func romanToInt(s string) int {
-    result := 0
+    roman := map[byte]int{
+        'I': 1, 'V': 5, 'X': 10,
+        'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+    }
     
-    for n := 0; n < len(s); n++ {
-        var nextChar byte
-        if n+1 < len(s) {
-            nextChar = s[n+1]
-        }
-        
-        switch s[n] {
-        case 'M':
-            result += 1000
-        case 'D':
-            result += 500
-        case 'C':
-            result += 100 * calcScale(nextChar, 'M', 'D')
-        case 'L':
-            result += 50
-        case 'X':
-            result += 10 * calcScale(nextChar, 'C', 'L')
-        case 'V':
-            result += 5
-        case 'I':
-            result += 1 * calcScale(nextChar, 'X', 'V')
+    n := len(s)
+    result := roman[s[n-1]]
+    
+    for i := n-2; i >= 0; i-- {
+        if roman[s[i]] < roman[s[i+1]] {
+            result -= roman[s[i]]
+        } else {
+            result += roman[s[i]]
         }
     }
+    
     return result
 }
