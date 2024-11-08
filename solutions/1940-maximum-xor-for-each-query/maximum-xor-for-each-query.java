a7dbb3902b5b6b6e2195fa2433a7757eec9e9cfd
Piotr M <filo74b@gmail.com>
2024-11-08 01:39:41 +0100

    Time: 3 ms (78.95%) | Memory: 58.5 MB (88.79%) - LeetSync

--- solutions/1940-maximum-xor-for-each-query/maximum-xor-for-each-query.java
+++ solutions/1940-maximum-xor-for-each-query/maximum-xor-for-each-query.java
@@ -7,7 +7,7 @@ class Solution {
         
         for(int i = 0; i < n; i++) {
             curr ^= nums[i];
-            res[n-i-1] = ~curr & mask;
+            res[n-1-i] = curr ^ mask;
         }
         
         return res;
