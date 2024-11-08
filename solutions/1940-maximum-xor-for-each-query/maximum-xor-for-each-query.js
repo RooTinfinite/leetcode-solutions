4c1d8a16e6eecdc01846dd85b388241072baf426
Piotr M <filo74b@gmail.com>
2024-11-08 01:22:22 +0100

    Time: 8 ms (80.00%) | Memory: 74.7 MB (78.13%) - LeetSync

---
+++ solutions/1940-maximum-xor-for-each-query/maximum-xor-for-each-query.js
@@ -0,0 +1,15 @@
+// JavaScript
+
+var getMaximumXor = function(nums, maximumBit) {
+    const mask = (1 << maximumBit) - 1;
+    const n = nums.length;
+    const res = new Array(n);
+    let curr = 0;
+    
+    for(let i = 0; i < n; i++) {
+        curr ^= nums[i];
+        res[n-i-1] = (~curr & mask);
+    }
+    
+    return res;
+};
