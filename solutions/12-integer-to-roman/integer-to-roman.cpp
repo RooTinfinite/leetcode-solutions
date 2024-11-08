f8140db28be7bbc8be11600ea0a204d2682a2b46
Piotr M <filo74b@gmail.com>
2024-11-08 15:08:53 +0100

    Time: 8 ms (26.48%) | Memory: 10.1 MB (79.11%) - LeetSync

--- solutions/12-integer-to-roman/integer-to-roman.cpp
+++ solutions/12-integer-to-roman/integer-to-roman.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int n[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string s[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
-        string str;
-        int i =0;
-        while(num>0){
-            if(num>=n[i]){
-                str+=s[i];
-                num-=n[i];
-            }
-            else ++i;
-        }
-        return str;
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
