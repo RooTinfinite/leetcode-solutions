634ce3eadc027bc9d2c7d70fbad8d22f72c4c70a
Piotr M <filo74b@gmail.com>
2024-11-09 04:23:14 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (100.00%) - LeetSync

--- solutions/3394-minimum-array-end/minimum-array-end.swift
+++ solutions/3394-minimum-array-end/minimum-array-end.swift
@@ -1,35 +1,17 @@
 class Solution {
     func minEnd(_ n: Int, _ x: Int) -> Int {
-        // Store available bit positions that are 0 in x
-        var shifts: [Int] = []
+        var result = Int64(x)
+        var remaining = Int64(n - 1)
+        var position: Int64 = 1
         
-        // Initialize result with starting number x
-        var cur = x
-        
-        // Calculate how many 1's we need to add (n-1)
-        var calc = n - 1
-        
-        // Find all bit positions that are 0 in x (up to 32 bits)
-        for i in 0..<32 {
-            if ((1 << i) & x) == 0 {
-                shifts.append(i)
+        while remaining != 0 {
+            if (Int64(x) & position) == 0 {
+                result |= (remaining & 1) * position
+                remaining >>= 1
             }
+            position <<= 1
         }
         
-        // Add remaining positions 32-63 since Int64 is 64 bit
-        for i in 32..<64 {
-            shifts.append(i)
-        }
-        
-        // Process each bit in calc (n-1)
-        // If bit is 1, set that bit in next available position from shifts
-        var i = 0
-        while calc > 0 {
-            cur += ((calc & 1) << shifts[i])
-            i += 1
-            calc >>= 1
-        }
-        
-        return cur
+        return Int(result)
     }
 }
