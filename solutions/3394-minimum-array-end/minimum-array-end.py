39cdc682069d6e8ac9d4f79279bbd6e5bb063a4b
Piotr M <filo74b@gmail.com>
2024-11-09 03:43:47 +0100

    Time: 0 ms (100.00%) | Memory: 16.6 MB (69.70%) - LeetSync

--- solutions/3394-minimum-array-end/minimum-array-end.py
+++ solutions/3394-minimum-array-end/minimum-array-end.py
@@ -1,13 +1,14 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
         result = x
-        n -= 1
-        pos = 0
+        remaining = n - 1
+        position = 0
         
-        while n:
-            if not (x & (1 << pos)):
-                result |= (n & 1) << pos
-                n >>= 1
-            pos += 1
+        while remaining:
+            current_bit = 1 << position
+            if not (x & current_bit):
+                result |= (remaining & 1) << position
+                remaining >>= 1
+            position += 1
             
         return result
