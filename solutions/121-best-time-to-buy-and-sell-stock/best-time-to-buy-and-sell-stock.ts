3a183120822ef4455c0f60529ec98fa2be2f9921
Piotr M <filo74b@gmail.com>
2024-11-09 23:21:18 +0100

    Time: 3 ms (50.16%) | Memory: 59.6 MB (62.91%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -1,19 +1,12 @@
-/**
- * @param {number[]} prices
- * @return {number}
- */
 var maxProfit = function(prices) {
     let minPrice = prices[0];
     let maxProfit = 0;
 
     for (let i = 1; i < prices.length; i++) {
-        // Update minPrice if we find a lower price
         if (prices[i] < minPrice) {
             minPrice = prices[i];
         } else {
-            // Calculate profit if sold at the current price
             let profit = prices[i] - minPrice;
-            // Update maxProfit if this profit is higher
             if (profit > maxProfit) {
                 maxProfit = profit;
             }
