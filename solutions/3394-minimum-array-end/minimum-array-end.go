2cfd7dc5476cbc615c7637f463cc74a58a587ca7
Piotr M <filo74b@gmail.com>
2024-11-09 04:21:53 +0100

    Time: 0 ms (100.00%) | Memory: 4 MB (60.00%) - LeetSync

--- solutions/3394-minimum-array-end/minimum-array-end.go
+++ solutions/3394-minimum-array-end/minimum-array-end.go
@@ -1,30 +1,15 @@
 func minEnd(n int, x int) int64 {
-    // Store available bit positions that are 0 in x
-    shifts := make([]int16, 0)
+    result := int64(x)
+    remaining := int64(n - 1)
+    position := int64(1)
     
-    // Initialize result with starting number x
-    cur := int64(x)
-    
-    // Calculate how many 1's we need to add (n-1)
-    calc := int64(n - 1)
-    
-    // Find all bit positions that are 0 in x (up to 32 bits)
-    for i := 0; i < 32; i++ {
-        if ((1 << i) & x) == 0 {
-            shifts = append(shifts, int16(i))
+    for remaining != 0 {
+        if (int64(x) & position) == 0 {
+            result |= (remaining & 1) * position
+            remaining >>= 1
         }
+        position <<= 1
     }
     
-    // Add remaining positions 32-63 since int64 is 64 bit
-    for i := 32; i < 64; i++ {
-        shifts = append(shifts, int16(i))
-    }
-    
-    // Process each bit in calc (n-1)
-    for i := 0; calc > 0; i++ {
-        cur += (calc & 1) << shifts[i]
-        calc >>= 1
-    }
-    
-    return cur
+    return result
 }
