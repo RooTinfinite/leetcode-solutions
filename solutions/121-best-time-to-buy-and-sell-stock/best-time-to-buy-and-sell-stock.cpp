f576f32bd35440886011979bb8e12b9a3d802aaa
Piotr M <filo74b@gmail.com>
2024-11-09 18:15:53 +0100

    Time: 4 ms (19.21%) | Memory: 96.3 MB (6.37%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,21 @@
+#include <vector>
+#include <algorithm> // Include algorithm header for the min and max functions
+
 class Solution {
 public:
-    int maxProfit(vector<int>& nums) {
-        int minPro=INT_MAX, maxPro=0;
-        for(int i=0;i<nums.size();i++) {
-            minPro = min(nums[i],minPro);
-            maxPro = max(maxPro, (nums[i]-minPro));
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;   // Variable to store the calculated maximum profit
+        int min_price = prices[0]; // Initialize the minimum price to the first price
+
+        // Loop through all the prices
+        for (int price : prices) {
+            // Update max_profit if the difference between current price and min_price is greater than current max_profit
+            max_profit = max(max_profit, price - min_price);
+          
+            // Update min_price if the current price is less than the current min_price
+            min_price = min(min_price, price);
         }
-        return maxPro;
+      
+        return max_profit; // Return the calculated maximum profit
     }
 };
