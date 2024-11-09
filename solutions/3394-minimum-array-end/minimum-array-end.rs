fb86cdf6aa39f12215af3187536dfe3369d647c9
Piotr M <filo74b@gmail.com>
2024-11-09 13:48:52 +0100

    Time: 0 ms (100.00%) | Memory: 2.1 MB (100.00%) - LeetSync

--- solutions/3394-minimum-array-end/minimum-array-end.rs
+++ solutions/3394-minimum-array-end/minimum-array-end.rs
@@ -1,7 +1,8 @@
-use core::arch::x86_64::_pdep_u64;
-
 impl Solution {
     pub fn min_end(n: i32, x: i32) -> i64 {
-        return unsafe { (x as u64 | _pdep_u64(n as u64 - 1u64, !(x as u64))) as i64 };
+        #[cfg(target_arch = "x86_64")]
+        unsafe {
+            (x as u64 | core::arch::x86_64::_pdep_u64(n as u64 - 1, !(x as u64))) as i64
+        }
     }
 }
