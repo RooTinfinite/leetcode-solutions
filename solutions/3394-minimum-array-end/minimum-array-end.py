7e0e94c418120b8c7ae9538c039e420c33cf00ff
Piotr M <filo74b@gmail.com>
2024-11-09 01:02:58 +0100

    Time: 4 ms (31.91%) | Memory: 16.8 MB (8.33%) - LeetSync

--- solutions/3394-minimum-array-end/minimum-array-end.py
+++ solutions/3394-minimum-array-end/minimum-array-end.py
@@ -1,39 +1,40 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
+        if n == 1:
+            return x
+        one_count = x.bit_count()
+        k = len(bin(x)) - 2
+        max_ = 2 ** k - 1
+        if one_count == k:
+            size = 1
+        else:
+            size = 2 ** (k - one_count)
+        if floor((n - 1)/ size) > 0:
+            a = floor((n -1) / size) << k
+        else:
+            a = 0
+        result = a
+        left = (n - 1)% size
+        x = bin(x)[2:]
+        t = ""
 
-        result = 0
-        # Reducing n by 1 to exclude x from the iteration
-        n -= 1
-
-        # Step 1: Initialize lists to hold the binary representation of x and n-1
-        binaryX = [0] * 64  # Binary representation of x
-        binaryN = [0] * 64  # Binary representation of n-1
-
-        # Step 2: Build binary representations of x and n-1
-        for i in range(64):
-            bit = (x >> i) & 1  # Extract i-th bit of x
-            binaryX[i] = bit
-
-            bit = (n >> i) & 1  # Extract i-th bit of n-1
-            binaryN[i] = bit
-
-        posX = 0
-        posN = 0
-
-        # Step 3: Combine binary representation of x and n-1
-        while posX < 63:
-            # Traverse binaryX until we find a 0 bit
-            while binaryX[posX] != 0 and posX < 63:
-                posX += 1
-            # Copy bits from binaryN (n-1) into binaryX (x) starting from the first 0
-            binaryX[posX] = binaryN[posN]
-            posX += 1
-            posN += 1
-
-        # Step 4: Rebuild the final result from the combined binary representation
-        for i in range(64):
-            if binaryX[i] == 1:
-                # convert binary bit to decimal value
-                result += pow(2, i)
-
+        left = bin(left)[2:]
+        limit = len(left)
+        i = limit - 1
+        print(left, result)
+        print(x)
+        for k in x[::-1]:
+            print("k", k, t)
+            if k == "1":
+                t = "1" + t
+            elif i >= 0:
+                if left[i] == "1":
+                    t = "1" + t
+                else:
+                    t = "0" + t
+                i -= 1
+            else:
+                t = "0" + t
+        result += int(t, 2)
         return result
+       
