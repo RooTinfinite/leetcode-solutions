c2fb55bb5b6ea97ad463b59a14290c8b15c63580
Piotr M <filo74b@gmail.com>
2024-11-09 04:31:30 +0100

    Time: 0 ms (100.00%) | Memory: 211.4 MB (100.00%) - LeetSync

--- solutions/3394-minimum-array-end/minimum-array-end.rb
+++ solutions/3394-minimum-array-end/minimum-array-end.rb
@@ -1,30 +1,15 @@
 def min_end(n, x)
-    # Store available bit positions that are 0 in x
-    shifts = []
+    result = x
+    remaining = n - 1
+    position = 1
     
-    # Initialize result with starting number x
-    cur = x
-    
-    # Calculate how many 1's we need to add (n-1)
-    calc = n - 1
-    
-    # Find all bit positions that are 0 in x (up to 32 bits)
-    (0..31).each do |i|
-        shifts.push(i) if ((1 << i) & x) == 0
+    while remaining != 0
+        if (x & position) == 0
+            result |= (remaining & 1) * position
+            remaining >>= 1
+        end
+        position <<= 1
     end
     
-    # Add remaining positions 32-63 for larger numbers
-    (32..63).each do |i|
-        shifts.push(i)
-    end
-    
-    # Process each bit in calc (n-1)
-    i = 0
-    while calc > 0
-        cur += (calc & 1) << shifts[i]
-        calc >>= 1
-        i += 1
-    end
-    
-    cur
+    result.to_i
 end
