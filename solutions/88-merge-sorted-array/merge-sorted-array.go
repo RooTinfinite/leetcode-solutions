47957e8a4501ce64b906f27f50a8aab3a3852de2
Piotr M <filo74b@gmail.com>
2024-11-10 20:06:49 +0100

    Time: 0 ms (100.00%) | Memory: 4 MB (26.38%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.go
+++ solutions/88-merge-sorted-array/merge-sorted-array.go
@@ -1,6 +1,16 @@
 func merge(nums1 []int, m int, nums2 []int, n int) {
-    for j, i := 0, m; j < n; j, i = j+1, i+1 {
-        nums1[i] = nums2[j]
+    k := m + n - 1
+    i := m - 1
+    j := n - 1
+    
+    for j >= 0 {
+        if i >= 0 && nums1[i] > nums2[j] {
+            nums1[k] = nums1[i]
+            i--
+        } else {
+            nums1[k] = nums2[j]
+            j--
+        }
+        k--
     }
-    sort.Ints(nums1)
 }
