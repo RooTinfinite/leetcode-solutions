5b8737118972e358bee5f24745f1b32fe4084213
Piotr M <filo74b@gmail.com>
2024-11-10 14:21:25 +0100

    Time: 88 ms (56.23%) | Memory: 26 MB (87.63%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
+
+        if not prices or len(prices) < 2:
+            return 0
+            
         max_profit = 0
+        left_buy = 0
+        right_sell = 1 #you can't buy later and sell earlier, unless you're doctor who :)
         
-        for current_price in prices:
-            min_price = min(current_price, min_price)
-            max_profit = max(max_profit, current_price - min_price)
+        while right_sell < len(prices):
+            current_price = prices[right_sell]
+            buy_price = prices[left_buy]
+            
+            if buy_price < current_price:
+                current_profit = current_price - buy_price
+                max_profit = max(max_profit, current_profit)
+            else:
+                left_buy = right_sell
+                
+            right_sell += 1
             
         return max_profit
-
