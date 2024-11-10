d1d868c79c33fc5a42f2d8265b4a71487b792dd5
Piotr M <filo74b@gmail.com>
2024-11-10 01:31:58 +0100

    Time: 402 ms (100.00%) | Memory: 35.9 MB (69.92%) - LeetSync

--- solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.py
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.py
@@ -1,11 +1,31 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        ans = inf
-        d = dict() 
-        for i, x in enumerate(nums):
-            d = {or_ | x: left for or_, left in d.items()}
-            d[x] = i  
-            for or_, left in d.items():
-                if or_ >= k:
-                    ans = min(ans, i - left + 1)
-        return ans if ans < inf else -1
+        if k == 0:
+            return 1
+        shortest = len(nums) + 1
+        count = [0] * 32
+        val = 0
+        start = 0
+        
+        for i in range(len(nums)):
+            num = nums[i]
+            val |= num
+            ibit = 0
+            while num:
+                count[ibit] += num & 1
+                num >>= 1
+                ibit += 1
+                
+            while val >= k and start < len(nums):
+                shortest = min(shortest, i - start + 1)
+                num = nums[start]
+                start += 1
+                ibit = 0
+                while num:
+                    count[ibit] -= num & 1
+                    if count[ibit] == 0:
+                        val &= ~(1 << ibit)
+                    num >>= 1
+                    ibit += 1
+                    
+        return -1 if shortest == len(nums) + 1 else shortest
