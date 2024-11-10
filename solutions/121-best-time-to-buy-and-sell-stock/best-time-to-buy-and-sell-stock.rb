dce8cd6905180fa715ca52b67c95021b769fcca2
Piotr M <filo74b@gmail.com>
2024-11-10 14:58:08 +0100

    Time: 42 ms (90.40%) | Memory: 221.2 MB (23.90%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rb
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rb
@@ -1,10 +1,22 @@
 def max_profit(prices)
-    min_price = Float::INFINITY
-    max_profit = 0
+    return 0 if prices.nil? || prices.length < 2
     
-    prices.each do |current_price|
-        min_price = [current_price, min_price].min
-        max_profit = [max_profit, current_price - min_price].max
+    max_profit = 0
+    left_buy = 0
+    right_sell = 1
+    
+    while right_sell < prices.length
+        current_price = prices[right_sell]
+        buy_price = prices[left_buy]
+        
+        if buy_price < current_price
+            current_profit = current_price - buy_price
+            max_profit = [max_profit, current_profit].max
+        else
+            left_buy = right_sell
+        end
+        
+        right_sell += 1
     end
     
     max_profit
