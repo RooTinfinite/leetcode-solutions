272f7341a4b76c96ba5d493f752de1178fe24eaf
Piotr M <filo74b@gmail.com>
2024-11-10 02:05:57 +0100

    Time: 3 ms (100.00%) | Memory: 4.1 MB (66.67%) - LeetSync

--- solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.rs
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.rs
@@ -1,37 +1,37 @@
 impl Solution {
     pub fn minimum_subarray_length(nums: Vec<i32>, k: i32) -> i32 {
-        if k == 0 {
-            return 1;
-        }
-        let mut shortest = nums.len() + 1;
-        let mut count = [0; 32];
-        let mut val = 0;
-        let mut start = 0;
+        let mut ans = i32::MAX;
+        let mut s = 0;
+        let mut left = 0;
+        let mut cnt = vec![0; 32];
         
-        for i in 0..nums.len() {
-            let mut num = nums[i];
-            val |= num;
-            let mut ibit = 0;
-            while num != 0 {
-                count[ibit] += num & 1;
-                num >>= 1;
-                ibit += 1;
+        for right in 0..nums.len() {
+            let mut num = nums[right];
+            s |= num;
+            let mut i = 0;
+            while num > 0 {
+                cnt[i] += num % 2;
+                num /= 2;
+                i += 1;
             }
-            while val >= k && start < nums.len() {
-                shortest = shortest.min(i - start + 1);
-                num = nums[start];
-                start += 1;
-                ibit = 0;
-                while num != 0 {
-                    count[ibit] -= num & 1;
-                    if count[ibit] == 0 {
-                        val &= !(1 << ibit);
+            
+            while s >= k && left <= right {
+                ans = ans.min((right - left + 1) as i32);
+                num = nums[left];
+                i = 0;
+                while num > 0 {
+                    if num % 2 == 1 {
+                        cnt[i] -= 1;
+                        if cnt[i] == 0 {
+                            s ^= 1 << i;
+                        }
                     }
-                    num >>= 1;
-                    ibit += 1;
+                    num /= 2;
+                    i += 1;
                 }
+                left += 1;
             }
         }
-        if shortest == nums.len() + 1 { -1 } else { shortest as i32 }
+        if ans == i32::MAX { -1 } else { ans }
     }
 }
