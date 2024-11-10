faa7422df9803298f4de3ab6003292b178e3ecf7
Piotr M <filo74b@gmail.com>
2024-11-10 12:54:12 +0100

    Time: 0 ms (100.00%) | Memory: 96.2 MB (25.47%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& nums) {
-        int minPro=INT_MAX, maxPro=0;
-        for(int i=0;i<nums.size();i++) {
-            minPro = min(nums[i],minPro);
-            maxPro = max(maxPro, (nums[i]-minPro));
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+        
+        for (int currentPrice : prices) {
+            minPrice = min(currentPrice, minPrice);
+            maxProfit = max(maxProfit, currentPrice - minPrice);
         }
-        return maxPro;
+        
+        return maxProfit;
     }
 };
