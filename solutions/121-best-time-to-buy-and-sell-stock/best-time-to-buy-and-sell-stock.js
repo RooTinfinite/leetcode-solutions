fa3275df8f79cf1c906d4768d9860b4b6e450833
Piotr M <filo74b@gmail.com>
2024-11-10 14:57:52 +0100

    Time: 2 ms (75.14%) | Memory: 59.4 MB (34.24%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -1,10 +1,26 @@
+// JavaScript
+
 var maxProfit = function(prices) {
-    let minPrice = Number.MAX_VALUE;
-    let maxProfit = 0;
+    if (!prices || prices.length < 2) {
+        return 0;
+    }
     
-    for (let currentPrice of prices) {
-        minPrice = Math.min(currentPrice, minPrice);
-        maxProfit = Math.max(maxProfit, currentPrice - minPrice);
+    let maxProfit = 0;
+    let leftBuy = 0;
+    let rightSell = 1;
+    
+    while (rightSell < prices.length) {
+        const currentPrice = prices[rightSell];
+        const buyPrice = prices[leftBuy];
+        
+        if (buyPrice < currentPrice) {
+            const currentProfit = currentPrice - buyPrice;
+            maxProfit = Math.max(maxProfit, currentProfit);
+        } else {
+            leftBuy = rightSell;
+        }
+        
+        rightSell++;
     }
     
     return maxProfit;
