df72e563726b9da762224a093b3654508d612c59
Piotr M <filo74b@gmail.com>
2024-11-10 02:00:51 +0100

    Time: 11 ms (100.00%) | Memory: 69.6 MB (100.00%) - LeetSync

--- solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cs
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cs
@@ -1,33 +1,36 @@
 public class Solution {
     public int MinimumSubarrayLength(int[] nums, int k) {
-        if (k == 0) {
-            return 1;
-        }
-        int shortest = nums.Length + 1;
-        int[] count = new int[32];
-        int val = 0;
-        int start = 0;
+        int ans = int.MaxValue, s = 0, left = 0;
+        int[] cnt = new int[32];
         
-        for (int i = 0; i < nums.Length; ++i) {
-            int num = nums[i];
-            val |= num;
-            for (int ibit = 0; num != 0; ++ibit) {
-                count[ibit] += num & 1;
-                num >>= 1;
+        for (int right = 0; right < nums.Length; right++) {
+            int num = nums[right];
+            s |= num;
+            int i = 0;
+            int temp = num;
+            while (temp > 0) {
+                cnt[i] += temp % 2;
+                temp /= 2;
+                i++;
             }
-            while (val >= k && start < nums.Length) {
-                shortest = Math.Min(shortest, i - start + 1);
-                num = nums[start];
-                ++start;
-                for (int ibit = 0; num != 0; ++ibit) {
-                    count[ibit] -= num & 1;
-                    if (count[ibit] == 0) {
-                        val &= ~(1 << ibit);
+            
+            while (s >= k && left <= right) {
+                ans = Math.Min(right - left + 1, ans);
+                temp = nums[left];
+                i = 0;
+                while (temp > 0) {
+                    if (temp % 2 == 1) {
+                        cnt[i]--;
+                        if (cnt[i] == 0) {
+                            s ^= 1 << i;
+                        }
                     }
-                    num >>= 1;
+                    temp /= 2;
+                    i++;
                 }
+                left++;
             }
         }
-        return shortest == (nums.Length + 1) ? -1 : shortest;
+        return ans == int.MaxValue ? -1 : ans;
     }
 }
