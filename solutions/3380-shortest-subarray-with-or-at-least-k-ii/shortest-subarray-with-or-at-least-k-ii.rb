00878515155db2dd47c9125f65fb0b502bad2c87
Piotr M <filo74b@gmail.com>
2024-11-10 01:32:47 +0100

    Time: 401 ms (100.00%) | Memory: 220.9 MB (100.00%) - LeetSync

--- solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.rb
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.rb
@@ -1,21 +1,35 @@
 def minimum_subarray_length(nums, k)
-    ans = Float::INFINITY
-    d = {}
+    return 1 if k == 0
+    shortest = nums.length + 1
+    count = Array.new(32, 0)
+    val = 0
+    start = 0
     
-    nums.each_with_index do |x, i|
-        temp = {}
-        d.each do |or_, left|
-            temp[or_ | x] = left
+    (0...nums.length).each do |i|
+        num = nums[i]
+        val |= num
+        ibit = 0
+        while num != 0
+            count[ibit] += num & 1
+            num >>= 1
+            ibit += 1
         end
-        temp[x] = i
-        d = temp
         
-        d.each do |or_, left|
-            if or_ >= k
-                ans = [ans, i - left + 1].min
+        while val >= k && start < nums.length
+            shortest = [shortest, i - start + 1].min
+            num = nums[start]
+            start += 1
+            ibit = 0
+            while num != 0
+                count[ibit] -= num & 1
+                if count[ibit] == 0
+                    val &= ~(1 << ibit)
+                end
+                num >>= 1
+                ibit += 1
             end
         end
     end
     
-    ans == Float::INFINITY ? -1 : ans
+    shortest == nums.length + 1 ? -1 : shortest
 end
