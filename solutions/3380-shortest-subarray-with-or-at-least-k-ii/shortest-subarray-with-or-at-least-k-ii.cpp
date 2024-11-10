9ac6320f39d766e5c7d0c93b4cdfb6850e3d558c
Piotr M <filo74b@gmail.com>
2024-11-10 01:55:44 +0100

    Time: 12 ms (98.90%) | Memory: 88.2 MB (74.37%) - LeetSync

--- solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    int minimumSubarrayLength(vector<int>& nums, int k) {
-        if (k == 0) {
-            return 1;
-        }
-        int shortest = nums.size() + 1;
-        int count[32] = {};
-        int val = 0;
-        int start = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            int num = nums[i];
-            val |= num;
-            for (int ibit = 0; num; ++ibit) {
-                count[ibit] += num & 1;
-                num >>= 1;
+    int minimumSubarrayLength(std::vector<int>& nums, int k) {
+        int ans = INT_MAX;
+        int s = 0;
+        int left = 0;
+        std::vector<int> cnt(32, 0);
+        
+        for (int right = 0; right < nums.size(); ++right) {
+            int num = nums[right];
+            s |= num;
+            int i = 0;
+            
+            // Counting bits in num
+            while (num > 0) {
+                cnt[i] += num % 2;
+                num /= 2;
+                ++i;
             }
-            while (val >= k && start < nums.size()) {
-                shortest = min(shortest, i - start + 1);
-                num = nums[start];
-                ++start;
-                for (int ibit = 0; num; ++ibit) {
-                    count[ibit] -= num & 1;
-                    if (count[ibit] == 0) {
-                        val &= ~(1 << ibit);
+
+            // Shrink window from the left
+            while (s >= k && left <= right) {
+                ans = std::min(right - left + 1, ans);
+                num = nums[left];
+                i = 0;
+
+                while (num > 0) {
+                    if (num % 2) {
+                        cnt[i] -= 1;
+                        if (cnt[i] == 0) {
+                            s ^= (1 << i);
+                        }
                     }
-                    num >>= 1;
+                    num /= 2;
+                    ++i;
                 }
+                ++left;
             }
         }
-        return shortest == (nums.size() + 1) ? -1 : shortest;
+        
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
