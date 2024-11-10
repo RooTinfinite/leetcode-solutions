b6aabe607df5acb9436cc16d76bb0f176364c844
Piotr M <filo74b@gmail.com>
2024-11-10 19:44:16 +0100

    Time: 0 ms (100.00%) | Memory: 51.6 MB (39.67%) - LeetSync

---
+++ solutions/88-merge-sorted-array/merge-sorted-array.ts
@@ -0,0 +1,8 @@
+// TypeScript
+
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+    for (let j = 0, i = m; j < n; j++, i++) {
+        nums1[i] = nums2[j];
+    }
+    nums1.sort((a, b) => a - b);
+}
