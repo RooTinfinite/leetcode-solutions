003f505e379a9ecf329abc8c7493a001c7d9ea07
Piotr M <filo74b@gmail.com>
2024-11-10 01:02:38 +0100

    Time: 12 ms (98.90%) | Memory: 88.3 MB (44.57%) - LeetSync

--- solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,53 +1,33 @@
 class Solution {
 public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
-        int minLength = INT_MAX;
-        int windowStart = 0;
-        int windowEnd = 0;
-        vector<int> bitCounts(32,
-                              0);  // Tracks count of set bits at each position
-
-        // Expand window until end of array
-        while (windowEnd < nums.size()) {
-            // Add current number to window
-            updateBitCounts(bitCounts, nums[windowEnd], 1);
-
-            // Contract window while OR value is valid
-            while (windowStart <= windowEnd &&
-                   convertBitCountsToNumber(bitCounts) >= k) {
-                // Update minimum length found so far
-                minLength = min(minLength, windowEnd - windowStart + 1);
-
-                // Remove leftmost number and shrink window
-                updateBitCounts(bitCounts, nums[windowStart], -1);
-                windowStart++;
-            }
-
-            windowEnd++;
+        if (k == 0) {
+            return 1;
         }
-
-        return minLength == INT_MAX ? -1 : minLength;
-    }
-
-private:
-    // Updates bit count array when adding/removing a number from window
-    void updateBitCounts(vector<int>& bitCounts, int number, int delta) {
-        for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
-            // Check if bit is set at current position
-            if ((number >> bitPosition) & 1) {
-                bitCounts[bitPosition] += delta;
+        int shortest = nums.size() + 1;
+        int count[32] = {};
+        int val = 0;
+        int start = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            val |= num;
+            for (int ibit = 0; num; ++ibit) {
+                count[ibit] += num & 1;
+                num >>= 1;
+            }
+            while (val >= k && start < nums.size()) {
+                shortest = min(shortest, i - start + 1);
+                num = nums[start];
+                ++start;
+                for (int ibit = 0; num; ++ibit) {
+                    count[ibit] -= num & 1;
+                    if (count[ibit] == 0) {
+                        val &= ~(1 << ibit);
+                    }
+                    num >>= 1;
+                }
             }
         }
-    }
-
-    // Converts bit count array back to number using OR operation
-    int convertBitCountsToNumber(const vector<int>& bitCounts) {
-        int result = 0;
-        for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
-            if (bitCounts[bitPosition] != 0) {
-                result |= 1 << bitPosition;
-            }
-        }
-        return result;
+        return shortest == (nums.size() + 1) ? -1 : shortest;
     }
 };
