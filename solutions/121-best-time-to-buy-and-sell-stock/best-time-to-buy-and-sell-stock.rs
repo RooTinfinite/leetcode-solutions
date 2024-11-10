aabc28d0058b02847ee60077f62b0fc0202fd42e
Piotr M <filo74b@gmail.com>
2024-11-10 14:58:24 +0100

    Time: 0 ms (100.00%) | Memory: 2.9 MB (64.09%) - LeetSync

--- solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
+++ solutions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.rs
@@ -1,27 +1,13 @@
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        if prices.len() < 2 {
-            return 0;
-        }
-        
+        let mut min_price = i32::MAX;
         let mut max_profit = 0;
-        let mut left_buy = 0;
-        let mut right_sell = 1;
         
-        while right_sell < prices.len() {
-            let current_price = prices[right_sell];
-            let buy_price = prices[left_buy];
-            
-            if buy_price < current_price {
-                let current_profit = current_price - buy_price;
-                max_profit = max_profit.max(current_profit);
-            } else {
-                left_buy = right_sell;
-            }
-            
-            right_sell += 1;
+        for current_price in prices {
+            min_price = min_price.min(current_price);
+            max_profit = max_profit.max(current_price - min_price);
         }
         
-        max_profit
+        return max_profit;
     }
 }
