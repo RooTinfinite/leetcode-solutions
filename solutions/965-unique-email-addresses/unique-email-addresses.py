80a9989180eecfe5377ce1571fd2ee950bdfa370
Piotr M <filo74b@gmail.com>
2024-11-10 22:08:16 +0100

    Time: 4 ms (70.59%) | Memory: 16.9 MB (14.25%) - LeetSync

--- solutions/965-unique-email-addresses/unique-email-addresses.py
+++ solutions/965-unique-email-addresses/unique-email-addresses.py
@@ -1,9 +1,9 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        ret = set()
+        unique_emails = set()
         for email in emails:
             local, domain = email.split('@')
             local = local.split('+')[0]
             local = local.replace('.', '')
-            ret.add((local, domain))
-        return len(ret)
+            unique_emails.add((local, domain))
+        return len(unique_emails)
