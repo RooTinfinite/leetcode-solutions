33844b550b6dd83317df0eb11750534540f96f7a
Piotr M <filo74b@gmail.com>
2024-11-10 01:24:11 +0100

    Time: 9 ms (100.00%) | Memory: 69.8 MB (100.00%) - LeetSync

---
+++ solutions/3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int MinimumSubarrayLength(int[] nums, int k) {
+        if (k == 0) {
+            return 1;
+        }
+        int shortest = nums.Length + 1;
+        int[] count = new int[32];
+        int val = 0;
+        int start = 0;
+        
+        for (int i = 0; i < nums.Length; ++i) {
+            int num = nums[i];
+            val |= num;
+            for (int ibit = 0; num != 0; ++ibit) {
+                count[ibit] += num & 1;
+                num >>= 1;
+            }
+            while (val >= k && start < nums.Length) {
+                shortest = Math.Min(shortest, i - start + 1);
+                num = nums[start];
+                ++start;
+                for (int ibit = 0; num != 0; ++ibit) {
+                    count[ibit] -= num & 1;
+                    if (count[ibit] == 0) {
+                        val &= ~(1 << ibit);
+                    }
+                    num >>= 1;
+                }
+            }
+        }
+        return shortest == (nums.Length + 1) ? -1 : shortest;
+    }
+}
