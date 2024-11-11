1dfc3166c0e14980946d5ae9178dd53d7c1905f2
Piotr M <filo74b@gmail.com>
2024-11-11 04:37:27 +0100

    Time: 5 ms (93.85%) | Memory: 17 MB (5.34%) - LeetSync

--- solutions/2716-prime-subtraction-operation/prime-subtraction-operation.py
+++ solutions/2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -1,34 +1,13 @@
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021]
+
 class Solution:
-    def primeSubOperation(self, nums: list[int]) -> bool:
-        def getMaxElement(nums):
-            return max(nums)
-        
-        maxElement = getMaxElement(nums)
-        
-        # Create Sieve of Eratosthenes array to identify prime numbers
-        sieve = [True] * (maxElement + 1)
-        sieve[1] = False
-        
-        for i in range(2, int((maxElement + 1) ** 0.5) + 1):
-            if sieve[i]:
-                for j in range(i * i, maxElement + 1, i):
-                    sieve[j] = False
-        
-        # Check if array can be made strictly increasing by subtracting prime numbers
-        currValue = 1
-        i = 0
-        while i < len(nums):
-            difference = nums[i] - currValue
-            
-            # Return false if current number is already smaller than required value
-            if difference < 0:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        prev = 0
+        for num in nums:
+            if num <= prev:
                 return False
-            
-            # Move to next number if difference is prime or zero
-            if sieve[difference] == True or difference == 0:
-                i += 1
-                currValue += 1
-            else:
-                currValue += 1
-                
+            i = bisect_left(primes, num - prev) - 1
+            if i != -1:
+                num -= primes[i]
+            prev = num
         return True
