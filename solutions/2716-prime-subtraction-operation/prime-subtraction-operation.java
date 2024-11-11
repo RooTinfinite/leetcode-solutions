4cd30a768e585c0c485f554231c5ad923aac9080
Piotr M <filo74b@gmail.com>
2024-11-12 00:00:55 +0100

    Time: 2 ms (100.00%) | Memory: 44.8 MB (33.11%) - LeetSync

--- solutions/2716-prime-subtraction-operation/prime-subtraction-operation.java
+++ solutions/2716-prime-subtraction-operation/prime-subtraction-operation.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean primeSubOperation(int[] nums) {
         int maxElement = getMaxElement(nums);
 
-        // Create Sieve of Eratosthenes array to identify prime numbers
         boolean[] sieve = new boolean[maxElement + 1];
         fill(sieve, true);
         sieve[1] = false;
@@ -14,18 +13,15 @@ class Solution {
             }
         }
 
-        // Check if array can be made strictly increasing by subtracting prime numbers
         int currValue = 1;
         int i = 0;
         while (i < nums.length) {
             int difference = nums[i] - currValue;
 
-            // Return false if current number is already smaller than required value
             if (difference < 0) {
                 return false;
             }
 
-            // Move to next number if difference is prime or zero
             if (sieve[difference] == true || difference == 0) {
                 i++;
                 currValue++;
@@ -36,7 +32,6 @@ class Solution {
         return true;
     }
 
-    // Helper method to find maximum element in array
     private int getMaxElement(int[] nums) {
         int max = -1;
         for (int num : nums) {
@@ -47,7 +42,6 @@ class Solution {
         return max;
     }
 
-    // Helper method to initialize boolean array
     private void fill(boolean[] arr, boolean value) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = value;
