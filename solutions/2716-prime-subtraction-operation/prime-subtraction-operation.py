c358af20e11ce4f86c4ad2b9b9445fa592614b28
Piotr M <filo74b@gmail.com>
2024-11-11 02:32:04 +0100

    Time: 19 ms (81.01%) | Memory: 17 MB (12.98%) - LeetSync

--- solutions/2716-prime-subtraction-operation/prime-subtraction-operation.py
+++ solutions/2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -1,18 +1,39 @@
-valid = [True] * 1001
-valid[0] = valid[1] = False
-for i in range(2, len(valid)):
-    if valid[i]:
-        for j in range(i * i, len(valid), i):
-            valid[j] = False
-primes = [i for i in range(len(valid)) if valid[i]]
 class Solution:
-    def primeSubOperation(self, nums: List[int]) -> bool:
-        prev = 0
-        for num in nums:
-            if num <= prev:
+    def primeSubOperation(self, nums: list[int]) -> bool:
+        def getMaxElement(nums):
+            return max(nums)
+        
+        maxElement = getMaxElement(nums)
+        
+        # Store the sieve array
+        sieve = [True] * (maxElement + 1)
+        sieve[1] = False
+        
+        for i in range(2, int((maxElement + 1) ** 0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, maxElement + 1, i):
+                    sieve[j] = False
+        
+        # Start by storing the currValue as 1, and the initial index as 0
+        currValue = 1
+        i = 0
+        
+        while i < len(nums):
+            # Store the difference needed to make nums[i] equal to currValue
+            difference = nums[i] - currValue
+            
+            # If difference is less than 0, then nums[i] is already less than
+            # currValue. Return false in this case
+            if difference < 0:
                 return False
-            i = bisect_left(primes, num - prev) - 1
-            if i != -1:
-                num -= primes[i]
-            prev = num
+            
+            # If the difference is prime or zero, then nums[i] can be made
+            # equal to currValue
+            if sieve[difference] or difference == 0:
+                i += 1
+                currValue += 1
+            else:
+                # Otherwise, try for the next currValue
+                currValue += 1
+        
         return True
