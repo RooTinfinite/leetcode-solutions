66cedb9b58fbe88824ae74852c53c120b71bcd62
Piotr M <filo74b@gmail.com>
2024-11-11 12:57:59 +0100

    Time: 3 ms (91.77%) | Memory: 23.1 MB (30.86%) - LeetSync

--- solutions/56-merge-intervals/merge-intervals.cpp
+++ solutions/56-merge-intervals/merge-intervals.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
         sort(intervals.begin(), intervals.end());
 
-        vector<vector<int>> merged;
-        for (auto interval : intervals) {
-            // if the list of merged intervals is empty or if the current
-            // interval does not overlap with the previous, simply append it.
-            if (merged.empty() || merged.back()[1] < interval[0]) {
-                merged.push_back(interval);
-            }
-            // otherwise, there is overlap, so we merge the current and previous
-            // intervals.
-            else {
-                merged.back()[1] = max(merged.back()[1], interval[1]);
+        int start = intervals[0][0], end = intervals[0][1];
+        vector<vector<int>> ans;
+        for ( int i =1; i< n; i++){
+            if ( intervals[i][0] > end){
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else {
+                end = max(intervals[i][1], end);
             }
         }
-        return merged;
+        ans.push_back({start, end});
+
+        return ans;
     }
 };
