71476ff3a4a7a1f44006360524827de2e71d89f9
Piotr M <filo74b@gmail.com>
2024-11-11 05:02:32 +0100

    Time: 3 ms (25.00%) | Memory: 5.8 MB (100.00%) - LeetSync

--- solutions/2716-prime-subtraction-operation/prime-subtraction-operation.go
+++ solutions/2716-prime-subtraction-operation/prime-subtraction-operation.go
@@ -1,18 +1,6 @@
 func primeSubOperation(nums []int) bool {
-    maxElement := getMaxElement(nums)
-    
-    // Create Sieve of Eratosthenes array to identify prime numbers
-    sieve := make([]bool, maxElement+1)
-    fill(sieve, true)
-    sieve[1] = false
-    
-    for i := 2; i <= int(math.Sqrt(float64(maxElement+1))); i++ {
-        if sieve[i] {
-            for j := i * i; j <= maxElement; j += i {
-                sieve[j] = false
-            }
-        }
-    }
+    // Pre-defined list of prime numbers
+    primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013, 1019, 1021}
     
     // Check if array can be made strictly increasing by subtracting prime numbers
     currValue := 1
@@ -25,8 +13,21 @@ func primeSubOperation(nums []int) bool {
             return false
         }
         
-        // Move to next number if difference is prime or zero
-        if sieve[difference] == true || difference == 0 {
+        // Check if difference is prime by looking up in the pre-defined list
+        isPrime := difference == 0 // 0 is considered valid in this context
+        if !isPrime {
+            for _, prime := range primes {
+                if prime == difference {
+                    isPrime = true
+                    break
+                }
+                if prime > difference {
+                    break
+                }
+            }
+        }
+        
+        if isPrime {
             i++
             currValue++
         } else {
@@ -35,22 +36,3 @@ func primeSubOperation(nums []int) bool {
     }
     return true
 }
-
-// Helper function to find maximum element in array
-func getMaxElement(nums []int) int {
-    max := -1
-    for _, num := range nums {
-        if num > max {
-            max = num
-        }
-    }
-    return max
-}
-
-// Helper function to initialize boolean array
-func fill(arr []bool, value bool) {
-    for i := range arr {
-        arr[i] = value
-    }
-}
-
