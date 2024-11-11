8ab53f957a2ea171de8ddec4f40cc844cebbfc8d
Piotr M <filo74b@gmail.com>
2024-11-11 20:07:17 +0100

    Time: 3 ms (81.68%) | Memory: 17.6 MB (71.23%) - LeetSync

--- solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,23 @@
+from typing import List
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        hash_map = {}
-
-        for i in range(len(numbers)):
-            if ((target - numbers[i]) not in hash_map):
-                hash_map[numbers[i]] = i+1
-            else:       
-                return[hash_map[target - numbers[i]], i+1]
+        """
+        Find two numbers in the array that add up to the target.
+        
+        Args:
+            numbers: A list of integers
+            target: The target sum to find
+            
+        Returns:
+            List[int]: Indices (1-based) of the two numbers that add up to target
+        """
+        seen_numbers = {}  # Maps number to its index (1-based)
+        
+        for current_index, current_number in enumerate(numbers, 1):
+            complement = target - current_number
+            
+            if complement in seen_numbers:
+                return [seen_numbers[complement], current_index]
+                
+            seen_numbers[current_number] = current_index
