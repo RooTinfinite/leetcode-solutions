b3329267188b18c76eea1f0bbc586addc0e2546b
Piotr M <filo74b@gmail.com>
2024-11-11 05:43:55 +0100

    Time: 0 ms (100.00%) | Memory: 27.3 MB (80.19%) - LeetSync

--- solutions/2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ solutions/2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -1,37 +1,71 @@
 class Solution {
 public:
-    bool isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i <= sqrt(n); i++) {
-            if (n % i == 0) return false;
-        }
-        return true;
-    }
-    
-    int findLargestValidPrime(int num, int prev) {
-        for (int i = num - prev - 1; i >= 2; i--) {
-            if (isPrime(i) && num - i > prev) {
-                return i;
-            }
-        }
-        return 0;
-    }
-    
     bool primeSubOperation(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            int prev = (i == 0) ? 0 : nums[i-1];
-            
-            if (nums[i] <= prev) {
-                return false;
-            }
-            
-            int prime = findLargestValidPrime(nums[i], prev);
-            if (prime > 0) {
-                nums[i] -= prime;
+        int maxElement = getMaxElement(nums);
+        
+        // Create Sieve of Atkin array to identify prime numbers
+        vector<bool> sieve(maxElement + 1, false);
+        if (maxElement > 2) sieve[2] = true;
+        if (maxElement > 3) sieve[3] = true;
+        
+        // Implement Sieve of Atkin
+        for (int x = 1; x * x <= maxElement; x++) {
+            for (int y = 1; y * y <= maxElement; y++) {
+                int n = (4 * x * x) + (y * y);
+                if (n <= maxElement && (n % 12 == 1 || n % 12 == 5)) {
+                    sieve[n] = !sieve[n];
+                }
+                
+                n = (3 * x * x) + (y * y);
+                if (n <= maxElement && n % 12 == 7) {
+                    sieve[n] = !sieve[n];
+                }
+                
+                n = (3 * x * x) - (y * y);
+                if (x > y && n <= maxElement && n % 12 == 11) {
+                    sieve[n] = !sieve[n];
+                }
             }
         }
         
+        // Mark squares of primes as non-prime
+        for (int i = 5; i * i <= maxElement; i++) {
+            if (sieve[i]) {
+                for (int j = i * i; j <= maxElement; j += i * i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+        
+        // Check if array can be made strictly increasing by subtracting prime numbers
+        int currValue = 1;
+        int i = 0;
+        while (i < nums.size()) {
+            int difference = nums[i] - currValue;
+            
+            if (difference < 0) {
+                return false;
+            }
+            
+            if (sieve[difference] == true || difference == 0) {
+                i++;
+                currValue++;
+            } else {
+                currValue++;
+            }
+        }
         return true;
+    }
+    
+private:
+    int getMaxElement(vector<int>& nums) {
+        int max = -1;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 };
 
