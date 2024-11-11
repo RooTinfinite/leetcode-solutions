f2856de14c3c1dfc3df923b6f13d0b4e1d64507b
Piotr M <filo74b@gmail.com>
2024-11-11 01:45:42 +0100

    Time: 3 ms (97.77%) | Memory: 17 MB (12.98%) - LeetSync

--- solutions/2716-prime-subtraction-operation/prime-subtraction-operation.py
+++ solutions/2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -1,31 +1,23 @@
-class PrimeNumberUtils:
-    def __init__(self, max_size: int = 1001):
-        self.primes = self._generate_primes(max_size)
+valid = [True] * 1001
+valid[0] = valid[1] = False
+for i in range(2, len(valid)):
+    if valid[i]:
+        for j in range(i * i, len(valid), i):
+            valid[j] = False
+primes = [i for i in range(len(valid)) if valid[i]]
 
-    def _generate_primes(self, size: int) -> List[int]:
-        valid = [True] * size
-        valid[0] = valid[1] = False
-        
-        for i in range(2, size):
-            if valid[i]:
-                for j in range(i * i, size, i):
-                    valid[j] = False
-                    
-        return [i for i in range(size) if valid[i]]
 
 class Solution:
     def primeSubOperation(self, nums: List[int]) -> bool:
-        prime_utils = PrimeNumberUtils()
-        previous = 0
-        
-        for current in nums:
-            if current <= previous:
+        prev = 0
+        for num in nums:
+            if num <= prev:
                 return False
-            
-            prime_index = bisect_left(prime_utils.primes, current - previous) - 1
-            if prime_index != -1:
-                current -= prime_utils.primes[prime_index]
-                
-            previous = current
-            
+
+            i = bisect_left(primes, num - prev) - 1
+            if i != -1:
+                num -= primes[i]
+            prev = num
+
         return True
+                
