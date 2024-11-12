9c74fa761b6aeab1713f5a9b6e61713bfa019ab8
Piotr M <filo74b@gmail.com>
2024-11-12 02:45:41 +0100

    Time: 49 ms (75.41%) | Memory: 94.1 MB (62.84%) - LeetSync

--- solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        // Step 1: Sort items by price in ascending order
         sort(items.begin(), items.end());
-        
-        // Step 2: Extract prices and beauties into separate vectors
-        vector<int> prices, beauties;
-        for (const auto& item : items) {
-            prices.push_back(item[0]);
-            beauties.push_back(item[1]);
+        vector<pair<int, int>> queriesWithIndex;
+        for (int i = 0; i < queries.size(); i++) {
+            queriesWithIndex.push_back({queries[i], i});
         }
+        sort(queriesWithIndex.begin(), queriesWithIndex.end());
         
-        // Step 3: Create running maximum beauty array
-        vector<int> max_beauties(1, 0);
-        int curr_max = 0;
-        for (int beauty : beauties) {
-            curr_max = max(curr_max, beauty);
-            max_beauties.push_back(curr_max);
+        vector<int> res(queries.size());
+        int maxBea = 0;
+        int j = 0;
+        for (auto [q, i] : queriesWithIndex) {
+            while (j < items.size() && items[j][0] <= q) {
+                maxBea = max(maxBea, items[j][1]);
+                j++;
+            }
+            res[i] = maxBea;
         }
-        
-        // Step 4: Process each query using binary search
-        vector<int> result;
-        for (int query_price : queries) {
-            auto index = upper_bound(prices.begin(), prices.end(), query_price) - prices.begin();
-            result.push_back(max_beauties[index]);
-        }
-        
-        return result;
+        return res;
     }
 };
