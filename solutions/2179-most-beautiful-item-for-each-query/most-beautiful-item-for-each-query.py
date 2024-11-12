05232b3b798137c00c55bc3be8614260aec49955
Piotr M <filo74b@gmail.com>
2024-11-12 02:01:58 +0100

    Time: 147 ms (69.51%) | Memory: 65.8 MB (63.44%) - LeetSync

--- solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,24 +1,5 @@
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        items.sort(key=lambda x: x[0])
-        n = len(items)
-        max_beauties = [0] * n
-        max_beauties[0] = items[0][1]
-        
-        for i in range(1, n):
-            max_beauties[i] = max(max_beauties[i-1], items[i][1])
-        
-        def binary_search(target):
-            left, right = 0, len(items) - 1
-            result = -1
-            
-            while left <= right:
-                mid = left + (right - left) // 2
-                if items[mid][0] <= target:
-                    result = mid
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return result
-        
-        return [0 if binary_search(q) < 0 else max_beauties[binary_search(q)] for q in queries]
+        items.sort(key = itemgetter(0))
+        max_beauty = [*accumulate(map(itemgetter(1), items), max, initial = 0)]
+        return [max_beauty[bisect_right(items, q, key = itemgetter(0))] for q in queries]
