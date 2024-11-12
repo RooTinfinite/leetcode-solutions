80f7070955d995861ed708cf7dd222368e383026
Piotr M <filo74b@gmail.com>
2024-11-12 02:24:40 +0100

    Time: 92 ms (98.78%) | Memory: 66.3 MB (61.83%) - LeetSync

--- solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,7 +1,7 @@
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
         sorted_items = sorted(items, key=lambda item: item[0])  # Step 1: Sort items by price
-        
+
         prices = [item[0] for item in sorted_items] # Step 2: Extract prices and beauties
         beauties = [item[1] for item in sorted_items]
         
@@ -10,6 +10,5 @@ class Solution:
         result = []     # Step 4: Find maximum beauty for each query price
         for query_price in queries:
             index = bisect_right(prices, query_price)
-            result.append(max_beauties[index])
-            
+            result.append(max_beauties[index])  
         return result
