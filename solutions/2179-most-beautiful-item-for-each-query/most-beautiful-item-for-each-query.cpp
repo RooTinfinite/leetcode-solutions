8bdc7f11b0e2b8657505cee576d5ab373585264a
Piotr M <filo74b@gmail.com>
2024-11-12 01:28:56 +0100

    Time: 80 ms (22.13%) | Memory: 97.1 MB (42.91%) - LeetSync

--- solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ solutions/2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        sort(items.begin(), items.end());
-        
-        int n = items.size();
-        vector<int> maxBeauties(n);
-        maxBeauties[0] = items[0][1];
-        
-        for(int i = 1; i < n; i++) {
-            maxBeauties[i] = max(maxBeauties[i-1], items[i][1]);
+    static vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        sort(items.begin(), items.end()); // Sort items by price
+        const int n = items.size(), m = queries.size();
+
+        vector<int> mostBeauty(n);
+        mostBeauty[0] = items[0][1];
+
+        // Build up the mostBeauty array
+        for (int i = 1; i < n; i++)
+            mostBeauty[i] = max(mostBeauty[i-1], items[i][1]);
+
+        vector<int> ans(m);
+        for (int i = 0; i < m; i++) {
+            int query = queries[i];
+            vector<int>&& target={query, INT_MAX};
+            // Use upper_bound to find the first item with a price > query
+            int j = upper_bound(items.begin(), items.end(), target)-items.begin();
+            // If j is 0, no items have price <= query
+            if (j == 0) ans[i] = 0;
+            else ans[i] = mostBeauty[j - 1];
         }
-        
-        vector<int> result;
-        for(int query : queries) {
-            int idx = binarySearch(items, query);
-            result.push_back(idx < 0 ? 0 : maxBeauties[idx]);
-        }
-        
-        return result;
-    }
-    
-private:
-    int binarySearch(vector<vector<int>>& items, int target) {
-        int left = 0, right = items.size() - 1;
-        int result = -1;
-        
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(items[mid][0] <= target) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return result;
+
+        return ans;
     }
 };
