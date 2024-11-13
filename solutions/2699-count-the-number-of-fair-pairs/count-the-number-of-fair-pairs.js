8e897a8ce8a52fa3667041c3f222724d4890bcff
Piotr M <filo74b@gmail.com>
2024-11-13 13:36:53 +0100

    Time: 96 ms (100.00%) | Memory: 61 MB (91.30%) - LeetSync

---
+++ solutions/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.js
@@ -0,0 +1,21 @@
+var countFairPairs = function(nums, lower, upper) {
+    nums.sort((a, b) => a - b);
+    return countPairs(nums, upper) - countPairs(nums, lower - 1);
+};
+
+function countPairs(nums, target) {
+    let count = 0;
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+        if (nums[left] + nums[right] > target) {
+            right--;
+        } else {
+            count += right - left;
+            left++;
+        }
+    }
+    
+    return count;
+}
