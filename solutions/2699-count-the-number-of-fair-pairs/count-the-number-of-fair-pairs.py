d346888255d1ce3b01b60942980da17fd5c91e45
Piotr M <filo74b@gmail.com>
2024-11-13 14:45:10 +0100

    Time: 977 ms (24.41%) | Memory: 30.5 MB (91.73%) - LeetSync

--- solutions/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ solutions/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -1,13 +1,21 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        nums.sort()
-        ans = 0
-        for i in range(len(nums)-1, -1, -1):
-            v = nums.pop()
-            a = bisect_left(nums, lower - v)
-            b = bisect_right(nums, upper - v)
+        def bin_search(l, r, target):
+            while l <= r:
+                m = (l + r) // 2
+                if nums[m] >= target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return r
 
-            ans += b - a
-        
-        return ans
-          
+        nums.sort()
+        res = 0
+        for i in range(len(nums)):
+            low = lower - nums[i]
+            up = upper - nums[i]
+            res += (
+                bin_search(i + 1, len(nums) - 1, up + 1) -
+                bin_search(i + 1, len(nums) - 1, low)
+            )
+        return res
