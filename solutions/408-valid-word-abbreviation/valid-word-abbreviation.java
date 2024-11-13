bfe27be0fd9ea7873dd3400a2eeff7f311173c77
Piotr M <filo74b@gmail.com>
2024-11-13 17:35:38 +0100

    Time: 1 ms (96.27%) | Memory: 41.9 MB (33.44%) - LeetSync

--- solutions/408-valid-word-abbreviation/valid-word-abbreviation.java
+++ solutions/408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -1,21 +1,35 @@
 class Solution {
-    public boolean validWordAbbreviation(String word, String abbr) {
-        int i = 0, j = 0;
-        while (i < word.length() && j < abbr.length()) {
-            if (Character.isDigit(abbr.charAt(j))) {
-                if (abbr.charAt(j) == '0') return false;
-                int num = 0;
-                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
-                    num = num * 10 + (abbr.charAt(j) - '0');
-                    j++;
+    private int parseNumber(String abbreviation, int startPos) {
+        int number = 0;
+        while (startPos < abbreviation.length() && 
+               Character.isDigit(abbreviation.charAt(startPos))) {
+            number = number * 10 + (abbreviation.charAt(startPos) - '0');
+            startPos++;
+        }
+        return number;
+    }
+    
+    public boolean validWordAbbreviation(String word, String abbreviation) {
+        int wordPos = 0, abbrPos = 0;
+        
+        while (abbrPos < abbreviation.length() && wordPos < word.length()) {
+            if (Character.isLetter(abbreviation.charAt(abbrPos))) {
+                if (abbreviation.charAt(abbrPos) != word.charAt(wordPos)) {
+                    return false;
                 }
-                i += num;
+                wordPos++;
+                abbrPos++;
             } else {
-                if (word.charAt(i) != abbr.charAt(j)) return false;
-                i++;
-                j++;
+                if (abbreviation.charAt(abbrPos) == '0') {
+                    return false;
+                }
+                
+                int skipCount = parseNumber(abbreviation, abbrPos);
+                abbrPos += String.valueOf(skipCount).length();
+                wordPos += skipCount;
             }
         }
-        return i == word.length() && j == abbr.length();
+        
+        return abbrPos == abbreviation.length() && wordPos == word.length();
     }
 }
