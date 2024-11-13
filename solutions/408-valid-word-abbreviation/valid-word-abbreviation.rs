3f6f77177f6405559af2b8a0258cd4ce4aa6db95
Piotr M <filo74b@gmail.com>
2024-11-13 17:38:37 +0100

    Time: 0 ms (100.00%) | Memory: 2.1 MB (33.33%) - LeetSync

--- solutions/408-valid-word-abbreviation/valid-word-abbreviation.rs
+++ solutions/408-valid-word-abbreviation/valid-word-abbreviation.rs
@@ -1,25 +1,39 @@
 impl Solution {
-    pub fn valid_word_abbreviation(word: String, abbr: String) -> bool {
-        let word = word.as_bytes();
-        let abbr = abbr.as_bytes();
-        let mut i = 0;
-        let mut j = 0;
-       
-        while i < word.len() && j < abbr.len() {
-            if abbr[j].is_ascii_digit() {
-                if abbr[j] == b'0' { return false; }
-                let mut num = 0;
-                while j < abbr.len() && abbr[j].is_ascii_digit() {
-                    num = num * 10 + (abbr[j] - b'0') as usize;
-                    j += 1;
+    fn parse_number(abbreviation: &str, start_pos: usize) -> u32 {
+        let mut number = 0;
+        let chars: Vec<char> = abbreviation.chars().collect();
+        let mut pos = start_pos;
+        while pos < chars.len() && chars[pos].is_digit(10) {
+            number = number * 10 + chars[pos].to_digit(10).unwrap();
+            pos += 1;
+        }
+        number
+    }
+    
+    pub fn valid_word_abbreviation(word: String, abbreviation: String) -> bool {
+        let word_chars: Vec<char> = word.chars().collect();
+        let abbr_chars: Vec<char> = abbreviation.chars().collect();
+        let mut word_pos = 0;
+        let mut abbr_pos = 0;
+        
+        while abbr_pos < abbr_chars.len() && word_pos < word_chars.len() {
+            if abbr_chars[abbr_pos].is_alphabetic() {
+                if abbr_chars[abbr_pos] != word_chars[word_pos] {
+                    return false;
                 }
-                i += num;
+                word_pos += 1;
+                abbr_pos += 1;
             } else {
-                if word[i] != abbr[j] { return false; }
-                i += 1;
-                j += 1;
+                if abbr_chars[abbr_pos] == '0' {
+                    return false;
+                }
+                
+                let skip_count = Self::parse_number(&abbreviation, abbr_pos) as usize;
+                abbr_pos += skip_count.to_string().len();
+                word_pos += skip_count;
             }
         }
-        i == word.len() && j == abbr.len()
+        
+        abbr_pos == abbr_chars.len() && word_pos == word_chars.len()
     }
 }
