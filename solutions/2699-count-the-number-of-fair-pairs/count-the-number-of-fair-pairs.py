8014dd4e8d649598f75d0fba110c01b43ab98968
Piotr M <filo74b@gmail.com>
2024-11-13 13:40:30 +0100

    Time: 103 ms (94.64%) | Memory: 30.6 MB (91.73%) - LeetSync

--- solutions/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ solutions/2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -1,18 +1,18 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
-        return self.count_less_than(nums, upper + 1) - self.count_less_than(nums, lower)
-    
-    def count_less_than(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        count = 0
         
-        while left < right:
-            curr_sum = nums[left] + nums[right]
-            if curr_sum < target:
-                count += right - left
-                left += 1
-            else:
-                right -= 1
-                
-        return count
+        def countPairs(target):
+            left, right = 0, len(nums) - 1
+            count = 0
+            
+            while left < right:
+                curr_sum = nums[left] + nums[right]
+                if curr_sum <= target:
+                    count += right - left
+                    left += 1
+                else:
+                    right -= 1
+            return count
+        
+        return countPairs(upper) - countPairs(lower - 1)
