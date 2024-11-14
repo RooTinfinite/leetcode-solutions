a1a3899d2fbd125a02d48302f182f30d53aa5f4f
Piotr M <filo74b@gmail.com>
2024-11-14 10:54:33 +0100

    Time: 43 ms (58.05%) | Memory: 87.3 MB (34.13%) - LeetSync

--- solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,34 +1,33 @@
-
 class Solution {
 public:
-    int minimizedMaximum(int storeCount, vector<int>& productQuantities) {
-        int maxQuantity = *max_element(productQuantities.begin(), productQuantities.end());
-        int left = 1;
-        int right = maxQuantity;
-        int result = 0;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canDistributeProducts(mid, storeCount, productQuantities)) {
-                result = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+    bool isPossible(int target, int n, vector<int> &q){
+        int sz = q.size();
+        int cnt = 0;
+        for(int i=0;i<sz;i++){
+            int div = q[i]/target;
+            int rem = q[i] % target;
+            cnt += div;
+            if(rem > 0) cnt ++;
         }
-        
-        return result;
-    }
+        return cnt <= n;
 
-private:
-    bool canDistributeProducts(int maxProductsPerStore, int storeCount, const vector<int>& quantities) {
-        int requiredStores = 0;
-        
-        for (int quantity : quantities) {
-            // Calculate stores needed for current product type
-            requiredStores += (quantity + maxProductsPerStore - 1) / maxProductsPerStore;
+    }
+    int minimizedMaximum(int n, vector<int>& q) {
+        int sz = q.size();
+        int mx = 0;
+        for(int i=0;i<sz;i++) {
+            mx = max(mx, q[i]);
         }
-        
-        return requiredStores <= storeCount;
+        int lo = 1, hi = mx;
+        int ans = 0;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(isPossible(mid,n,q)) {
+                ans = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        return ans;
     }
 };
