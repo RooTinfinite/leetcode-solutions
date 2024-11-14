3a4daa7598d083da7b3947553b8c5cea0c9e3f91
Piotr M <filo74b@gmail.com>
2024-11-14 13:20:56 +0100

    Time: 4 ms (91.81%) | Memory: 24.1 MB (39.22%) - LeetSync

--- solutions/359-logger-rate-limiter/logger-rate-limiter.py
+++ solutions/359-logger-rate-limiter/logger-rate-limiter.py
@@ -1,18 +1,17 @@
 class Logger:
 
     def __init__(self):
-        self.history = set()  # set of past messages younger than 10 timesteps
-        self.queue = list()  # ordered list of messages younger than 10 timesteps
+        self.log={}
+        
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        q = self.queue
-        h = self.history
-        while self.queue and self.queue[0][0] <= timestamp - 10:
-            self.history.remove(self.queue[0][1])
-            self.queue.pop(0)
-        if message in self.history:
-            return False
-        else:
-            self.history.add(message)
-            self.queue.append((timestamp, message))
+        if message not in self.log:
+            self.log[message]=timestamp+10
             return True
+        else:
+            if timestamp>=self.log[message]:
+                self.log[message]=timestamp+10
+                return True
+
+            else:
+                return False
