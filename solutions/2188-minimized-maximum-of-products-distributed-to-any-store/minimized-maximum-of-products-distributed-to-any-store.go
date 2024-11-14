3a5da2340437bc215d8ecc88e65e4201bb1e8910
Piotr M <filo74b@gmail.com>
2024-11-14 11:43:21 +0100

    Time: 21 ms (100.00%) | Memory: 10 MB (63.64%) - LeetSync

--- solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.go
+++ solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.go
@@ -1,34 +1,29 @@
-func minimizedMaximum(storeCount int, productQuantities []int) int {
-    maxQuantity := 0
-    for _, q := range productQuantities {
-        if q > maxQuantity {
-            maxQuantity = q
+func minimizedMaximum(n int, quantities []int) int {
+    canDistribute := func(x int) bool {
+        stores := 0
+        for _, q := range quantities {
+            stores += (q + x - 1) / x
+        }
+        return stores <= n
+    }
+    
+    left, right := 1, 0
+    for _, q := range quantities {
+        if q > right {
+            right = q
         }
     }
     
-    left := 1
-    right := maxQuantity
     result := 0
-    
     for left <= right {
-        mid := left + (right-left)/2
-        if canDistributeProducts(mid, storeCount, productQuantities) {
-            result = mid
-            right = mid - 1
+        x := (left + right) / 2
+        if canDistribute(x) {
+            result = x
+            right = x - 1
         } else {
-            left = mid + 1
+            left = x + 1
         }
     }
     
     return result
-}
-
-func canDistributeProducts(maxProductsPerStore int, storeCount int, quantities []int) bool {
-    requiredStores := 0
-    
-    for _, quantity := range quantities {
-        requiredStores += (quantity + maxProductsPerStore - 1) / maxProductsPerStore
-    }
-    
-    return requiredStores <= storeCount
 }
