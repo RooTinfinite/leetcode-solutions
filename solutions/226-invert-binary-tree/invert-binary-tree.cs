71ac94ca84f0131146655162c61199be6ae470f0
Piotr M <filo74b@gmail.com>
2024-11-14 20:34:34 +0100

    Time: 0 ms (100.00%) | Memory: 40.7 MB (26.51%) - LeetSync

---
+++ solutions/226-invert-binary-tree/invert-binary-tree.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        
+        TreeNode temp = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(temp);
+        
+        return root;
+    }
+}
