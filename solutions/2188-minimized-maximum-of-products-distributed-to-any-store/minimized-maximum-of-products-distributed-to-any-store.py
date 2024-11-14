2f66cdae34204065d65a1302a29785bdad61b492
Piotr M <filo74b@gmail.com>
2024-11-14 11:34:04 +0100

    Time: 581 ms (64.25%) | Memory: 28.7 MB (73.44%) - LeetSync

--- solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.py
+++ solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.py
@@ -1,30 +1,19 @@
 class Solution:
-    def _solve_with_bin_search_by_value(self, n: int, q: List[int]) -> int:
-        q.sort(reverse=True)
-        m = len(q)
-        left, right = 1, q[0]
-        res = right
-        while left <= right:
-            free_slots = n - m
-            mid = (left + right) // 2
-            i = 0
-            while i < m and free_slots >= 0:
-                slots = math.ceil(q[i] / mid) - 1
-                if not slots:
-                    break
-                free_slots -= slots
-                i += 1
-            if free_slots < 0: #not enough slots
-                left = mid + 1
-            else:
-                right = mid - 1
-                res = mid
-        
-        return res
-            
-
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        if n == len(quantities):
-            return max(quantities)
+        def can_distribute(x):
+            stores = 0
+            for q in quantities:
+                stores += math.ceil(q / x)
+            return stores <= n
 
-        return self._solve_with_bin_search_by_value(n, quantities)
+        l, r = 1, max(quantities)
+        res = 0
+        while l <= r:
+            x = (l + r) // 2
+            if can_distribute(x):
+                res = x
+                r = x - 1
+            else:
+                l = x + 1
+
+        return res
