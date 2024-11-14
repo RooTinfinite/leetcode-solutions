e3b7e6a5ddcbdfd3a2ab72cdf6c5b0bfce6c2184
Piotr M <filo74b@gmail.com>
2024-11-14 12:32:42 +0100

    Time: 24 ms (8.36%) | Memory: 21.6 MB (78.34%) - LeetSync

--- solutions/907-koko-eating-bananas/koko-eating-bananas.cpp
+++ solutions/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1;
-        int high = *max_element(piles.begin(), piles.end());
-        int ans = high;  // Initialize ans with the maximum possible value
-
-        while (l <= high) {
-            int mid = l + (high - l) / 2;
-            if (canEatAll(piles, h, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                l = mid + 1;
-            }
+    int getH(vector<int>& pile,int k){
+        int h = 0;
+        for (int i = 0; i < pile.size(); ++i) {
+            h += pile[i]/k;
+            if(pile[i]%k != 0)h++;
         }
-        return ans;
+        return h;
     }
 
-private:
-    bool canEatAll(const vector<int>& piles, int h, int k) {
-        long long hours = 0;  // Use long long to store the number of hours
-
-        for (int pile : piles) {
-            hours += (pile + k - 1) / k;  // This is equivalent to ceil(pile / k)
-            if (hours > h) {
-                return false;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int high = *max_element(piles.begin(), piles.end());
+        int low = 1,mid;
+        while(low<=high){
+            mid = (high - low)/2 + low;
+            if(getH(piles,mid) <= h && (mid == 1 || getH(piles,mid-1) > h)){
+                return mid;
             }
+            if(getH(piles,mid) > h){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+
         }
-        return true;
+        return -1;
+
     }
 };
