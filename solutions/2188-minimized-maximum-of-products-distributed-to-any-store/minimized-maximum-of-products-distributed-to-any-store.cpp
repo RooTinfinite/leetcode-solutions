bf515b264d89316b53ddf08ecf6abd43194cb00d
Piotr M <filo74b@gmail.com>
2024-11-14 10:43:50 +0100

    Time: 1704 ms (5.99%) | Memory: 113.7 MB (12.82%) - LeetSync

--- solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ solutions/2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    static inline bool f(int x, vector<int>& quantities, int n){
-        int sum=0;
-        for(int a: quantities)
-            sum+=(a+x-1)/x;// ceil(a/x)
-        return sum>n;
-    }
-    static int minimizedMaximum(int n, vector<int>& quantities) {
-        int l=1, r=100000, m;
-        while(l<r){
-            m=(l+r)/2;
-            if (f(m, quantities, n)) l=m+1;
-            else r=m;
-        } 
-        return l;
-        
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int m = quantities.size();
+
+        // Define a custom comparator for the priority queue
+        // It sorts the pairs based on the ratio of their first to their second
+        // element
+        auto compareTypeStorePairs = [](pair<int, int>& a, pair<int, int>& b) {
+            return (long long)a.first * b.second <
+                   (long long)a.second * b.first;
+        };
+
+        // Helper array - useful for the efficient initialization of the
+        // priority queue
+        vector<pair<int, int>> typeStorePairsArray;
+
+        // Push all product types to the array, after assigning one store to
+        // each of them
+        for (int i = 0; i < m; i++) {
+            typeStorePairsArray.push_back({quantities[i], 1});
+        }
+
+        // Initialize the priority queue
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       decltype(compareTypeStorePairs)>
+            typeStorePairs(typeStorePairsArray.begin(),
+                           typeStorePairsArray.begin() + m);
+
+        // Iterate over the remaining n - m stores.
+        for (int i = 0; i < n - m; i++) {
+            // Pop first element
+            pair<int, int> pairWithMaxRatio = typeStorePairs.top();
+            int totalQuantityOfType = pairWithMaxRatio.first;
+            int storesAssignedToType = pairWithMaxRatio.second;
+            typeStorePairs.pop();
+
+            // Push same element after assigning one more store to its product
+            // type
+            typeStorePairs.push(
+                {totalQuantityOfType, storesAssignedToType + 1});
+        }
+
+        // Pop first element
+        pair<int, int> pairWithMaxRatio = typeStorePairs.top();
+        int totalQuantityOfType = pairWithMaxRatio.first;
+        int storesAssignedToType = pairWithMaxRatio.second;
+
+        // Return the maximum minimum ratio
+        return ceil((double)totalQuantityOfType / storesAssignedToType);
     }
 };
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
