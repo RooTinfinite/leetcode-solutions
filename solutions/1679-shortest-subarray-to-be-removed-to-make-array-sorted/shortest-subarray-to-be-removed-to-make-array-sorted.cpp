72b03e6cc194349529f9e3f28d9517bef0bd71b6
Piotr M <filo74b@gmail.com>
2024-11-15 09:44:57 +0100

    Time: 0 ms (100.00%) | Memory: 69.6 MB (66.98%) - LeetSync

--- solutions/1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ solutions/1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     int findLengthOfShortestSubarray(vector<int>& arr) {
-        int right = arr.size() - 1;
+        int n = arr.size();
         
-        while (right > 0 && arr[right] >= arr[right - 1]) {
-            right--;
+        if (n == 1) {
+            return 0;
         }
         
-        int ret = right;
+        // Find the longest non-decreasing prefix
         int left = 0;
-        
-        while (left < right && (left == 0 || arr[left - 1] <= arr[left])) {
-            while (right < arr.size() && arr[left] > arr[right]) {
-                right++;
-            }
-            
-            ret = min(ret, right - left - 1);
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
             left++;
         }
         
-        return ret;
+        if (left == n - 1) {  // Array is already non-decreasing
+            return 0;
+        }
+        
+        // Find the longest non-decreasing suffix
+        int right = n - 1;
+        while (right > 0 && arr[right - 1] <= arr[right]) {
+            right--;
+        }
+        
+        // Initial result: remove everything between left and right
+        int result = min(n - left - 1, right);
+        
+        // Try to merge prefix and suffix
+        int i = 0, j = right;
+        while (i <= left && j < n) {
+            if (arr[i] <= arr[j]) {
+                result = min(result, j - i - 1);
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return result;
     }
 };
