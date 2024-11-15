2870142b7713a44aa915fdc56fea0806e6e593f9
Piotr M <filo74b@gmail.com>
2024-11-15 10:55:42 +0100

    Time: 61 ms (99.92%) | Memory: 18 MB (96.08%) - LeetSync

--- solutions/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ solutions/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,58 +1,96 @@
-class UnionFind:
-    def __init__(self, size: int) -> None:
-        self.group = [0] * size
-        self.rank = [0] * size
-        
-        for i in range(size):
-            self.group[i] = i
-      
-    def find(self, node: int) -> int:
-        if self.group[node] != node:
-            self.group[node] = self.find(self.group[node])
-        return self.group[node]
-
-    def join(self, node1: int, node2: int) -> bool:
-        group1 = self.find(node1)
-        group2 = self.find(node2)
-        
-        # node1 and node2 already belong to same group.
-        if group1 == group2:
-            return False
-
-        if self.rank[group1] > self.rank[group2]:
-            self.group[group2] = group1
-        elif self.rank[group1] < self.rank[group2]:
-            self.group[group1] = group2
-        else:
-            self.group[group1] = group2
-            self.rank[group2] += 1
-
-        return True
-    
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        all_edges = []
+        if n == 1:
+            return 0
+        edges = build_edges(points)
+        return kruskal(edges, n)
+
+def kruskal(edges: List[Tuple[int, int, int]], n: int):
+    uf = UnionFind(n)
+    return sum(cost for cost, a, b in edges if uf.union(a, b))
+
+def build_edges(points: List[List[int]]):
+    edges = []
+    n = len(points)
+
+    def intercept(id: int):
+        x, y = points[id]
+        return y - x, x
+
+    for iteration in range(4):
+        if iteration & 1:
+            for point in points:
+                point.reverse()
+        elif iteration:
+            for point in points:
+                point[0] = -point[0]
         
-        # Storing all edges of our complete graph.
-        for curr_node in range(n): 
-            for next_node in range(curr_node + 1, n): 
-                weight = abs(points[curr_node][0] - points[next_node][0]) +\
-                         abs(points[curr_node][1] - points[next_node][1])
-                all_edges.append((weight, curr_node, next_node))
-      
+        sorted_by_x = sorted(enumerate(points), key=itemgetter(1), reverse=True)
+        sorted_by_intercept = sorted(range(n), key=intercept, reverse=True)
+        rank_lookup = [0] * n
+        for rank, point_id in enumerate(sorted_by_intercept, 1):
+            rank_lookup[point_id] = rank
         
-        # Sort all edges in increasing order.
-        all_edges.sort()
-        
-        uf = UnionFind(n)
-        mst_cost = 0
-        edges_used = 0
-        
-        for weight, node1, node2 in all_edges:
-            if uf.join(node1, node2):
-                mst_cost += weight
-                edges_used += 1
-                if edges_used == n - 1:
-                    break
-        return mst_cost
+        bit = BIT(n)
+        for point_id, (x, y) in sorted_by_x:
+            rank = rank_lookup[point_id]
+            sw_most = bit.query(rank)
+            if sw_most != -1:
+                sw_x, sw_y = points[sw_most]
+                dist = abs(x - sw_x) + abs(y - sw_y)
+                edges.append((dist, point_id, sw_most))
+            bit.update(rank, x + y, point_id)
+    edges.sort()
+    return edges
+
+class BIT:
+    def __init__(self, n: int):
+        self.num = [(inf, -1) for _ in range(n + 1)]
+        self.n = n
+    
+    def query(self, max_rank: int):
+        min_dist = inf
+        res = -1
+        while max_rank:
+            curr_dist, curr_num = self.num[max_rank]
+            if curr_dist < min_dist:
+                min_dist = curr_dist
+                res = curr_num
+            max_rank -= max_rank & -max_rank
+        return res
+
+    def update(self, rank: int, dist: int, id: int):
+        while rank <= self.n:
+            curr_dist, _ = self.num[rank]
+            if dist >= curr_dist:
+                break
+            self.num[rank] = dist, id
+            rank += rank & -rank
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.root_lookup = list(range(n))
+        self.rank_lookup = [1] * n
+    
+    def find(self, num: int):
+        root = self.root_lookup[num]
+        path = [num]
+        while root != self.root_lookup[root]:
+            path.append(root)
+            root = self.root_lookup[root]
+        for node in path:
+            self.root_lookup[node] = root
+        return root
+
+    def union(self, num1: int, num2: int):
+        root1, root2 = self.find(num1), self.find(num2)
+        if root1 == root2:
+            return False
+        rank1, rank2 = self.rank_lookup[root1], self.rank_lookup[root2]
+        if rank1 < rank2:
+            root1, root2 = root2, root1
+            rank2 = rank1
+        self.root_lookup[root2] = root1
+        self.rank_lookup[root1] += rank2
+        return True
