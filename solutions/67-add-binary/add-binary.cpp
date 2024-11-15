8d16afb46afc683cd2aa83a2d9a497bfb1d63ed3
Piotr M <filo74b@gmail.com>
2024-11-15 10:33:14 +0100

    Time: 0 ms (100.00%) | Memory: 8.2 MB (90.70%) - LeetSync

--- solutions/67-add-binary/add-binary.cpp
+++ solutions/67-add-binary/add-binary.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n = a.size(), m = b.size();
-        if (n < m)
-            return addBinary(b, a);  // Ensure 'a' is always the longer string
-
-        string result;
-        int carry = 0, j = m - 1;
-
-        for (int i = n - 1; i >= 0; --i) {
-            if (a[i] == '1') ++carry;
-            if (j >= 0 && b[j--] == '1') ++carry;
-
-            result.push_back((carry % 2) ? '1' : '0');
-            carry /= 2;
+        if(a.size()<b.size()) {
+            swap(a,b);
         }
+        int i, j;
+        for(i = b.size()-1, j = a.size()-1; i>=0; i--, j--) {
+            a[j]+=b[i]-'0';
+            if(j==0) {
+                break;
+            }
+            while(a[j]>'1') {
+                a[j-1]+=1;
+                a[j]-=2;
+            }
+        }
+        for(;j>0; j--) {
+            while(a[j]>'1') {
+                a[j-1]+=1;
+                a[j]-=2;
+            }
+        }
+        if(a[0]!='0' && a[0]!='1') {
+            a[0]-=2;
+            a = '1'+a;
 
-        if (carry == 1) result.push_back('1');  // Handle the last carry
-
-        reverse(result.begin(),
-                result.end());  // The result is built in reverse
-        return result;
+        }
+        return a;
     }
 };
