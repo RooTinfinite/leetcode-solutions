619a03962e514fc7f8a18aa86d704de77e20709e
Piotr M <filo74b@gmail.com>
2024-11-18 14:14:02 +0100

    Time: 0 ms (100.00%) | Memory: 10.2 MB (96.59%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,46 +1,59 @@
-#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
-#define INL __attribute__((always_inline))
-#define HOT __attribute__((hot))
-
 class Solution {
 private:
-    static uint mod1(const uint i, const uint n) INL NO_SAN HOT {
-        return i - (-(i >= n) & n);
+    static int mod1(int i, int n) {
+        return i - ((-(i >= n ? 1 : 0)) & n);
     }
-
+    
 public:
-    static vector<int> decrypt(vector<int> &code, const int8_t k) INL NO_SAN HOT {
-        const uint8_t n = code.size();
+    static vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        
         if (k < 0) {
-            uint s = reduce(code.cbegin() + (n + k), code.cend());
-            transform(code.cbegin(), code.cend(), code.begin(), [](const uint8_t v) INL NO_SAN HOT { return v << 16; });
-            for (uint8_t i = 0; i < n; i++) {
-                const uint8_t v = code[i] >> 16;
+            int s = 0;
+            for (int i = n + k; i < n; i++) {
+                s += code[i];
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] <<= 16;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                int v = code[i] >> 16;
                 code[i] += s;
-                const uint8_t j = mod1(n + k + i, n);
+                int j = mod1(n + k + i, n);
                 s += v - (code[j] >> 16);
             }
-            transform(code.cbegin(), code.cend(), code.begin(), [](const uint v) INL NO_SAN HOT { return v & 0x3FFF; });
+            
+            for (int i = 0; i < n; i++) {
+                code[i] &= 0x3FFF;
+            }
+            
         } else if (k > 0) {
-            uint s = reduce(code.cbegin(), code.cbegin() + k);
-            transform(code.cbegin(), code.cend(), code.begin(), [](const uint8_t v) INL NO_SAN HOT { return v << 16; });
-            for (uint8_t i = 0; i < n; i++) {
-                const uint8_t v = code[i] >> 16;
-                const uint8_t j = mod1(i + k, n);
+            int s = 0;
+            for (int i = 0; i < k; i++) {
+                s += code[i];
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] <<= 16;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                int v = code[i] >> 16;
+                int j = mod1(i + k, n);
                 s += (code[j] >> 16) - v;
                 code[i] += s;
             }
-            transform(code.cbegin(), code.cend(), code.begin(), [](const uint v) INL NO_SAN HOT { return v & 0x3FFF; });
+            
+            for (int i = 0; i < n; i++) {
+                code[i] &= 0x3FFF;
+            }
+            
         } else {
             fill(code.begin(), code.end(), 0);
         }
-        return move(code);
+        
+        return code;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
