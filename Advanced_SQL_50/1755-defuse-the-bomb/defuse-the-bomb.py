a5d31462cec754fea0fd9ee9be990497f4b135f1
Piotr M <filo74b@gmail.com>
2024-11-18 16:26:59 +0100

    Time: 0 ms (100.00%) | Memory: 17 MB (5.59%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,13 +1,9 @@
-from typing import List
-from functools import reduce
-
 class Solution:
     @staticmethod
     def mod1(i: int, n: int) -> int:
         return i - (-(i >= n) & n)
-
-    @staticmethod
-    def decrypt(code: List[int], k: int) -> List[int]:
+    
+    def decrypt(self, code: list[int], k: int) -> list[int]:
         n = len(code)
         if k < 0:
             s = sum(code[n + k:])
@@ -15,7 +11,7 @@ class Solution:
             for i in range(n):
                 v = code[i] >> 16
                 code[i] += s
-                j = Solution.mod1(n + k + i, n)
+                j = self.mod1(n + k + i, n)
                 s += v - (code[j] >> 16)
             code = [v & 0x3FFF for v in code]
         elif k > 0:
@@ -23,14 +19,10 @@ class Solution:
             code = [v << 16 for v in code]
             for i in range(n):
                 v = code[i] >> 16
-                j = Solution.mod1(i + k, n)
+                j = self.mod1(i + k, n)
                 s += (code[j] >> 16) - v
                 code[i] += s
             code = [v & 0x3FFF for v in code]
         else:
             code = [0] * n
         return code
-
-# Initialize faster I/O
-import sys
-input = sys.stdin.readline
