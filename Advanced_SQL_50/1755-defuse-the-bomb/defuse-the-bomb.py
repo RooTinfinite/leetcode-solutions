987709a2d3de9fb3f58cf3ca6415283f95402edf
Piotr M <filo74b@gmail.com>
2024-11-18 16:35:03 +0100

    Time: 0 ms (100.00%) | Memory: 16.8 MB (5.59%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,28 +1,43 @@
 class Solution:
     @staticmethod
     def mod1(i: int, n: int) -> int:
-        return i - (-(i >= n) & n)
+        return i - ((-(1 if i >= n else 0)) & n)
     
-    def decrypt(self, code: list[int], k: int) -> list[int]:
+    @staticmethod
+    def decrypt(code: list[int], k: int) -> list[int]:
         n = len(code)
+        
         if k < 0:
-            s = sum(code[n + k:])
-            code = [v << 16 for v in code]
+            s = sum(code[i] for i in range(n + k, n))
+            
+            for i in range(n):
+                code[i] <<= 16
+            
             for i in range(n):
                 v = code[i] >> 16
                 code[i] += s
-                j = self.mod1(n + k + i, n)
+                j = Solution.mod1(n + k + i, n)
                 s += v - (code[j] >> 16)
-            code = [v & 0x3FFF for v in code]
+            
+            for i in range(n):
+                code[i] &= 0x3FFF
+                
         elif k > 0:
-            s = sum(code[:k])
-            code = [v << 16 for v in code]
+            s = sum(code[i] for i in range(k))
+            
+            for i in range(n):
+                code[i] <<= 16
+            
             for i in range(n):
                 v = code[i] >> 16
-                j = self.mod1(i + k, n)
+                j = Solution.mod1(i + k, n)
                 s += (code[j] >> 16) - v
                 code[i] += s
-            code = [v & 0x3FFF for v in code]
+            
+            for i in range(n):
+                code[i] &= 0x3FFF
+                
         else:
             code = [0] * n
+            
         return code
