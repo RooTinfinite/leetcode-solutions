f7164671ab8cbd71cbd6da757c737a7fdd866a2d
Piotr M <filo74b@gmail.com>
2024-11-18 15:37:18 +0100

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.07%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -2,25 +2,20 @@ class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
-        vector<int> result(n, 0);
+        vector<int> result(n);
+
         if (k == 0) return result;
-        
-        if (k > 0) {
-            int windowSum = accumulate(code.begin() + 1, code.begin() + k + 1, 0);
-            result[0] = windowSum;
-            for (int i = 1; i < n; i++) {
-                windowSum += -code[i] + code[(i + k) % n];
-                result[i] = windowSum;
-            }
-        } else {
-            k = -k;
-            int windowSum = accumulate(code.end() - k, code.end(), 0);
-            result[0] = windowSum;
-            for (int i = 1; i < n; i++) {
-                windowSum += -code[(n - k + i - 1) % n] + code[i - 1];
-                result[i] = windowSum;
-            }
+
+        int windowSum = 0; int start = (k > 0) ? 1 : n + k; int end = (k > 0) ? k : n - 1;
+        for (int i = start; i <= end; i++) {
+            windowSum += code[i % n];
         }
+        for (int i = 0; i < n; i++) {
+            result[i] = windowSum;
+            windowSum -= code[(start + i) % n];
+            windowSum += code[(end + i + 1) % n];
+        }
+        
         return result;
     }
 };
