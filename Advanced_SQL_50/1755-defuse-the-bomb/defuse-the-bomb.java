235fa71c5476abc430e7226ec5d0d91c7ed33246
Piotr M <filo74b@gmail.com>
2024-11-18 14:12:44 +0100

    Time: 0 ms (100.00%) | Memory: 42.2 MB (55.38%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.java
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,32 +1,57 @@
 class Solution {
-    public int[] decrypt(int[] code, int k) {
-        int N = code.length;
-        int[] res = new int[N];
+    private static int mod1(int i, int n) {
+        return i - ((-(i >= n ? 1 : 0)) & n);
+    }
+
+    public static int[] decrypt(int[] code, int k) {
+        int n = code.length;
         
-        if (k == 0) {
-            return res;
-        }
-        
-        int l = 0;
-        int curSum = 0;
-        
-        for (int r = 0; r < N + Math.abs(k); r++) {
-            curSum += code[r % N];
-            
-            if (r - l + 1 > Math.abs(k)) {
-                curSum -= code[l % N];
-                l = (l + 1) % N;
+        if (k < 0) {
+            int s = 0;
+            for (int i = n + k; i < n; i++) {
+                s += code[i];
             }
             
-            if (r - l + 1 == Math.abs(k)) {
-                if (k > 0) {
-                    res[(l - 1 + N) % N] = curSum;
-                } else {
-                    res[(r + 1) % N] = curSum;
-                }
+            for (int i = 0; i < n; i++) {
+                code[i] <<= 16;
             }
+            
+            for (int i = 0; i < n; i++) {
+                int v = code[i] >> 16;
+                code[i] += s;
+                int j = mod1(n + k + i, n);
+                s += v - (code[j] >> 16);
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] &= 0x3FFF;
+            }
+            
+        } else if (k > 0) {
+            int s = 0;
+            for (int i = 0; i < k; i++) {
+                s += code[i];
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] <<= 16;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                int v = code[i] >> 16;
+                int j = mod1(i + k, n);
+                s += (code[j] >> 16) - v;
+                code[i] += s;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] &= 0x3FFF;
+            }
+            
+        } else {
+            Arrays.fill(code, 0);
         }
         
-        return res;
+        return code;
     }
 }
