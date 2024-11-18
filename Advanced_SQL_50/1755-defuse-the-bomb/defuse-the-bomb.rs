07273ebaf63797eb420ebd1f546965fdaee3b164
Piotr M <filo74b@gmail.com>
2024-11-18 16:49:58 +0100

    Time: 0 ms (100.00%) | Memory: 2.2 MB (61.11%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.rs
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.rs
@@ -1,19 +1,58 @@
 impl Solution {
-    pub fn decrypt(code: Vec<i32>, k: i32) -> Vec<i32> {
-        if k == 0{
-            return code.iter().map(|_| 0).collect();
-        }
+    fn mod1(i: i32, n: i32) -> i32 {
+        i - ((-(if i >= n { 1 } else { 0 })) & n)
+    }
 
-        let mut k = k;
-        let mut code = code;
+    pub fn decrypt(mut code: Vec<i32>, k: i32) -> Vec<i32> {
+        let n = code.len() as i32;
+        
         if k < 0 {
-            code.reverse();
-            k = -k;
-            (0..code.len()).map(|i| code.iter().cycle().skip(i+1).take(k as usize).sum()).rev().collect()
+            let mut s = 0;
+            for i in (n + k)..n {
+                s += code[i as usize];
+            }
+            
+            for i in 0..n {
+                code[i as usize] <<= 16;
+            }
+            
+            for i in 0..n {
+                let v = code[i as usize] >> 16;
+                code[i as usize] += s;
+                let j = Self::mod1(n + k + i, n);
+                s += v - (code[j as usize] >> 16);
+            }
+            
+            for i in 0..n {
+                code[i as usize] &= 0x3FFF;
+            }
+            
+        } else if k > 0 {
+            let mut s = 0;
+            for i in 0..k {
+                s += code[i as usize];
+            }
+            
+            for i in 0..n {
+                code[i as usize] <<= 16;
+            }
+            
+            for i in 0..n {
+                let v = code[i as usize] >> 16;
+                let j = Self::mod1(i + k, n);
+                s += (code[j as usize] >> 16) - v;
+                code[i as usize] += s;
+            }
+            
+            for i in 0..n {
+                code[i as usize] &= 0x3FFF;
+            }
+            
         } else {
-            (0..code.len()).map(|i| code.iter().cycle().skip(i+1).take(k as usize).sum()).collect()
+            code.fill(0);
         }
-
-        // (0..code.len()).map(|i| code.iter().cycle().skip(i+offset).take(k as usize).sum()).collect()
+        
+        code
     }
 }
+
