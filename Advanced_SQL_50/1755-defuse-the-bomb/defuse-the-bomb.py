dda760b29ae11064e4c8b042c3c4f8506a9ab209
Piotr M <filo74b@gmail.com>
2024-11-18 15:34:07 +0100

    Time: 0 ms (100.00%) | Memory: 16.8 MB (22.92%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,21 +1,29 @@
 class Solution:
-    def decrypt(self, code: List[int], k: int) -> List[int]:
-        N = len(code)
-        res = [0] * N
-
-        l = 0
-        cur_sum = 0
-        for r in range(N + abs(k)):
-            cur_sum += code[r % N]
-
-            if r - l + 1 > abs(k):
-                cur_sum -= code[l % N]
-                l = (l + 1) % N
-
-            if r - l + 1 == abs(k):
-                if k > 0:
-                    res[(l - 1) % N] = cur_sum
-                elif k < 0:
-                    res[(r + 1) % N] = cur_sum
-
-        return res
+    def decrypt(self, circ: List[int], k: int) -> List[int]:
+        n = len(circ)                   # Circular Array
+        result = [0] * n
+        
+        if k == 0:                      # k = 0
+            return result
+        
+        wsum = 0
+        if k > 0:                       # k > 0
+            for i in range(k):
+                wsum += circ[i + 1]
+            result[0] = wsum
+            
+            for i in range(1, n):
+                wsum = wsum - circ[i] + circ[(i + k) % n]
+                result[i] = wsum
+                
+        else:                           # k < 0
+            k = -k
+            for i in range(k):
+                wsum += circ[n - k + i]
+            result[0] = wsum
+            
+            for i in range(1, n):
+                wsum = wsum - circ[(n - k + i - 1) % n] + circ[i - 1]
+                result[i] = wsum
+                
+        return result
