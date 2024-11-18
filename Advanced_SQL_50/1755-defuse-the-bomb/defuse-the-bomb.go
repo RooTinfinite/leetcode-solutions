5a5fef1ef62ae2accd7b099fe7917b9b7216e2ec
Piotr M <filo74b@gmail.com>
2024-11-18 13:27:14 +0100

    Time: 0 ms (100.00%) | Memory: 4.1 MB (35.90%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.go
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.go
@@ -2,18 +2,28 @@ func decrypt(code []int, k int) []int {
     N := len(code)
     res := make([]int, N)
     
-    for i := 0; i < N; i++ {
-        if k > 0 {
-            for j := i + 1; j < i + 1 + k; j++ {
-                res[i] += code[j % N]
-            }
-        } else if k < 0 {
-            for j := i - 1; j > i - 1 - abs(k); j-- {
-                res[i] += code[((j % N) + N) % N]
+    if k == 0 {
+        return res
+    }
+    
+    l := 0
+    curSum := 0
+    for r := 0; r < N + abs(k); r++ {
+        curSum += code[r % N]
+        
+        if r - l + 1 > abs(k) {
+            curSum -= code[l % N]
+            l = (l + 1) % N
+        }
+        
+        if r - l + 1 == abs(k) {
+            if k > 0 {
+                res[(l - 1 + N) % N] = curSum
+            } else {
+                res[(r + 1) % N] = curSum
             }
         }
     }
-    
     return res
 }
 
