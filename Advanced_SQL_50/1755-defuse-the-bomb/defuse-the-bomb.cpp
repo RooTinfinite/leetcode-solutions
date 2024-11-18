d913fd1ebc405e9cc24f822a910efbc206758970
Piotr M <filo74b@gmail.com>
2024-11-18 14:26:31 +0100

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.07%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-int h(int k,vector<int>&code,int j,int n,int sign){
-    int sum = 0;
-    while(k){
-        j = j + sign;
-        if(j==-1) j= n-1;
-        else if(j==n) j =0;
-        sum+=code[j];
-        k--;
-    }
-    return sum;
-}
     vector<int> decrypt(vector<int>& code, int k) {
         int n=code.size();
-        vector<int>ans(n,0);
-        if(k==0){
+        vector<int> ans(n, 0);
+        if (k==0) return ans;
+        if (k>0){
+            int wsum=accumulate(code.begin()+1, code.begin()+k+1, 0);
+            ans[0]=wsum;
+            for(int l=1, r=k+1; l<n ; r++, l++){
+                wsum+=-code[l]+code[r%n];
+                ans[l]=wsum;
+            }
             return ans;
         }
-        int sign = k>0?1:-1;
-
-
-        for(int i=0;i<n;i++){
-            ans[i] = h(abs(k),code,i,n,sign);
+        // k<0
+        k=-k;
+        int wsum=accumulate(code.end()-k , code.end(), 0);
+        ans[0]=wsum;
+        for(int r=0, l=n-k; r<n-1; r++, l++){
+            wsum+=-code[l%n]+code[r];
+            ans[r+1]=wsum;
         }
         return ans;
-
-
-
     }
 };
