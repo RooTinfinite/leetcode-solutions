7f97d2bec705ae937fce0f97cf4738e01d021d23
Piotr M <filo74b@gmail.com>
2024-11-18 12:22:42 +0100

    Time: 7 ms (38.94%) | Memory: 16.9 MB (5.59%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,20 +1,14 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        n=len(code)
-        ans=[0]*n
-        if k==0: return ans
-        if k>0:
-            ans[0]=wsum=sum(code[1:k+1])
-            for l in range(1, n):
-                r=(l+k)%n
-                wsum+=-code[l]+code[r]
-                ans[l]=wsum
-            return ans
-        ans[0]=wsum=sum(code[-1:k-1:-1])
-        for l in range(1, n):
-            r=(l-k)%n
-            wsum+=-code[-l]+code[-r]
-            ans[-l]=wsum
-        return ans
-        
-        
+        N = len(code)
+        res = [0] * N
+
+        for i in range(N):
+            if k > 0:
+                for j in range(i + 1, i + 1 + k):
+                    res[i] += code[j % N]
+            elif k < 0:
+                for j in range(i - 1, i - 1 - abs(k), -1):
+                    res[i] += code[j % N]
+
+        return res
