f380d36f815fc6e9203593b776f4dccc14df1a4b
Piotr M <filo74b@gmail.com>
2024-11-18 15:52:21 +0100

    Time: 0 ms (100.00%) | Memory: 10.3 MB (60.07%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> decrypt(vector<int>& code, int k) {
-        int n = code.size();
+    vector<int> decrypt(vector<int>& circ, int k) {
+        int n = circ.size(); // circular array
         vector<int> result(n);
 
         if (k == 0) return result;
 
-        int wSum = 0; int start = (k > 0) ? 1 : n + k; int end = (k > 0) ? k : n - 1;
+        int wSum = 0; 
+        int start = (k > 0) ? 1 : n + k;
+        int end = (k > 0) ? k : n - 1;
+
         for (int i = start; i <= end; i++) {
-            wSum += code[i % n];
+            wSum += circ[i % n];
         }
         for (int i = 0; i < n; i++) {
             result[i] = wSum;
-            wSum -= code[(start + i) % n];
-            wSum += code[(end + i + 1) % n];
+            wSum -= circ[(start + i) % n];
+            wSum += circ[(end + i + 1) % n];
         }
         
         return result;
