0cbd25780a9b59a0d05ae9c9e1ca0885462999da
Piotr M <filo74b@gmail.com>
2024-11-18 13:27:43 +0100

    Time: 0 ms (100.00%) | Memory: 49.4 MB (95.78%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.js
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.js
@@ -4,14 +4,24 @@ var decrypt = function(code, k) {
     const N = code.length;
     const res = new Array(N).fill(0);
     
-    for (let i = 0; i < N; i++) {
-        if (k > 0) {
-            for (let j = i + 1; j < i + 1 + k; j++) {
-                res[i] += code[j % N];
-            }
-        } else if (k < 0) {
-            for (let j = i - 1; j > i - 1 + k; j--) {
-                res[i] += code[((j % N) + N) % N];
+    if (k === 0) return res;
+    
+    let l = 0;
+    let curSum = 0;
+    
+    for (let r = 0; r < N + Math.abs(k); r++) {
+        curSum += code[r % N];
+        
+        if (r - l + 1 > Math.abs(k)) {
+            curSum -= code[l % N];
+            l = (l + 1) % N;
+        }
+        
+        if (r - l + 1 === Math.abs(k)) {
+            if (k > 0) {
+                res[(l - 1 + N) % N] = curSum;
+            } else {
+                res[(r + 1) % N] = curSum;
             }
         }
     }
