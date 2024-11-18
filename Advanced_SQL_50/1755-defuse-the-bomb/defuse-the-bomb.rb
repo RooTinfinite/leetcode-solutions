1f7705d5a9569b822029d25f12bd1d457a1fa43a
Piotr M <filo74b@gmail.com>
2024-11-18 16:12:34 +0100

    Time: 0 ms (100.00%) | Memory: 211.6 MB (40.00%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.rb
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.rb
@@ -1,28 +1,22 @@
-def decrypt(code, k)
-    n = code.length
-    res = Array.new(n, 0)
+def decrypt(circ, k)
+    n = circ.size
+    result = Array.new(n, 0)
     
-    return res if k == 0
+    return result if k == 0
     
-    l = 0
-    cur_sum = 0
+    w_sum = 0
+    start = k > 0 ? 1 : n + k
+    end_pos = k > 0 ? k : n - 1
     
-    (0...(n + k.abs)).each do |r|
-        cur_sum += code[r % n]
-        
-        if r - l + 1 > k.abs
-            cur_sum -= code[l % n]
-            l = (l + 1) % n
-        end
-        
-        if r - l + 1 == k.abs
-            if k > 0
-                res[(l - 1) % n] = cur_sum
-            elsif k < 0
-                res[(r + 1) % n] = cur_sum
-            end
-        end
+    (start..end_pos).each do |i|
+        w_sum += circ[i % n]
     end
     
-    res
+    n.times do |i|
+        result[i] = w_sum
+        w_sum -= circ[(start + i) % n]
+        w_sum += circ[(end_pos + i + 1) % n]
+    end
+    
+    result
 end
