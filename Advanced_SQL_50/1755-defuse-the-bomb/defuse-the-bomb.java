42e933f3a4efa3f2225a0297de7663820d1377a9
Piotr M <filo74b@gmail.com>
2024-11-18 16:39:35 +0100

    Time: 0 ms (100.00%) | Memory: 42.2 MB (55.38%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.java
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,24 +1,58 @@
 class Solution {
-    public int[] decrypt(int[] circ, int k) {
-        int n = circ.length; // circular array
-        int[] result = new int[n];
+    private static int mod1(int i, int n) {
+        return i - ((-(i >= n ? 1 : 0)) & n);
+    }
+    
+    public static int[] decrypt(int[] code, int k) {
+        int n = code.length;
         
-        if (k == 0) return result;
-        
-        int wSum = 0;
-        int start = (k > 0) ? 1 : n + k;
-        int end = (k > 0) ? k : n - 1;
-        
-        for (int i = start; i <= end; i++) {
-            wSum += circ[i % n];
+        if (k < 0) {
+            int s = 0;
+            for (int i = n + k; i < n; i++) {
+                s += code[i];
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] <<= 16;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                int v = code[i] >> 16;
+                code[i] += s;
+                int j = mod1(n + k + i, n);
+                s += v - (code[j] >> 16);
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] &= 0x3FFF;
+            }
+            
+        } else if (k > 0) {
+            int s = 0;
+            for (int i = 0; i < k; i++) {
+                s += code[i];
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] <<= 16;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                int v = code[i] >> 16;
+                int j = mod1(i + k, n);
+                s += (code[j] >> 16) - v;
+                code[i] += s;
+            }
+            
+            for (int i = 0; i < n; i++) {
+                code[i] &= 0x3FFF;
+            }
+            
+        } else {
+            Arrays.fill(code, 0);
         }
         
-        for (int i = 0; i < n; i++) {
-            result[i] = wSum;
-            wSum -= circ[(start + i) % n];
-            wSum += circ[(end + i + 1) % n];
-        }
-        
-        return result;
+        return code;
     }
 }
+
