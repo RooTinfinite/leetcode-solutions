737c67a2e4571002c9993291c1dcd1f8189c3bfd
Piotr M <filo74b@gmail.com>
2024-11-18 13:12:06 +0100

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.07%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -4,15 +4,24 @@ public:
         int N = code.size();
         vector<int> res(N, 0);
         
-        for (int i = 0; i < N; i++) {
-            if (k > 0) {
-                for (int j = i + 1; j < i + 1 + k; j++) {
-                    res[i] += code[j % N];
-                }
+        if (k == 0) return res;
+        
+        int l = 0;
+        int cur_sum = 0;
+        
+        for (int r = 0; r < N + abs(k); r++) {
+            cur_sum += code[r % N];
+            
+            if (r - l + 1 > abs(k)) {
+                cur_sum -= code[l % N];
+                l = (l + 1) % N;
             }
-            else if (k < 0) {
-                for (int j = i - 1; j > i - 1 - abs(k); j--) {
-                    res[i] += code[((j % N) + N) % N];
+            
+            if (r - l + 1 == abs(k)) {
+                if (k > 0) {
+                    res[(l - 1 + N) % N] = cur_sum;
+                } else if (k < 0) {
+                    res[(r + 1) % N] = cur_sum;
                 }
             }
         }
