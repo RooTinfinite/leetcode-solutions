5073b2d9f2118b167e07239d9327ae98dc78d34e
Piotr M <filo74b@gmail.com>
2024-11-18 14:14:51 +0100

    Time: 0 ms (100.00%) | Memory: 10.4 MB (60.07%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,59 +1,31 @@
 class Solution {
-private:
-    static int mod1(int i, int n) {
-        return i - ((-(i >= n ? 1 : 0)) & n);
-    }
-    
 public:
-    static vector<int> decrypt(vector<int>& code, int k) {
-        int n = code.size();
-        
-        if (k < 0) {
-            int s = 0;
-            for (int i = n + k; i < n; i++) {
-                s += code[i];
-            }
-            
-            for (int i = 0; i < n; i++) {
-                code[i] <<= 16;
-            }
-            
-            for (int i = 0; i < n; i++) {
-                int v = code[i] >> 16;
-                code[i] += s;
-                int j = mod1(n + k + i, n);
-                s += v - (code[j] >> 16);
-            }
-            
-            for (int i = 0; i < n; i++) {
-                code[i] &= 0x3FFF;
-            }
-            
-        } else if (k > 0) {
-            int s = 0;
-            for (int i = 0; i < k; i++) {
-                s += code[i];
-            }
-            
-            for (int i = 0; i < n; i++) {
-                code[i] <<= 16;
-            }
-            
-            for (int i = 0; i < n; i++) {
-                int v = code[i] >> 16;
-                int j = mod1(i + k, n);
-                s += (code[j] >> 16) - v;
-                code[i] += s;
-            }
-            
-            for (int i = 0; i < n; i++) {
-                code[i] &= 0x3FFF;
-            }
-            
-        } else {
-            fill(code.begin(), code.end(), 0);
+int h(int k,vector<int>&code,int j,int n,int sign){
+    int sum = 0;
+    while(k){
+        j = j + sign;
+        if(j==-1) j= n-1;
+        else if(j==n) j =0;
+        sum+=code[j];
+        k--;
+    }
+    return sum;
+}
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n=code.size();
+        vector<int>ans(n,0);
+        if(k==0){
+            return ans;
         }
-        
-        return code;
+        int sign = k>0?1:-1;
+
+
+        for(int i=0;i<n;i++){
+            ans[i] = h(abs(k),code,i,n,sign);
+        }
+        return ans;
+
+
+
     }
 };
