5aa8d32db5c9ea07e473fc714620fc3d0451b8b2
Piotr M <filo74b@gmail.com>
2024-11-18 16:11:48 +0100

    Time: 0 ms (100.00%) | Memory: 2.2 MB (61.11%) - LeetSync

--- Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.rs
+++ Advanced_SQL_50/1755-defuse-the-bomb/defuse-the-bomb.rs
@@ -1,28 +1,26 @@
 impl Solution {
-    pub fn decrypt(code: Vec<i32>, k: i32) -> Vec<i32> {
-        let n = code.len();
-        let mut res = vec![0; n];
+    pub fn decrypt(circ: Vec<i32>, k: i32) -> Vec<i32> {
+        let n = circ.len();
+        let mut result = vec![0; n];
         
-        if k == 0 { return res; }
-        
-        let mut l = 0;
-        let mut cur_sum = 0;
-        for r in 0..(n + k.abs() as usize) {
-            cur_sum += code[r % n];
-            
-            if r - l + 1 > k.abs() as usize {
-                cur_sum -= code[l % n];
-                l = (l + 1) % n;
-            }
-            
-            if r - l + 1 == k.abs() as usize {
-                if k > 0 {
-                    res[((l as i32 - 1 + n as i32) as usize) % n] = cur_sum;
-                } else {
-                    res[(r + 1) % n] = cur_sum;
-                }
-            }
+        if k == 0 {
+            return result;
         }
-        res
+        
+        let mut w_sum = 0;
+        let start = if k > 0 { 1 } else { n as i32 + k } as usize;
+        let end = if k > 0 { k } else { n as i32 - 1 } as usize;
+        
+        for i in start..=end {
+            w_sum += circ[i % n];
+        }
+        
+        for i in 0..n {
+            result[i] = w_sum;
+            w_sum -= circ[(start + i) % n];
+            w_sum += circ[(end + i + 1) % n];
+        }
+        
+        result
     }
 }
