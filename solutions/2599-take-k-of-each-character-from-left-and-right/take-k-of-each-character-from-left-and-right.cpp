c5a544e3022eb96a784dcf12eadfc56117a0a2cc
Piotr M <filo74b@gmail.com>
2024-11-20 02:36:47 +0100

    Time: 15 ms (59.02%) | Memory: 12.1 MB (56.21%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int n = s.size();
-        int countA = 0, countB = 0, countC = 0;
         
-        // Count total occurrences of each character
-        for(int i = 0; i < n; i++) {
-            if(s[i] == 'a') countA++;
-            if(s[i] == 'b') countB++;
-            if(s[i] == 'c') countC++;
+       
+        
+        
+      int ca=0,cb=0,cc=0;
+        int n=s.size();
+        int ans=n;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a') ca++;
+            if(s[i]=='b') cb++;
+            if(s[i]=='c') cc++;
         }
-        
-        // Check if it's possible to get k occurrences of each character
-        if(countA < k || countB < k || countC < k) {
-            return -1;
-        }
-        
-        int minLength = n;
-        int right = n - 1;
-        
-        // Try different windows by moving left pointer
-        for(int left = n - 1; left >= 0; left--) {
-            // Remove character at left pointer from counts
-            if(s[left] == 'a') countA--;
-            if(s[left] == 'b') countB--;
-            if(s[left] == 'c') countC--;
-            
-            // Adjust right pointer until we have k of each character
-            while(countA < k || countB < k || countC < k) {
-                if(s[right] == 'a') countA++;
-                if(s[right] == 'b') countB++;
-                if(s[right] == 'c') countC++;
-                right--;
+        if(ca<k||cb<k||cc<k) return -1;
+        int i=n-1,j=n-1;
+        while(i>=0){
+            if(s[i]=='a') ca--;
+            if(s[i]=='b') cb--;
+            if(s[i]=='c') cc--;
+            while(ca<k||cb<k||cc<k){
+            if(s[j]=='a') ca++;
+            if(s[j]=='b') cb++;
+            if(s[j]=='c') cc++;
+                j--;
             }
-            
-            // Update minimum length required
-            minLength = min(minLength, left + n - 1 - right);
+            ans=min(ans,i+n-1-j); i--;
         }
-        
-        return minLength;
+        return ans;
+    
     }
 };
