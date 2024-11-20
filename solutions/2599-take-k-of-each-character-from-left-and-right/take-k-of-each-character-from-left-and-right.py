547d4e054e87093dc99174fc13d64388a9367cdb
Piotr M <filo74b@gmail.com>
2024-11-20 01:01:24 +0100

    Time: 123 ms (97.22%) | Memory: 17.1 MB (79.81%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -1,16 +1,25 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        c = collections.Counter(s)
-        if c['a'] < k or c['b'] < k or c['c'] < k:
+        freqs = [0] * 3
+        n = len(s)
+        
+        for c in s:
+            freqs[ord(c) - ord('a')] += 1
+        
+        i = 0
+        j = 0
+        if freqs[0] < k or freqs[1] < k or freqs[2] < k:
             return -1
-        target_c = target_a = target_b = k
-        window_start = 0
-        longest_window = 0
-        for window_end, letter in enumerate(s):
-            c[letter] -= 1
-            while c['a'] < target_a or c['b'] < target_b or c['c'] < target_c:
-                l = s[window_start]
-                c[l] += 1
-                window_start += 1
-            longest_window = max(longest_window, window_end - window_start + 1)
-        return len(s) - longest_window
+        
+        for j in range(n):
+            freqs[ord(s[j]) - ord('a')] -= 1
+            
+            if freqs[0] < k or freqs[1] < k or freqs[2] < k:
+                freqs[ord(s[i]) - ord('a')] += 1
+                i += 1
+            
+
+        return n - (j - i + 1)
+                
+        
+            
