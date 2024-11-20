3e25c878102b5a106bb122a015b034a912b39e77
Piotr M <filo74b@gmail.com>
2024-11-20 13:35:02 +0100

    Time: 8 ms (79.02%) | Memory: 11.1 MB (100.00%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,73 +1,45 @@
-#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
-#define INL __attribute__((always_inline))
-#define HOT __attribute__((hot))
-#define ALIGNED __attribute__((aligned(64)))
-#define RESTRICT __restrict__
-#define PURE __attribute__((pure))
-#define CONST __attribute__((const))
-
 class Solution {
 private:
-    static constexpr uint MAXV = 100'000;
-    alignas(64) static uint16_t freqs[3];
+    static constexpr uint8_t SH = 18;
+    static constexpr uint64_t CMASK = 0x1000040001ull, SMASK = CMASK << (SH - 1);
 
-    static bool checkFreqs(const int k) INL NO_SAN HOT PURE {
-        return __builtin_expect((freqs[0] >= k && freqs[1] >= k && freqs[2] >= k), 1);
-    }
-
-    static void updateWindow(uint16_t* RESTRICT window, char c, int delta) INL NO_SAN HOT {
-        window[c - 'a'] += delta;
-    }
-
-    static bool isValidWindow(const uint16_t* RESTRICT window, const int k) INL NO_SAN HOT PURE {
-        return (freqs[0] - window[0] >= k) & 
-               (freqs[1] - window[1] >= k) & 
-               (freqs[2] - window[2] >= k);
+    static bool vlto(const uint64_t vsa, const uint64_t vb) {
+        return ((vsa - vb) & SMASK) != SMASK;
     }
 
 public:
-    static int takeCharacters(string& RESTRICT s, const int k) NO_SAN HOT {
-        const int n = s.length();
-        
-        #pragma GCC unroll 3
-        for (int i = 0; i < 3; ++i) {
-            freqs[i] = 0;
+    static int takeCharacters(const string &s, const uint k) {
+        const uint64_t vk = k * CMASK;
+        const uint n = s.length();
+        if (!k) return 0;
+        uint l = 0, r = n - 1u;
+        uint64_t vcnt = SMASK;
+        bool lt = true;
+        for ( ; l < n && lt; l += lt) {
+            const uint8_t i = s[l] - 'a';
+            vcnt += 1ull << (i * SH);
+            lt = vlto(vcnt, vk);
         }
-        
-        #pragma GCC ivdep
-        for (char c : s) {
-            ++freqs[c - 'a'];
-        }
-        
-        if (!checkFreqs(k)) {
-            return -1;
-        }
-
-        int result = n;
-        int left = 0;
-        alignas(64) uint16_t window[3] = {0};
-
-        #pragma GCC optimize("unroll-loops")
-        for (int right = 0; right < n; right++) {
-            updateWindow(window, s[right], 1);
-            
-            while (left <= right && !isValidWindow(window, k)) {
-                updateWindow(window, s[left++], -1);
+        if (lt) return -1;
+        uint res = l + 1u;
+        for (l++; l--; ) {
+            {
+                const uint8_t i = s[l] - 'a';
+                vcnt -= 1ull << (i * SH);
             }
-            
-            result = min(result, n - (right - left + 1));
+            for (r++; r-- && vlto(vcnt, vk); ) {
+                const uint8_t j = s[r] - 'a';
+                vcnt += 1ull << (j * SH);
+            }
+            res = min(res, l + n - r - 1u);
         }
-        
-        return result;
+        return res;
     }
 };
 
-alignas(64) uint16_t Solution::freqs[3];
-
-[[maybe_unused]] static auto init = []() NO_SAN HOT {
-    ios_base::sync_with_stdio(false);
+auto init = []() {
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-    return 'c';
+    return 1337;
 }();
