7166be6b398e98ac8df9c5fd9f7d9e8da9df6a40
Piotr M <filo74b@gmail.com>
2024-11-20 02:36:34 +0100

    Time: 6 ms (91.71%) | Memory: 11.8 MB (96.89%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        // Total counts
-        vector<int> count(3, 0);
-        for (char c : s) {
-            count[c - 'a']++;
+        int n = s.size();
+        int countA = 0, countB = 0, countC = 0;
+        
+        // Count total occurrences of each character
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'a') countA++;
+            if(s[i] == 'b') countB++;
+            if(s[i] == 'c') countC++;
         }
         
-        if (*min_element(count.begin(), count.end()) < k) {
+        // Check if it's possible to get k occurrences of each character
+        if(countA < k || countB < k || countC < k) {
             return -1;
         }
         
-        // Sliding Window
-        int res = INT_MAX;
-        int l = 0;
-        for (int r = 0; r < s.length(); r++) {
-            count[s[r] - 'a']--;
+        int minLength = n;
+        int right = n - 1;
+        
+        // Try different windows by moving left pointer
+        for(int left = n - 1; left >= 0; left--) {
+            // Remove character at left pointer from counts
+            if(s[left] == 'a') countA--;
+            if(s[left] == 'b') countB--;
+            if(s[left] == 'c') countC--;
             
-            while (*min_element(count.begin(), count.end()) < k) {
-                count[s[l] - 'a']++;
-                l++;
+            // Adjust right pointer until we have k of each character
+            while(countA < k || countB < k || countC < k) {
+                if(s[right] == 'a') countA++;
+                if(s[right] == 'b') countB++;
+                if(s[right] == 'c') countC++;
+                right--;
             }
-            res = min(res, static_cast<int>(s.length()) - (r - l + 1));
+            
+            // Update minimum length required
+            minLength = min(minLength, left + n - 1 - right);
         }
-        return res;
+        
+        return minLength;
     }
 };
