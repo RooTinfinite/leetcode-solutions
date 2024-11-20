e644225c2df775d20b3d548f625762477c917c19
Piotr M <filo74b@gmail.com>
2024-11-20 02:25:15 +0100

    Time: 19 ms (100.00%) | Memory: 58.2 MB (61.90%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.js
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.js
@@ -1,33 +1,27 @@
 // JavaScript
 
 var takeCharacters = function(s, k) {
-    const freq = [0, 0, 0];
-    const n = s.length;
-    
-    for (const c of s) {
-        freq[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    // Total counts
+    const count = [0, 0, 0];
+    for (let c of s) {
+        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
     }
     
-    if (freq[0] < k || freq[1] < k || freq[2] < k) {
+    if (Math.min(...count) < k) {
         return -1;
     }
     
-    const curr = [0, 0, 0];
-    let maxLen = 0;
-    let left = 0;
-    
-    for (let right = 0; right < n; right++) {
-        curr[s.charCodeAt(right) - 'a'.charCodeAt(0)]++;
+    // Sliding Window
+    let res = Infinity;
+    let l = 0;
+    for (let r = 0; r < s.length; r++) {
+        count[s.charCodeAt(r) - 'a'.charCodeAt(0)]--;
         
-        while (left <= right && (curr[0] > freq[0] - k ||
-               curr[1] > freq[1] - k ||
-               curr[2] > freq[2] - k)) {
-            curr[s.charCodeAt(left) - 'a'.charCodeAt(0)]--;
-            left++;
+        while (Math.min(...count) < k) {
+            count[s.charCodeAt(l) - 'a'.charCodeAt(0)]++;
+            l++;
         }
-        
-        maxLen = Math.max(maxLen, right - left + 1);
+        res = Math.min(res, s.length - (r - l + 1));
     }
-    
-    return n - maxLen;
+    return res;
 };
