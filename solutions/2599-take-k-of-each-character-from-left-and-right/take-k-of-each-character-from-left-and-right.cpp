cf34bae38dc39023ed1d85b07140e7b19475fd22
Piotr M <filo74b@gmail.com>
2024-11-20 02:50:04 +0100

    Time: 8 ms (79.02%) | Memory: 12.1 MB (56.21%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int takeCharacters(string_view s, int k) {
-        if (k == 0) return 0;
-        
-        array<int, 3> count{};
-        const int n = s.length();
-        
-        // Count total frequencies
-        const char* ptr = s.data();
-        const char* end = ptr + n;
-        while (ptr < end) {
-            count[*ptr++ - 'a']++;
+    int takeCharacters(string s, int k) {
+        vector<int> count = {0, 0, 0};
+        for (char c : s) {
+            count[c - 'a']++;
         }
         
-        if (count[0] < k || count[1] < k || count[2] < k) return -1;
+        if (count[0] < k || count[1] < k || count[2] < k) {
+            return -1;
+        }
         
-        array<int, 3> window{};
-        int left = 0, maxWindow = 0;
+        int n = s.length();
+        vector<int> window = {0, 0, 0};
+        int maxWindow = 0;
+        int left = 0;
         
-        for (int right = 0; right < n; ++right) {
-            ++window[s[right] - 'a'];
+        for (int right = 0; right < n; right++) {
+            window[s[right] - 'a']++;
             
             while (left <= right && (
                 window[0] > count[0] - k ||
                 window[1] > count[1] - k ||
                 window[2] > count[2] - k
             )) {
-                --window[s[left] - 'a'];
-                ++left;
+                window[s[left] - 'a']--;
+                left++;
             }
             
             maxWindow = max(maxWindow, right - left + 1);
