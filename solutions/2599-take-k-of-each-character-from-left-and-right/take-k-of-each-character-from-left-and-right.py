462001f78b0e813c8a20970a828279ec78bc3d94
Piotr M <filo74b@gmail.com>
2024-11-20 01:05:23 +0100

    Time: 126 ms (96.67%) | Memory: 17.2 MB (79.81%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -1,20 +1,25 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        cnt = Counter(s)
+        freqs = [0] * 3
+        n = len(s)
         
-        if cnt['a'] < k or cnt['b'] < k or cnt['c'] < k:
+        for c in s:
+            freqs[ord(c) - ord('a')] += 1
+        
+        i = 0
+        j = 0
+        if freqs[0] < k or freqs[1] < k or freqs[2] < k:
             return -1
         
-        min_len = len(s)
-        l = 0
-        
-        for r in range(len(s)):
-            cnt[s[r]] -= 1
+        for j in range(n):
+            freqs[ord(s[j]) - ord('a')] -= 1
             
-            while cnt[s[r]] < k:
-                cnt[s[l]] += 1
-                l += 1
+            if freqs[0] < k or freqs[1] < k or freqs[2] < k:
+                freqs[ord(s[i]) - ord('a')] += 1
+                i += 1
+            
+
+        return n - (j - i + 1)
                 
-            min_len = min(min_len, cnt['a'] + cnt['b'] + cnt['c'])
+        
             
-        return min_len
