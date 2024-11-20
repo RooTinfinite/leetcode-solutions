0efd5183a935edd6f9a7cb5494693c1523b52270
Piotr M <filo74b@gmail.com>
2024-11-20 02:00:53 +0100

    Time: 143 ms (92.78%) | Memory: 17.3 MB (55.20%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.py
@@ -1,11 +1,11 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        lim = {c: s.count(c) - k for c in 'abc'}
-        if min(lim.values()) < 0: return -1
-        cnt = {c: 0 for c in 'abc'}
+        d = {c:s.count(c)-k for c in 'abc'}
+        if min(d.values())<0: return -1
+        h = {c:0 for c in 'abc'}
         m = l = 0
-        for r, c in enumerate(s):
-            cnt[c] += 1
-            while cnt[c] > lim[c]: cnt[s[l]] -= 1; l += 1
-            m = max(m, r - l + 1)
-        return len(s) - m
+        for r,c in enumerate(s):
+            h[c] += 1
+            while h[c]>d[c]: h[s[l]]-=1; l+=1
+            m = max(m,r-l+1)
+        return len(s)-m
