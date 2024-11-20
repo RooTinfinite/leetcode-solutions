fa48e1822258938636aee563603bf1c53fb21aea
Piotr M <filo74b@gmail.com>
2024-11-20 02:25:39 +0100

    Time: 7 ms (100.00%) | Memory: 2.4 MB (75.00%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.rs
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.rs
@@ -1,34 +1,28 @@
 impl Solution {
     pub fn take_characters(s: String, k: i32) -> i32 {
-        let mut freq = vec![0; 3];
-        let n = s.len();
-        let s_bytes = s.as_bytes();
-        
-        for &c in s_bytes {
-            freq[(c - b'a') as usize] += 1;
+        // Total counts
+        let mut count = vec![0; 3];
+        for c in s.chars() {
+            count[(c as u8 - b'a') as usize] += 1;
         }
         
-        if freq[0] < k || freq[1] < k || freq[2] < k {
+        if count.iter().min().unwrap() < &k {
             return -1;
         }
         
-        let mut curr = vec![0; 3];
-        let mut max_len = 0;
-        let mut left = 0;
-        
-        for right in 0..n {
-            curr[(s_bytes[right] - b'a') as usize] += 1;
+        // Sliding Window
+        let s_bytes = s.as_bytes();
+        let mut res = i32::MAX;
+        let mut l = 0;
+        for r in 0..s_bytes.len() {
+            count[(s_bytes[r] - b'a') as usize] -= 1;
             
-            while left <= right && (curr[0] > freq[0] - k ||
-                   curr[1] > freq[1] - k ||
-                   curr[2] > freq[2] - k) {
-                curr[(s_bytes[left] - b'a') as usize] -= 1;
-                left += 1;
+            while count.iter().min().unwrap() < &k {
+                count[(s_bytes[l] - b'a') as usize] += 1;
+                l += 1;
             }
-            
-            max_len = max_len.max(right - left + 1);
+            res = res.min(s_bytes.len() as i32 - (r as i32 - l as i32 + 1));
         }
-        
-        (n - max_len) as i32
+        res
     }
 }
