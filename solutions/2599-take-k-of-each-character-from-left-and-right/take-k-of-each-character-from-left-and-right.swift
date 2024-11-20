4df52110a141be267d5ddc543406a4f76acd23d3
Piotr M <filo74b@gmail.com>
2024-11-20 02:25:07 +0100

    Time: 111 ms (50.00%) | Memory: 19.9 MB (11.11%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.swift
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.swift
@@ -1,34 +1,28 @@
 class Solution {
     func takeCharacters(_ s: String, _ k: Int) -> Int {
-        var freq = [0, 0, 0]
+        // Total counts
+        var count = [0, 0, 0]
         let chars = Array(s)
-        let n = chars.count
-        
         for c in chars {
-            freq[Int(c.asciiValue! - Character("a").asciiValue!)] += 1
+            count[Int(c.asciiValue! - Character("a").asciiValue!)] += 1
         }
         
-        if freq[0] < k || freq[1] < k || freq[2] < k {
+        if count.min()! < k {
             return -1
         }
         
-        var curr = [0, 0, 0]
-        var maxLen = 0
-        var left = 0
-        
-        for right in 0..<n {
-            curr[Int(chars[right].asciiValue! - Character("a").asciiValue!)] += 1
+        // Sliding Window
+        var res = Int.max
+        var l = 0
+        for r in 0..<chars.count {
+            count[Int(chars[r].asciiValue! - Character("a").asciiValue!)] -= 1
             
-            while left <= right && (curr[0] > freq[0] - k ||
-                   curr[1] > freq[1] - k ||
-                   curr[2] > freq[2] - k) {
-                curr[Int(chars[left].asciiValue! - Character("a").asciiValue!)] -= 1
-                left += 1
+            while count.min()! < k {
+                count[Int(chars[l].asciiValue! - Character("a").asciiValue!)] += 1
+                l += 1
             }
-            
-            maxLen = max(maxLen, right - left + 1)
+            res = min(res, chars.count - (r - l + 1))
         }
-        
-        return n - maxLen
+        return res
     }
 }
