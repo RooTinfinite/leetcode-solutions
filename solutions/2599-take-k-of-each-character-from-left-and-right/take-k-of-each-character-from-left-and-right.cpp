e06f66b4ff55e8dfe6a0f3a748a98a63f0f10571
Piotr M <filo74b@gmail.com>
2024-11-20 02:55:06 +0100

    Time: 20 ms (41.46%) | Memory: 12.2 MB (56.21%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        int n = s.size();
-        if (k == 0) return 0;
-        int countA = 0, countB = 0, countC = 0;
-        for (char &ch : s) {
-            if (ch == 'a') countA++;
-            else if (ch == 'b') countB++;
-            else countC++;
+        vector<int> d(3), h(3);
+        for (char c : s) d[c-'a']++;
+        for (int i = 0; i < 3; i++) if ((d[i] -= k) < 0) return -1;
+        
+        int m = 0, l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            h[s[r]-'a']++;
+            while (l <= r && h[s[r]-'a'] > d[s[r]-'a']) h[s[l++]-'a']--;
+            m = max(m, r - l + 1);
         }
-        int a = countA - k;
-        int b = countB - k;
-        int c = countC - k;
-        if (a < 0 || b < 0 || c < 0) return -1;
-        int left = 0, right = 0;
-        int maxLen = 0;
-        int c_a = 0, c_b = 0, c_c = 0;
-        while (right < n) {
-            if (s[right] == 'a') c_a++;
-            else if (s[right] == 'b') c_b++;
-            else c_c++;
-            while (left <= right && (c_a > a || c_b > b || c_c > c)) {
-                if (s[left] == 'a') c_a--;
-                else if (s[left] == 'b') c_b--;
-                else c_c--;
-                left++;
-            }
-            maxLen = max(maxLen, right - left + 1);
-            right++;
-        }
-        return n - maxLen;
+        return s.length() - m;
     }
 };
