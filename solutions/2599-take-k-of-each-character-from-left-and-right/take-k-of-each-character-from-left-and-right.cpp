90c95559d10e5a3743b30f031e121d8ef70fceca
Piotr M <filo74b@gmail.com>
2024-11-20 13:46:49 +0100

    Time: 3 ms (97.56%) | Memory: 11 MB (100.00%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,28 +1,34 @@
+#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
+#define INL __attribute__((always_inline))
+#define HOT __attribute__((hot))
+
 class Solution {
 private:
     static constexpr uint8_t SH = 18;
     static constexpr uint64_t CMASK = 0x1000040001ull, SMASK = CMASK << (SH - 1);
 
-    static bool vlto(const uint64_t vsa, const uint64_t vb) {
+    static bool vlto(const uint64_t vsa, const uint64_t vb) INL NO_SAN HOT {
         return ((vsa - vb) & SMASK) != SMASK;
     }
 
 public:
-    static int takeCharacters(const string &s, const uint k) {
+    static int takeCharacters(const string &s, const uint k) NO_SAN HOT {
         const uint64_t vk = k * CMASK;
         const uint n = s.length();
         if (!k) return 0;
         uint l = 0, r = n - 1u;
         uint64_t vcnt = SMASK;
         bool lt = true;
-        for ( ; l < n && lt; l += lt) {
+        [[unroll(8)]]
+        for ( ; l < n && lt; l++) {
             const uint8_t i = s[l] - 'a';
             vcnt += 1ull << (i * SH);
             lt = vlto(vcnt, vk);
         }
         if (lt) return -1;
-        uint res = l + 1u;
-        for (l++; l--; ) {
+        uint res = l;
+        [[unroll(4)]]
+        while (l--) {
             {
                 const uint8_t i = s[l] - 'a';
                 vcnt -= 1ull << (i * SH);
@@ -41,5 +47,5 @@ auto init = []() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    return 1337;
+    return 'c';
 }();
