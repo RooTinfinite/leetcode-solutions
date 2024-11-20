fa3ae8a955055bc7ddbe560a71d92cfa7fcd5975
Piotr M <filo74b@gmail.com>
2024-11-20 02:52:49 +0100

    Time: 11 ms (69.76%) | Memory: 12.2 MB (56.21%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,57 +1,16 @@
-#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
-#define INL __attribute__((always_inline))
-#define HOT __attribute__((hot))
-
 class Solution {
-private:
-    static constexpr uint MAXN = 100'000;
-    static int counts[3];
-    static int window[3];
-    
-    static bool checkWindow(const int k) INL NO_SAN HOT {
-        return window[0] <= counts[0] - k && 
-               window[1] <= counts[1] - k && 
-               window[2] <= counts[2] - k;
-    }
-
 public:
-    static int takeCharacters(string& s, const int k) NO_SAN HOT {
-        const int n = s.length();
-        fill(counts, counts + 3, 0);
+    int takeCharacters(string s, int k) {
+        vector<int> d(3), h(3);
+        for (char c : s) d[c-'a']++;
+        for (int i = 0; i < 3; i++) if ((d[i] -= k) < 0) return -1;
         
-        for (const char c : s) {
-            counts[c - 'a']++;
+        int m = 0, l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            h[s[r]-'a']++;
+            while (l <= r && h[s[r]-'a'] > d[s[r]-'a']) h[s[l++]-'a']--;
+            m = max(m, r - l + 1);
         }
-        
-        if (counts[0] < k || counts[1] < k || counts[2] < k) {
-            return -1;
-        }
-        
-        fill(window, window + 3, 0);
-        int maxWindow = 0;
-        int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            window[s[right] - 'a']++;
-            
-            while (left <= right && !checkWindow(k)) {
-                window[s[left] - 'a']--;
-                left++;
-            }
-            
-            maxWindow = max(maxWindow, right - left + 1);
-        }
-        
-        return n - maxWindow;
+        return s.length() - m;
     }
 };
-
-int Solution::counts[3];
-int Solution::window[3];
-
-static auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
