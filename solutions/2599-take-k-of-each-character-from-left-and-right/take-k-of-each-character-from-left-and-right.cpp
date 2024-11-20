a8ce40bc817ac9a5a6a8308c2295cbd534b12882
Piotr M <filo74b@gmail.com>
2024-11-20 02:06:51 +0100

    Time: 0 ms (100.00%) | Memory: 11.9 MB (96.89%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int takeCharacters(string s, int k) {
-        vector<int> d(3);
-        for (char c : s) d[c - 'a']++;
-        for (int i = 0; i < 3; i++) {
-            d[i] -= k;
-            if (d[i] < 0) return -1;
-        }
+        vector<int> d(3), h(3);
+        for (char c : s) d[c-'a']++;
+        for (int i = 0; i < 3; i++) if ((d[i] -= k) < 0) return -1;
         
-        vector<int> h(3);
         int m = 0, l = 0;
-        
         for (int r = 0; r < s.length(); r++) {
-            h[s[r] - 'a']++;
-            while (h[s[r] - 'a'] > d[s[r] - 'a']) {
-                h[s[l] - 'a']--;
-                l++;
-            }
+            h[s[r]-'a']++;
+            while (l <= r && h[s[r]-'a'] > d[s[r]-'a']) h[s[l++]-'a']--;
             m = max(m, r - l + 1);
         }
-        
         return s.length() - m;
     }
 };
