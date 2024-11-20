c73036b96dff34fac8eb28be12b26c5e31b600af
Piotr M <filo74b@gmail.com>
2024-11-20 01:45:44 +0100

    Time: 6 ms (100.00%) | Memory: 45.4 MB (34.91%) - LeetSync

--- solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
+++ solutions/2599-take-k-of-each-character-from-left-and-right/take-k-of-each-character-from-left-and-right.java
@@ -1,11 +1,9 @@
 class Solution {
     public int takeCharacters(String s, int k) {
-        // Count frequencies of characters a, b, c
         int[] charCount = new int[3];
         char[] chars = s.toCharArray();
         int length = chars.length;
         
-        // Find first valid position from left
         int left;
         for (left = 0; left < length; left++) {
             charCount[chars[left] - 'a']++;
@@ -14,22 +12,18 @@ class Solution {
             }
         }
         
-        // If we couldn't find valid count
         if (left == length) {
             return -1;
         }
         
-        // Initialize variables for sliding window
         int currentCount = left + 1;
         int minCount = currentCount;
         int right = length - 1;
         
-        // Slide window to find minimum length
         while (left >= 0) {
             int currentChar = chars[left] - 'a';
             
             if (charCount[currentChar] == k) {
-                // Move right pointer until matching character found
                 while (chars[left] != chars[right]) {
                     charCount[chars[right] - 'a']++;
                     right--;
@@ -51,3 +45,4 @@ class Solution {
         return count[0] >= k && count[1] >= k && count[2] >= k;
     }
 }
+
