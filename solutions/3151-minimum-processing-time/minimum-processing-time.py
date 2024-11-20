7b6f24121dda96a254d39ee4ce2fb6085c696895
Piotr M <filo74b@gmail.com>
2024-11-20 17:26:36 +0100

    Time: 47 ms (98.64%) | Memory: 32.2 MB (87.55%) - LeetSync

--- solutions/3151-minimum-processing-time/minimum-processing-time.py
+++ solutions/3151-minimum-processing-time/minimum-processing-time.py
@@ -1,15 +1,17 @@
 class Solution:
-    def minProcessingTime(self, t, v):
-        n = len(v)
-        t.sort()
-        v.sort()
-        j = n - 1
-        m = len(t)
-        ans = 0
-        for i in range(m):
-            c = 0
-            while c < 4:
-                ans = max(ans, t[i] + v[j])
-                c += 1
-                j -= 1
-        return ans
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        processorTime.sort()
+        tasks.sort(reverse = True)
+        a=[]
+        j = 0
+        for i in range(len(processorTime)):
+            
+            ans = processorTime[i]+tasks[j]
+            a.append(ans)
+            j = j+4
+                
+        return max(a)
+                    
+                
+                
+        
