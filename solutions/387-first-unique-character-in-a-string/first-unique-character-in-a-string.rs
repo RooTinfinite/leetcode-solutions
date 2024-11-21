c95b56b784a85b68ab4d13e09f15d4e5ceff734a
Piotr M <filo74b@gmail.com>
2024-11-21 19:51:14 +0100

    Time: 0 ms (100.00%) | Memory: 2.3 MB (81.25%) - LeetSync

--- solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
+++ solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.rs
@@ -1,18 +1,15 @@
-use std::collections::HashMap;
-
 impl Solution {
-    pub fn first_uniq_char(s: String) -> i32 {
-        let mut count: HashMap<char, i32> = HashMap::new();
+    pub fn first_uniq_char(input_str: String) -> i32 {
+        let mut first_unique = i32::MAX;
         
-        for c in s.chars() {
-            *count.entry(c).or_insert(0) += 1;
-        }
-        
-        for (i, c) in s.chars().enumerate() {
-            if count[&c] == 1 {
-                return i as i32;
+        for c in 'a'..='z' {
+            if input_str.contains(c) {
+                if input_str.find(c) == input_str.rfind(c) {
+                    first_unique = first_unique.min(input_str.find(c).unwrap() as i32);
+                }
             }
         }
-        -1
+        
+        if first_unique == i32::MAX { -1 } else { first_unique }
     }
 }
