d97e85e41057b6344c6933107aef90696f5f7895
Piotr M <filo74b@gmail.com>
2024-11-21 01:13:46 +0100

    Time: 19 ms (97.94%) | Memory: 150.9 MB (96.08%) - LeetSync

--- solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,48 +1,37 @@
 class Solution {
-private:
-    void fill_row(vector<vector<int>>& grid, int row, int col, int m, int n) {
-        for (int i = col - 1; i >= 0; i--) {
-            if (grid[row][i] == 2) break;
-            grid[row][i] = 1;
-        }
-        
-        for (int i = col + 1; i < n; i++) {
-            if (grid[row][i] == 2) break;
-            grid[row][i] = 1;
-        }
-        
-        for (int i = row - 1; i >= 0; i--) {
-            if (grid[i][col] == 2) break;
-            grid[i][col] = 1;
-        }
-        
-        for (int i = row + 1; i < m; i++) {
-            if (grid[i][col] == 2) break;
-            grid[i][col] = 1;
-        }
-    }
-    
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> grid(m, vector<int>(n, 0));
+        int g[m][n];
+        memset(g, 0, sizeof(g));
         
-        for (const auto& wall : walls) {
-            grid[wall[0]][wall[1]] = 2;
+        for (auto& e : guards) {
+            g[e[0]][e[1]] = 2;
         }
         
-        for (const auto& guard : guards) {
-            grid[guard[0]][guard[1]] = 2;
+        for (auto& e : walls) {
+            g[e[0]][e[1]] = 2;
         }
         
-        for (const auto& guard : guards) {
-            fill_row(grid, guard[0], guard[1], m, n);
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        
+        for (auto& e : guards) {
+            for (int k = 0; k < 4; ++k) {
+                int x = e[0], y = e[1];
+                int dx = dirs[k], dy = dirs[k + 1];
+                
+                while (x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n && g[x + dx][y + dy] < 2) {
+                    x += dx;
+                    y += dy;
+                    g[x][y] = 1;
+                }
+            }
         }
         
-        int count = 0;
-        for (const auto& row : grid) {
-            count += count_if(row.begin(), row.end(), [](int x) { return x == 0; });
+        int unguardedCount = 0;
+        for (int i = 0; i < m; i++) {
+            unguardedCount += count(g[i], g[i] + n, 0);
         }
         
-        return count;
+        return unguardedCount;
     }
 };
