1698682a62d154f5dc221c6b6e7e69f42f24f8f8
Piotr M <filo74b@gmail.com>
2024-11-21 14:38:19 +0100

    Time: 25 ms (56.14%) | Memory: 78.5 MB (6.02%) - LeetSync

--- solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
+++ solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.java
@@ -1,49 +1,30 @@
 public class Solution {
+    private static final int[] DIRECTIONS_ROW = {-1, 1, 0, 0};
+    private static final int[] DIRECTIONS_COL = {0, 0, -1, 1};
+
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         char[][] grid = new char[m][n];
-        int unguarded = m * n;
+        int unguarded = m * n - guards.length - walls.length;
 
         for (int[] guard : guards) {
-            grid[guard[0]][guard[1]] = 1;
-            unguarded--;
+            grid[guard[0]][guard[1]] = 'G';
         }
         for (int[] wall : walls) {
-            grid[wall[0]][wall[1]] = 2;
-            unguarded--;
+            grid[wall[0]][wall[1]] = 'W';
         }
 
         for (int[] guard : guards) {
-            int row = guard[0], col = guard[1];
+            for (int dir = 0; dir < 4; dir++) {
+                int row = guard[0] + DIRECTIONS_ROW[dir];
+                int col = guard[1] + DIRECTIONS_COL[dir];
 
-            for (int c = col + 1; c < n; c++) {
-                if (grid[row][c] == 1 || grid[row][c] == 2) break;
-                if (grid[row][c] == 0) {
-                    grid[row][c] = 3;
-                    unguarded--;
-                }
-            }
-
-            for (int c = col - 1; c >= 0; c--) {
-                if (grid[row][c] == 1 || grid[row][c] == 2) break;
-                if (grid[row][c] == 0) {
-                    grid[row][c] = 3;
-                    unguarded--;
-                }
-            }
-
-            for (int r = row + 1; r < m; r++) {
-                if (grid[r][col] == 1 || grid[r][col] == 2) break;
-                if (grid[r][col] == 0) {
-                    grid[r][col] = 3;
-                    unguarded--;
-                }
-            }
-
-            for (int r = row - 1; r >= 0; r--) {
-                if (grid[r][col] == 1 || grid[r][col] == 2) break;
-                if (grid[r][col] == 0) {
-                    grid[r][col] = 3;
-                    unguarded--;
+                while (row >= 0 && row < m && col >= 0 && col < n && grid[row][col] != 'W' && grid[row][col] != 'G') {
+                    if (grid[row][col] != 'S') {
+                        grid[row][col] = 'S';
+                        unguarded--;
+                    }
+                    row += DIRECTIONS_ROW[dir];
+                    col += DIRECTIONS_COL[dir];
                 }
             }
         }
