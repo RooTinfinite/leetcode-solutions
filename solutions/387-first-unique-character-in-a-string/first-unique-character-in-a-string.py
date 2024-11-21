f534d0d49443103c7a8da177e96e560e52da0beb
Piotr M <filo74b@gmail.com>
2024-11-21 19:04:15 +0100

    Time: 0 ms (100.00%) | Memory: 16.8 MB (87.08%) - LeetSync

--- solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,12 +1,12 @@
 class Solution:
-    def firstUniqChar(self, text: str) -> int:
-        first_unique = float('inf') # first unique position
+    def firstUniqChar(self, input_str: str) -> int:
+        first_unique = float('inf') # first unique position/index
         
         for char_code in range(ord('a'), ord('z') + 1):
             curr_char = chr(char_code)
             
-            if curr_char in text:
-                if text.index(curr_char) == text.rindex(curr_char):
-                    first_unique = min(first_unique, text.index(curr_char))
+            if curr_char in input_str:
+                if input_str.index(curr_char) == input_str.rindex(curr_char):
+                    first_unique = min(first_unique, input_str.index(curr_char))
                     
         return first_unique if first_unique != float('inf') else -1
