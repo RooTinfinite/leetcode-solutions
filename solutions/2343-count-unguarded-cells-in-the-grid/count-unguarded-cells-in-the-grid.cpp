41b5387bd339f1d9d69d446cc62cee8829b873bf
Piotr M <filo74b@gmail.com>
2024-11-21 05:12:00 +0100

    Time: 4 ms (100.00%) | Memory: 150.1 MB (100.00%) - LeetSync

--- solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,13 +1,13 @@
 #define NO_SANITIZE __attribute__((no_sanitize("undefined", "address", "thread", "memory", "leak")))
 #define ALWAYS_INLINE __attribute__((always_inline)) inline
-#define HOT __attribute__((hot, optimize("O3")))
+#define HOT __attribute__((hot, optimize("O3", "unroll-loops", "fast-math")))
 #define PURE __attribute__((pure))
 #define CONST __attribute__((const))
 #define FLATTEN __attribute__((flatten))
 #define LIKELY(x) __builtin_expect(!!(x), 1)
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 #define ALIGNED(x) __attribute__((aligned(x)))
-#define VECTOR_SIZE(x) __attribute__((vector_size(x)))
+#define PREFETCH(x) __builtin_prefetch(x)
 
 class Solution {
 private:
@@ -45,33 +45,57 @@ public:
         
         if (UNLIKELY(guard_size + wall_size == size)) return 0;
         
-        #pragma GCC unroll 4
+        #pragma GCC ivdep
+        #pragma GCC unroll 8
         for (uint i = 0; i < wall_size; ++i) {
-            set(walls[i][0] * n + walls[i][1], wall);
+            const uint pos = walls[i][0] * n + walls[i][1];
+            PREFETCH(&grid[pos >> 5]);
+            set(pos, wall);
         }
         
-        #pragma GCC unroll 4
+        #pragma GCC ivdep
+        #pragma GCC unroll 8
         for (uint i = 0; i < guard_size; ++i) {
-            set(guards[i][0] * n + guards[i][1], guard);
+            const uint pos = guards[i][0] * n + guards[i][1];
+            PREFETCH(&grid[pos >> 5]);
+            set(pos, guard);
         }
 
-        #pragma GCC unroll 2
+        #pragma GCC unroll 4
         for (uint g = 0; g < guard_size; ++g) {
             const uint x = guards[g][0], y = guards[g][1];
+            const uint base = x * n;
             
-            for (uint i = x - 1u, d = i * n + y; i + 1u && get(d) <= spied; i--, d -= n)
+            #pragma GCC ivdep
+            for (uint i = x - 1u, d = i * n + y; i + 1u && get(d) <= spied; i--, d -= n) {
+                PREFETCH(&grid[(d-n) >> 5]);
                 set(d, spied);
-            for (uint i = x + 1u, d = i * n + y; i < m && get(d) <= spied; i++, d += n)
+            }
+            
+            #pragma GCC ivdep
+            for (uint i = x + 1u, d = i * n + y; i < m && get(d) <= spied; i++, d += n) {
+                PREFETCH(&grid[(d+n) >> 5]);
                 set(d, spied);
-            for (uint j = y - 1u, d = x * n + j; j + 1u && get(d) <= spied; j--, d--)
+            }
+            
+            #pragma GCC ivdep
+            for (uint j = y - 1u, d = base + j; j + 1u && get(d) <= spied; j--, d--) {
+                PREFETCH(&grid[(d-1) >> 5]);
                 set(d, spied);
-            for (uint j = y + 1u, d = x * n + j; j < n && get(d) <= spied; j++, d++)
+            }
+            
+            #pragma GCC ivdep
+            for (uint j = y + 1u, d = base + j; j < n && get(d) <= spied; j++, d++) {
+                PREFETCH(&grid[(d+1) >> 5]);
                 set(d, spied);
+            }
         }
 
         uint guarded = 0;
-        #pragma GCC unroll 8
+        #pragma GCC ivdep
+        #pragma GCC unroll 16
         for (uint k = 0; k < qsize; ++k) {
+            PREFETCH(&grid[k + 1]);
             guarded += __builtin_popcountll((grid[k] | (grid[k] >> 1)) & BIT_MASK);
         }
 
