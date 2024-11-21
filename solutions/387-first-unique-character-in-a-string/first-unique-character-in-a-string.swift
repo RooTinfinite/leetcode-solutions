c4f3feba465ce76bebab42eee27779a9cbede006
Piotr M <filo74b@gmail.com>
2024-11-21 19:50:40 +0100

    Time: 110 ms (20.00%) | Memory: 18.2 MB (33.67%) - LeetSync

--- solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.swift
+++ solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.swift
@@ -1,16 +1,18 @@
 class Solution {
-    func firstUniqChar(_ s: String) -> Int {
-        var count: [Character: Int] = [:]
+    func firstUniqChar(_ inputStr: String) -> Int {
+        var firstUnique = Int.max
         
-        for c in s {
-            count[c, default: 0] += 1
-        }
-        
-        for (i, c) in s.enumerated() {
-            if count[c] == 1 {
-                return i
+        for ascii in Character("a").asciiValue!...Character("z").asciiValue! {
+            let char = String(UnicodeScalar(ascii))
+            if inputStr.contains(char) {
+                if inputStr.firstIndex(of: Character(char)) == inputStr.lastIndex(of: Character(char)) {
+                    if let index = inputStr.firstIndex(of: Character(char)) {
+                        firstUnique = min(firstUnique, inputStr.distance(from: inputStr.startIndex, to: index))
+                    }
+                }
             }
         }
-        return -1
+        
+        return firstUnique != Int.max ? firstUnique : -1
     }
 }
