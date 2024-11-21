c1c1b8a02babfe1f8e2aea1e41b30775223ea867
Piotr M <filo74b@gmail.com>
2024-11-21 19:34:38 +0100

    Time: 26 ms (51.16%) | Memory: 7.6 MB (62.30%) - LeetSync

---
+++ solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -0,0 +1,14 @@
+func firstUniqChar(s string) int {
+    count := make(map[rune]int)
+    
+    for _, c := range s {
+        count[c]++
+    }
+    
+    for i, c := range s {
+        if count[c] == 1 {
+            return i
+        }
+    }
+    return -1
+}
