cb9fb88e4c11320f889787d25aad1727fc61492b
Piotr M <filo74b@gmail.com>
2024-11-21 19:04:23 +0100

    Time: 10 ms (99.74%) | Memory: 16.9 MB (87.08%) - LeetSync

--- solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ solutions/387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,12 +1,9 @@
 class Solution:
     def firstUniqChar(self, input_str: str) -> int:
-        first_unique = float('inf') # first unique position/index
+        first_unique = []
         
-        for char_code in range(ord('a'), ord('z') + 1):
-            curr_char = chr(char_code)
-            
-            if curr_char in input_str:
-                if input_str.index(curr_char) == input_str.rindex(curr_char):
-                    first_unique = min(first_unique, input_str.index(curr_char))
-                    
-        return first_unique if first_unique != float('inf') else -1
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            if input_str.count(letter) == 1:
+                first_unique.append(input_str.find(letter))
+        
+        return min(first_unique) if first_unique else -1
