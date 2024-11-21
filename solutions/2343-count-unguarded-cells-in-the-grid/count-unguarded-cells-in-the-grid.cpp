d2b6ea48d3522fc72c8e4e0be19f945856f6b88a
Piotr M <filo74b@gmail.com>
2024-11-21 01:05:43 +0100

    Time: 20 ms (97.94%) | Memory: 150.8 MB (96.08%) - LeetSync

--- solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ solutions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -1,54 +1,30 @@
 class Solution {
 public:
-    void markUnguarded(int row, int col, vector<vector<int>>& grid) {
-        // Traverse upwards
-        for (int r = row - 1; r >= 0; r--) {
-            if (grid[r][col] == 3 || grid[r][col] == 2) break;
-            grid[r][col] = 1;
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        int g[m][n];
+        memset(g, 0, sizeof(g));
+        for (auto& e : guards) {
+            g[e[0]][e[1]] = 2;
         }
-        // Traverse downwards
-        for (int r = row + 1; r < grid.size(); r++) {
-            if (grid[r][col] == 3 || grid[r][col] == 2) break;
-            grid[r][col] = 1;
+        for (auto& e : walls) {
+            g[e[0]][e[1]] = 2;
         }
-        // Traverse leftwards
-        for (int c = col - 1; c >= 0; c--) {
-            if (grid[row][c] == 3 || grid[row][c] == 2) break;
-            grid[row][c] = 1;
-        }
-        // Traverse rightwards
-        for (int c = col + 1; c < grid[row].size(); c++) {
-            if (grid[row][c] == 3 || grid[row][c] == 2) break;
-            grid[row][c] = 1;
-        }
-    }
-
-    int countUnguarded(int m, int n, vector<vector<int>>& guards,
-                       vector<vector<int>>& walls) {
-        vector<vector<int>> grid(m, vector<int>(n, 0));
-
-        // Mark guards' positions
-        for (const auto& guard : guards) {
-            grid[guard[0]][guard[1]] = 2;
-        }
-
-        // Mark walls' positions
-        for (const auto& wall : walls) {
-            grid[wall[0]][wall[1]] = 3;
-        }
-
-        // Mark cells as guarded by traversing from each guard
-        for (const auto& guard : guards) {
-            markUnguarded(guard[0], guard[1], grid);
-        }
-
-        // Count unguarded cells
-        int count = 0;
-        for (const auto& row : grid) {
-            for (const auto& cell : row) {
-                if (cell == 0) count++;
+        int dirs[5] = {-1, 0, 1, 0, -1};
+        for (auto& e : guards) {
+            for (int k = 0; k < 4; ++k) {
+                int x = e[0], y = e[1];
+                int a = dirs[k], b = dirs[k + 1];
+                while (x + a >= 0 && x + a < m && y + b >= 0 && y + b < n && g[x + a][y + b] < 2) {
+                    x += a;
+                    y += b;
+                    g[x][y] = 1;
+                }
             }
         }
-        return count;
+        int ans = 0;
+        for (auto& row : g) {
+            ans += count(row, row + n, 0);
+        }
+        return ans;
     }
 };
