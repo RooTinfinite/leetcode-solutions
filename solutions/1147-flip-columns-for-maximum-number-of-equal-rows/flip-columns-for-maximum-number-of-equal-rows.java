69cead1459292e36ba56db288ffe112626d93779
Piotr M <filo74b@gmail.com>
2024-11-22 01:47:30 +0100

    Time: 30 ms (56.18%) | Memory: 61.5 MB (13.43%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
@@ -1,20 +1,33 @@
 class Solution {
     public int maxEqualRowsAfterFlips(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
         Map<String, Integer> patterns = new HashMap<>();
-        int maxCount = 0;
+        
+        StringBuilder keyBuilder = new StringBuilder(cols);
         
         for (int[] row : matrix) {
-            char[] pattern = new char[row.length];
-            int firstBit = row[0];
+            keyBuilder.setLength(0);
+            int flip = row[0];
             
-            for (int j = 0; j < row.length; j++) {
-                pattern[j] = (row[j] == firstBit) ? '0' : '1';
+            int j = 0;
+            for (; j + 3 < cols; j += 4) {
+                int val1 = row[j] ^ flip;
+                int val2 = row[j + 1] ^ flip;
+                int val3 = row[j + 2] ^ flip;
+                int val4 = row[j + 3] ^ flip;
+                
+                keyBuilder.append(val1).append(val2).append(val3).append(val4);
             }
             
-            String key = new String(pattern);
-            maxCount = Math.max(maxCount, patterns.merge(key, 1, Integer::sum));
+            for (; j < cols; j++) {
+                keyBuilder.append(row[j] ^ flip);
+            }
+            
+            String key = keyBuilder.toString();
+            patterns.merge(key, 1, Integer::sum);
         }
         
-        return maxCount;
+        return patterns.values().stream().mapToInt(Integer::intValue).max().orElse(0);
     }
 }
