00fd08c4eba15e944755b66d30a12ad88b7b83c8
Piotr M <filo74b@gmail.com>
2024-11-22 02:48:56 +0100

    Time: 25 ms (89.89%) | Memory: 57.8 MB (80.60%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.java
@@ -1,26 +1,23 @@
-import java.nio.charset.StandardCharsets;
-
 class Solution {
-    public int maxEqualRowsAfterFlips(int[][] mat) {
-        int res = 0;
-        int n = mat[0].length;
-        Map<String, Integer> freq = new HashMap<>();
-
-        byte[] pattern = new byte[(n + 7) / 8];  
-
-        for (int[] row : mat) {
-            int firstBit = row[0];
-
-            Arrays.fill(pattern, (byte)0);
-
-            for (int i = 0; i < n; i++) {
-                if (row[i] != firstBit) {
-                    pattern[i / 8] |= (1 << (i % 8));
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        Map<String, Integer> count = new HashMap<>();
+        
+        for (int[] row : matrix) {
+            StringBuilder key = new StringBuilder();
+            
+            if (row[0] == 1) {
+                for (int n : row) {
+                    key.append(n == 0 ? 1 : 0);
+                }
+            } else {
+                for (int n : row) {
+                    key.append(n);
                 }
             }
-            String key = new String(pattern, StandardCharsets.ISO_8859_1);
-            res = Math.max(res, freq.merge(key, 1, Integer::sum));
+            
+            count.merge(key.toString(), 1, Integer::sum);
         }
-        return res;
+        
+        return Collections.max(count.values());
     }
 }
