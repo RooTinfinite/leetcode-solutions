da96ce245378bd24ea351640439f16f632d57d97
Piotr M <filo74b@gmail.com>
2024-11-22 01:27:36 +0100

    Time: 32 ms (100.00%) | Memory: 59.2 MB (62.50%) - LeetSync

---
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.ts
@@ -0,0 +1,15 @@
+// TypeScript
+
+function maxEqualRowsAfterFlips(mat: number[][]): number {
+    const patFreq = new Map<string, number>();
+    
+    for (const row of mat) {
+        const pattern = row[0] === 0 
+            ? row.join('')
+            : row.map(bit => bit ^ 1).join('');
+            
+        patFreq.set(pattern, (patFreq.get(pattern) || 0) + 1);
+    }
+    
+    return Math.max(...patFreq.values());
+}
