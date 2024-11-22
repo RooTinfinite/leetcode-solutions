724c5e73f25e285a95ac05392e645edd21a9eb69
Piotr M <filo74b@gmail.com>
2024-11-22 02:54:01 +0100

    Time: 40 ms (100.00%) | Memory: 18.1 MB (100.00%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.swift
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.swift
@@ -1,17 +1,15 @@
 class Solution {
-    func maxEqualRowsAfterFlips(_ mat: [[Int]]) -> Int {
-        var patFreq: [String: Int] = [:]
+    func maxEqualRowsAfterFlips(_ matrix: [[Int]]) -> Int {
+        var count: [String: Int] = [:]
         
-        for row in mat {
-            var pattern = ""
-            if row[0] == 0 {
-                pattern = row.map { String($0) }.joined()
-            } else {
-                pattern = row.map { String($0 ^ 1) }.joined()
+        for row in matrix {
+            var key = ""
+            for (i, num) in row.enumerated() {
+                key += String(row[0] == 1 ? 1 - num : num)
             }
-            patFreq[pattern, default: 0] += 1
+            count[key, default: 0] += 1
         }
         
-        return patFreq.values.max() ?? 0
+        return count.values.max() ?? 0
     }
 }
