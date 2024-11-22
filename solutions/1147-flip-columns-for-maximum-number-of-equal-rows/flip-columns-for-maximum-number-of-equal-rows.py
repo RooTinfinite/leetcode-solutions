10185f2cda621d01579b1c8e7ede277a02dbf47a
Piotr M <filo74b@gmail.com>
2024-11-22 02:47:01 +0100

    Time: 35 ms (91.67%) | Memory: 19.7 MB (31.16%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,31 +1,13 @@
 class Solution:
-    def maxEqualRowsAfterFlips(self, mat: List[List[int]]) -> int:
-        res = 0
-        n = len(mat[0])
-        freq = {}
-        
-        for row in mat:
-            first_bit = row[0]
-            pattern = bytearray()
-            
-            i = 0
-            while i + 7 < n:
-                packed = 0
-                for j in range(8):
-                    if row[i + j] != first_bit:
-                        packed |= (1 << j)
-                pattern.append(packed)
-                i += 8
-            
-            if i < n:
-                packed = 0
-                for j in range(n - i):
-                    if row[i + j] != first_bit:
-                        packed |= (1 << j)
-                pattern.append(packed)
-            
-            pattern_bytes = bytes(pattern)
-            freq[pattern_bytes] = freq.get(pattern_bytes, 0) + 1
-            res = max(res, freq[pattern_bytes])
-            
-        return res
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        count = defaultdict(int)
+
+        for row in matrix:
+            row_key = tuple(row)
+
+            if row[0]:
+                row_key = tuple([0 if n else 1 for n in row])
+
+            count[row_key] += 1
+
+        return max(count.values())
