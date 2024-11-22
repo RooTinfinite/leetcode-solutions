5fdf10f6ec4f319bb21806eecc68eb894e1a34af
Piotr M <filo74b@gmail.com>
2024-11-22 02:54:56 +0100

    Time: 37 ms (100.00%) | Memory: 2.8 MB (100.00%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.rs
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.rs
@@ -1,19 +1,18 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn max_equal_rows_after_flips(mat: Vec<Vec<i32>>) -> i32 {
-        let mut pat_freq = HashMap::new();
+    pub fn max_equal_rows_after_flips(matrix: Vec<Vec<i32>>) -> i32 {
+        let mut count: HashMap<String, i32> = HashMap::new();
         
-        for row in mat {
-            let pattern = if row[0] == 0 {
-                row.iter().map(|&x| x.to_string()).collect::<String>()
-            } else {
-                row.iter().map(|&x| (x ^ 1).to_string()).collect::<String>()
-            };
-            
-            *pat_freq.entry(pattern).or_insert(0) += 1;
+        for row in matrix.iter() {
+            let key: String = row.iter()
+                .map(|&n| if row[0] == 1 { 1 - n } else { n })
+                .map(|n| n.to_string())
+                .collect();
+                
+            *count.entry(key).or_insert(0) += 1;
         }
         
-        *pat_freq.values().max().unwrap()
+        *count.values().max().unwrap()
     }
 }
