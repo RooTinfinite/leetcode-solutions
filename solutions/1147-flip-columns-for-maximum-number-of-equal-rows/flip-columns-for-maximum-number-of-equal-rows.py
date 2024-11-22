e2270b2ce050f67861ec1efa7c1b3be07c2d7ffc
Piotr M <filo74b@gmail.com>
2024-11-22 02:09:38 +0100

    Time: 74 ms (42.59%) | Memory: 19.4 MB (39.20%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,7 +1,29 @@
 class Solution:
-    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        patterns = {}
-        for row in matrix:
-            pattern = tuple(row) if row[0] == 0 else tuple(1-x for x in row)
-            patterns[pattern] = patterns.get(pattern, 0) + 1
-        return max(patterns.values())
+    def maxEqualRowsAfterFlips(self, mat: List[List[int]]) -> int:
+        freq = {}
+        n = len(mat[0])
+        
+        for row in mat:
+            pattern = []
+            first_bit = row[0]
+            
+            i = 0
+            while i + 7 < n:
+                packed = 0
+                for j in range(8):
+                    if row[i + j] != first_bit:
+                        packed |= (1 << j)
+                pattern.append(packed)
+                i += 8
+            
+            if i < n:
+                packed = 0
+                for j in range(n - i):
+                    if row[i + j] != first_bit:
+                        packed |= (1 << j)
+                pattern.append(packed)
+                
+            pattern = tuple(pattern)
+            freq[pattern] = freq.get(pattern, 0) + 1
+            
+        return max(freq.values())
