421b4dc524288954fda3bf87825405d425678018
Piotr M <filo74b@gmail.com>
2024-11-22 01:04:31 +0100

    Time: 4 ms (98.55%) | Memory: 67.3 MB (95.52%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
-        unordered_map<string, int> patterns;
-        
-        for (const auto& row : matrix) {
-            string pattern;
-            int first = row[0];
-            
-            for (int num : row) {
-                pattern += (num == first ? '1' : '0');
-            }
-            patterns[pattern]++;
-        }
-        
-        int maxCount = 0;
-        for (const auto& [pattern, count] : patterns) {
-            maxCount = max(maxCount, count);
-        }
-        
-        return maxCount;
+    int maxEqualRowsAfterFlips(vector<vector<int>>& mat) {
+        int res = 0;
+        const int n = mat[0].size();
+        unordered_map<bitset<300>, int> freq;
+		for (auto& row : mat) {
+            bitset<300> same;
+            same.set(0);
+			for (int i = 1; i < row.size(); i++) {
+				if (row[0] != row[i]) {
+                    same.flip(i);
+                }
+			}
+			freq[same]++;
+            res = max(res, freq[same]);
+		}
+        return res;
     }
 };
