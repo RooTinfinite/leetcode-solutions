cd47ca5d7d51f4209306bb7dc6ec6681f25c781c
Piotr M <filo74b@gmail.com>
2024-11-22 03:52:58 +0100

    Time: 407 ms (5.56%) | Memory: 19.2 MB (49.75%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -1,13 +1,17 @@
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        count = defaultdict(int)
+        num_cols = len(matrix[0])
+        max_identical_rows = 0
 
-        for row in matrix:
-            row_key = tuple(row)
+        for current_row in matrix:
+            flipped_row = [1 - x for x in current_row]
 
-            if row[0]:
-                row_key = tuple([0 if n else 1 for n in row])
+            identical_row_count = sum(
+                1
+                for compare_row in matrix
+                if compare_row == current_row or compare_row == flipped_row
+            )
 
-            count[row_key] += 1
+            max_identical_rows = max(max_identical_rows, identical_row_count)
 
-        return max(count.values())
+        return max_identical_rows
