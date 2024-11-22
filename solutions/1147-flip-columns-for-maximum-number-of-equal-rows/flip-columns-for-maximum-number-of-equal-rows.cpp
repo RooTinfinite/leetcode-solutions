76c80b829508cc87b771789cb6ea100b7a9b1189
Piotr M <filo74b@gmail.com>
2024-11-22 02:47:50 +0100

    Time: 15 ms (85.51%) | Memory: 74.4 MB (73.51%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
-    int maxEqualRowsAfterFlips(vector<vector<int>>& mat) {
-        int res = 0;
-        const int n = mat[0].size();
-        unordered_map<string, int> freq;
-        string pattern;
-        pattern.reserve(n);
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        unordered_map<string, int> count;
         
-        for (const auto& row : mat) {
-            pattern.clear();
-            const int firstBit = row[0];
+        for (const auto& row : matrix) {
+            string key;
+            bool flip = row[0] == 1;
             
-            int i = 0;
-            for (; i + 7 < n; i += 8) {
-                char packed = 0;
-                for (int j = 0; j < 8; j++) {
-                    if (row[i + j] != firstBit) {
-                        packed |= (1 << j);
-                    }
-                }
-                pattern += packed;
+            for (int num : row) {
+                key += flip ? ('0' + (1 - num)) : ('0' + num);
             }
             
-            if (i < n) {
-                char packed = 0;
-                for (int j = 0; i + j < n; j++) {
-                    if (row[i + j] != firstBit) {
-                        packed |= (1 << j);
-                    }
-                }
-                pattern += packed;
-            }
-            
-            res = max(res, ++freq[pattern]);
+            count[key]++;
         }
-        return res;
+        
+        int maxCount = 0;
+        for (const auto& pair : count) {
+            maxCount = max(maxCount, pair.second);
+        }
+        
+        return maxCount;
     }
 };
