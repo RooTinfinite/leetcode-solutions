f6b7ca5103cea4f1f0ea6346d27b856f30351ddb
Piotr M <filo74b@gmail.com>
2024-11-22 01:27:26 +0100

    Time: 35 ms (80.00%) | Memory: 58.1 MB (73.91%) - LeetSync

---
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.js
@@ -0,0 +1,15 @@
+// JavaScript
+
+var maxEqualRowsAfterFlips = function(mat) {
+    const patFreq = new Map();
+    
+    for (const row of mat) {
+        const pattern = row[0] === 0 
+            ? row.join('')
+            : row.map(bit => bit ^ 1).join('');
+            
+        patFreq.set(pattern, (patFreq.get(pattern) || 0) + 1);
+    }
+    
+    return Math.max(...patFreq.values());
+};
