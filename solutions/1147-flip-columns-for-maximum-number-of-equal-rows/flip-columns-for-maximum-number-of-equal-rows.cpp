f52ec8d26ce5b8022e69d210d650ed481283bebf
Piotr M <filo74b@gmail.com>
2024-11-22 01:51:20 +0100

    Time: 10 ms (91.30%) | Memory: 70 MB (84.33%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int maxEqualRowsAfterFlips(vector<vector<int>>& mat) {
-        unordered_map<string, int> patFreq;
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        unordered_map<string, int> map;
+        int maxCount = 0;
         
-        for (const auto& row : mat) {
-            string pattern;
-            if (row[0] == 0) {
-                for (int bit : row) pattern += to_string(bit);
-            } else {
-                for (int bit : row) pattern += to_string(bit ^ 1);
+        for (const auto& row : matrix) {
+            string pattern(m, '0');
+            int firstBit = row[0];
+            
+            for (int j = 0; j < m; j++) {
+                if (row[j] != firstBit) {
+                    pattern[j] = '1';
+                }
             }
-            patFreq[pattern]++;
+            
+            maxCount = max(maxCount, ++map[pattern]);
         }
         
-        int maxFreq = 0;
-        for (const auto& pair : patFreq) {
-            maxFreq = max(maxFreq, pair.second);
-        }
-        return maxFreq;
+        return maxCount;
     }
 };
