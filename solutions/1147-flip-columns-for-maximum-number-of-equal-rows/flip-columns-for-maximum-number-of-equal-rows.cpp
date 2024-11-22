a4f249deda1fbcfdec481d13925e32b020d76431
Piotr M <filo74b@gmail.com>
2024-11-22 04:09:48 +0100

    Time: 0 ms (100.00%) | Memory: 67.2 MB (95.52%) - LeetSync

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -4,20 +4,29 @@ public:
         const int r = matrix.size(), c = matrix[0].size();
         unordered_map<bitset<300>, int, hash<bitset<300>>> mp(r);
         bitset<300> pattern;
+        int max_rows = 0;
 
         for (int i = 0; i < r; i++) {
             const bool term0 = matrix[i][0];
-            #pragma unroll
-            for (int j = 0; j < c; j++) 
+            int j = 0;
+            
+            for (; j + 7 < c; j += 8) {
+                unsigned char packed = 0;
+                for (int k = 0; k < 8; k++) {
+                    if (matrix[i][j + k] ^ term0) {
+                        packed |= (1 << k);
+                    }
+                }
+                for (int k = 0; k < 8; k++) {
+                    pattern[j + k] = (packed >> k) & 1;
+                }
+            }
+            for (; j < c; j++) {
                 pattern[j] = matrix[i][j] ^ term0;
-            ++mp[pattern];
+            }
+            max_rows = max(max_rows, ++mp[pattern]);
             pattern.reset();
         }
-
-        int max_rows = 0;
-        for (const auto& p : mp)
-            max_rows = max_rows < p.second ? p.second : max_rows;
-
         return max_rows;
     }
 };
