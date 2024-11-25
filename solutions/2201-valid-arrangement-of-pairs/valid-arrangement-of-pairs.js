4a488b1b40a363f40ea3ecfe19d0690fa769a0da
Piotr M <filo74b@gmail.com>
2024-11-25 13:57:25 +0100

    Time: 340 ms (60.00%) | Memory: 123.2 MB (20.00%) - LeetSync

--- solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.js
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.js
@@ -1,33 +1,36 @@
+const packDGInOutDegreeMap = (gm, edges, dm) => { for (const [u, v] of edges) { if (!gm.has(u)) gm.set(u, []); gm.get(u).push(v); dm.set(u, (dm.get(u) || 0) + 1); dm.set(v, (dm.get(v) || 0) - 1); } };
+
+/**
+ * @param {number[][]} pairs
+ * @return {number[][]}
+ */
 const validArrangement = (pairs) => {
-    // Build graph and degrees
-    const graph = new Map();
-    const inDegree = new Map();
-    
-    for (const [from, to] of pairs) {
-        if (!graph.has(from)) graph.set(from, []);
-        graph.get(from).push(to);
-        inDegree.set(to, (inDegree.get(to) || 0) + 1);
+    let g = new Map(), deg = new Map(), res = [];
+    packDGInOutDegreeMap(g, pairs, deg);
+    let start = -1;
+    for (const [node, ] of deg) { // looking for starting node
+        if (start == -1 || deg.get(node) == 1) start = node;
     }
-    
-    // Find start node
-    let start = pairs[0][0];
-    for (const [vertex, edges] of graph) {
-        if (edges.length - (inDegree.get(vertex) || 0) === 1) {
-            start = vertex;
-            break;
+    let path = eulerianPath(g, start);
+    path.reverse();
+    for (let i = 1; i < path.length; i++) {
+       res.push([path[i-1], path[i]]);
+    }
+    return res;
+};
+
+const eulerianPath = (g, start) => { // eulerian Path with Hierholzer’s Algorithm
+    let st = [start], path = [];
+    while (st.length) {
+        let u = st[st.length - 1], ua = g.get(u) || [];
+        if (ua.length) {
+            let v = ua.pop();
+            g.set(u, ua);
+            st.push(v);
+        } else {
+            path.push(u);
+            st.pop();
         }
     }
-    
-    // Hierholzer's algorithm implementation
-    const path = [];
-    const traverse = (vertex) => {
-        while (graph.has(vertex) && graph.get(vertex).length) {
-            const next = graph.get(vertex).pop();
-            traverse(next);
-            path.push([vertex, next]);
-        }
-    };
-    
-    traverse(start);
-    return path.reverse();
+    return path;
 };
