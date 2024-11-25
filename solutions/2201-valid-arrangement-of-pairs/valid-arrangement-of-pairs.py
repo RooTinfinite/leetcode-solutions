888b76490b4ff3e239b30843ed0931524f6b35bc
Piotr M <filo74b@gmail.com>
2024-11-25 12:14:32 +0100

    Time: 308 ms (83.70%) | Memory: 76.1 MB (94.43%) - LeetSync

--- solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
@@ -1,23 +1,38 @@
+from collections import defaultdict
+from typing import List
+
 class Solution:
-  def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-    # adjacency list, net out degree
-    G, D = defaultdict(list), defaultdict(int) 
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        # Create adjacency list and track in/out degrees
+        graph = defaultdict(list)
+        degree = defaultdict(int)
+        
+        # Build graph in one pass
+        for u, v in pairs:
+            graph[u].append(v)
+            degree[u] += 1
+            degree[v] -= 1
+        
+        # Find starting node - first node with out degree > in degree
+        start = pairs[0][0]  # Default to first node
+        for node, deg in degree.items():
+            if deg == 1:
+                start = node
+                break
+        
+        # Iterative Euler path implementation using stack
+        stack = [start]
+        path = []
+        
+        while stack:
+            curr = stack[-1]
+            if graph[curr]:
+                next_node = graph[curr].pop()
+                stack.append(next_node)
+            else:
+                node = stack.pop()
+                if stack:
+                    path.append([stack[-1], node])
+        
+        return path[::-1]
 
-    for u, v in pairs:
-      G[u].append(v)
-      D[u] += 1
-      D[v] -= 1
-
-    for node in D:
-      if D[node] == 1:
-        break
-
-    res = []
-    def dfs(node):
-      while G[node]:
-        nbr = G[node].pop()
-        dfs(nbr)
-        res.append((node, nbr))
-
-    dfs(node)
-    return res[::-1]
