2fc239e83968b3a066d7b842bb7cb396b9b735c7
Piotr M <filo74b@gmail.com>
2024-11-25 13:07:40 +0100

    Time: 187 ms (100.00%) | Memory: 58.8 MB (28.57%) - LeetSync

--- solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.swift
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.swift
@@ -1,37 +1,35 @@
 class Solution {
-    func validArrangement(_ pairs: [[Int]]) -> [[Int]] {
-        let capacity = pairs.count
-        var adjacencyList = Dictionary<Int, [Int]>(minimumCapacity: capacity)
-        var inOutDegree = Dictionary<Int, Int>(minimumCapacity: capacity * 2)
-        
-        for pair in pairs {
-            adjacencyList[pair[0], default: []].append(pair[1])
-            inOutDegree[pair[0], default: 0] += 1
-            inOutDegree[pair[1], default: 0] -= 1
+    func validArrangement(_ pairs: [[Int]]) -> [[Int]]
+    {
+        // build a graph
+        var adjList: [Int:[Int]] = [:]
+        var inDegree: [Int:Int] = [:]
+        for pair in pairs
+        {
+            adjList[pair[0], default:[]].append(pair[1])
+            inDegree[pair[1], default:0] += 1
         }
-        
-        // start node finding
-        let startNode = inOutDegree.first { $0.value == 1 }?.key ?? pairs[0][0]
-        
-        var path = [Int]()
-        path.reserveCapacity(capacity + 1)
-        var nodeStack = [Int]()
-        nodeStack.reserveCapacity(capacity)
-        nodeStack.append(startNode)
-        
-        while let currentNode = nodeStack.last {
-            guard var neighbors = adjacencyList[currentNode], !neighbors.isEmpty else {
-                path.append(nodeStack.removeLast())
-                continue
+
+        var start = pairs[0][0] // start anywhere if it's an Eulerian cycle
+        for (v, outEdges) in adjList
+        {
+            if outEdges.count - inDegree[v, default:0] == 1 { 
+                start = v; break // it's an Eulerian trail, have to start here
             }
-            
-            let nextNode = neighbors.removeLast()
-            adjacencyList[currentNode] = neighbors
-            nodeStack.append(nextNode)
         }
-        
-        return stride(from: path.count - 1, to: 0, by: -1).map { i in
-            [path[i], path[i-1]]
+
+        // Hierholzer's algorithm
+        var path: [[Int]] = []
+        func traverse(from vertex: Int)
+        {
+            while let next = adjList[vertex]?.popLast()
+            {
+                traverse(from: next)
+                path.append([vertex, next])
+            }
         }
+        traverse(from: start)
+        path.reverse()
+        return path
     }
 }
