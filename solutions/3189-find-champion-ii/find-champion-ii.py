bbf12c2868203f21bca9c932ea4c71608e901257
Piotr M <filo74b@gmail.com>
2024-11-26 00:48:20 +0100

    Time: 4 ms (87.42%) | Memory: 17.6 MB (80.31%) - LeetSync

--- solutions/3189-find-champion-ii/find-champion-ii.py
+++ solutions/3189-find-champion-ii/find-champion-ii.py
@@ -1,14 +1,19 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        can_win = [True] * n
-        for a, b in edges:
-            can_win[b] = False
-        winner = -1
-        winner_count = 0
-        for i in range(0, n):
-            if can_win[i]:
-                winner = i
-                winner_count += 1
-        if winner_count == 1:
-            return winner
+        isUndefeated = [True] * n
+        
+        for winner, loser in edges:
+            isUndefeated[loser] = False
+            
+        champion = -1
+        championCount = 0
+        
+        for team in range(n):
+            if isUndefeated[team]:
+                champion = team
+                championCount += 1
+                
+        if championCount == 1:
+            return champion
+            
         return -1
