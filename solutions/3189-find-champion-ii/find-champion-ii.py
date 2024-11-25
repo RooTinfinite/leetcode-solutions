e1799560688d25be786238e37f8a685301659a1b
Piotr M <filo74b@gmail.com>
2024-11-26 00:47:08 +0100

    Time: 7 ms (68.21%) | Memory: 17.6 MB (80.31%) - LeetSync

--- solutions/3189-find-champion-ii/find-champion-ii.py
+++ solutions/3189-find-champion-ii/find-champion-ii.py
@@ -1,12 +1,14 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        
-        if not edges: return (n==1) -1
-
-        _, sup = zip(*edges)
- 
-        roots = set(range(n))- set(sup)
-        
-        if len(roots) == 1: return roots.pop()
-        
+        can_win = [True] * n
+        for a, b in edges:
+            can_win[b] = False
+        winner = -1
+        winner_count = 0
+        for i in range(0, n):
+            if can_win[i]:
+                winner = i
+                winner_count += 1
+        if winner_count == 1:
+            return winner
         return -1
