5642029faca59b02d0b5bbd4065597ec6c205ccc
Piotr M <filo74b@gmail.com>
2024-11-25 13:10:47 +0100

    Time: 473 ms (50.00%) | Memory: 60.3 MB (14.29%) - LeetSync

--- solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.swift
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.swift
@@ -1,46 +1,32 @@
 class Solution {
     func validArrangement(_ pairs: [[Int]]) -> [[Int]] {
-        // Build adjacency list and track degrees
-        var adjacencyList: [Int: [Int]] = [:]
-        var inOutDegree: [Int: Int] = [:]
+        var adjList = Dictionary<Int, [Int]>(minimumCapacity: pairs.count)
+        var inDegree = Dictionary<Int, Int>(minimumCapacity: pairs.count)
         
-        // Construct graph
         for pair in pairs {
-            adjacencyList[pair[0], default: []].append(pair[1])
-            inOutDegree[pair[0], default: 0] += 1
-            inOutDegree[pair[1], default: 0] -= 1
+            adjList[pair[0], default: []].append(pair[1])
+            inDegree[pair[1], default: 0] += 1
         }
         
-        // Find starting node
-        var startNode = pairs[0][0]
-        for (node, degree) in inOutDegree {
-            if degree == 1 {
-                startNode = node
-                break
+
+        let start = adjList.first { vertex in
+            vertex.value.count - inDegree[vertex.key, default: 0] == 1
+        }?.key ?? pairs[0][0]
+        
+        var path = [[Int]]()
+        path.reserveCapacity(pairs.count)
+        
+        func traverse(from vertex: Int) {
+            while var edges = adjList[vertex], !edges.isEmpty {
+                let next = edges.removeLast()
+                adjList[vertex] = edges
+                traverse(from: next)
+                path.append([vertex, next])
             }
         }
         
-        var path: [Int] = []
-        var nodeStack: [Int] = [startNode]
-        
-        while !nodeStack.isEmpty {
-            if var neighbors = adjacencyList[nodeStack.last!], !neighbors.isEmpty {
-                let nextNode = neighbors.removeLast()
-                adjacencyList[nodeStack.last!] = neighbors
-                nodeStack.append(nextNode)
-            } else {
-                path.append(nodeStack.removeLast())
-            }
-        }
-        
-        // Construct final arrangement
-        var arrangement: [[Int]] = []
-        arrangement.reserveCapacity(path.count - 1)
-        
-        for i in stride(from: path.count - 1, to: 0, by: -1) {
-            arrangement.append([path[i], path[i-1]])
-        }
-        
-        return arrangement
+        traverse(from: start)
+        path.reverse()
+        return path
     }
 }
