48af8616c902c7c8fee984532a66f0e39ed4dffd
Piotr M <filo74b@gmail.com>
2024-11-25 13:12:13 +0100

    Time: 422 ms (75.00%) | Memory: 44.4 MB (100.00%) - LeetSync

--- solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.swift
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.swift
@@ -1,32 +1,41 @@
+// + Hierholzers algorithm
 class Solution {
     func validArrangement(_ pairs: [[Int]]) -> [[Int]] {
-        var adjList = Dictionary<Int, [Int]>(minimumCapacity: pairs.count)
-        var inDegree = Dictionary<Int, Int>(minimumCapacity: pairs.count)
+        var adjList = [Int: [Int]]()
+        var inDegree = [Int: Int]()
         
+        // Build graph
         for pair in pairs {
             adjList[pair[0], default: []].append(pair[1])
             inDegree[pair[1], default: 0] += 1
         }
         
-
-        let start = adjList.first { vertex in
-            vertex.value.count - inDegree[vertex.key, default: 0] == 1
-        }?.key ?? pairs[0][0]
-        
-        var path = [[Int]]()
-        path.reserveCapacity(pairs.count)
-        
-        func traverse(from vertex: Int) {
-            while var edges = adjList[vertex], !edges.isEmpty {
-                let next = edges.removeLast()
-                adjList[vertex] = edges
-                traverse(from: next)
-                path.append([vertex, next])
+        // Find start vertex
+        var start = pairs[0][0]
+        for (v, outEdges) in adjList {
+            if outEdges.count - (inDegree[v] ?? 0) == 1 {
+                start = v
+                break
             }
         }
         
-        traverse(from: start)
-        path.reverse()
-        return path
+        var path = [[Int]]()
+        var stack = [start]
+        
+        while !stack.isEmpty {
+            let current = stack.last!
+            if var neighbors = adjList[current], !neighbors.isEmpty {
+                let next = neighbors.removeLast()
+                adjList[current] = neighbors
+                stack.append(next)
+            } else {
+                let vertex = stack.removeLast()
+                if !stack.isEmpty {
+                    path.append([stack.last!, vertex])
+                }
+            }
+        }
+        
+        return path.reversed()
     }
 }
