8db30b6a51e0cee21a7e2bf7bac370560f37dc10
Piotr M <filo74b@gmail.com>
2024-11-25 13:49:19 +0100

    Time: 262 ms (100.00%) | Memory: 283.2 MB (100.00%) - LeetSync

---
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.rb
@@ -0,0 +1,40 @@
+def valid_arrangement(pairs)
+    adjacency_list = Hash.new { |h, k| h[k] = [] }
+    in_out_degree = Hash.new(0)
+    
+    pairs.each do |pair|
+        adjacency_list[pair[0]] << pair[1]
+        in_out_degree[pair[0]] += 1
+        in_out_degree[pair[1]] -= 1
+    end
+    
+    start_node = pairs[0][0]
+    in_out_degree.each do |node, degree|
+        if degree == 1
+            start_node = node
+            break
+        end
+    end
+    
+    path = []
+    node_stack = [start_node]
+    
+    while !node_stack.empty?
+        neighbors = adjacency_list[node_stack.last]
+        if neighbors.empty?
+            path << node_stack.pop
+        else
+            next_node = neighbors.pop
+            node_stack << next_node
+        end
+    end
+    
+    arrangement = []
+    path_size = path.size
+    
+    (path_size - 1).downto(1) do |i|
+        arrangement << [path[i], path[i-1]]
+    end
+    
+    arrangement
+end
