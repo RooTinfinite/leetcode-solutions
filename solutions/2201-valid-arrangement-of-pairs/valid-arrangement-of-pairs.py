a56568eb77e8d430f495b32b32f495a8c7482300
Piotr M <filo74b@gmail.com>
2024-11-25 12:16:56 +0100

    Time: 310 ms (83.15%) | Memory: 94 MB (33.77%) - LeetSync

--- solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
+++ solutions/2201-valid-arrangement-of-pairs/valid-arrangement-of-pairs.py
@@ -1,37 +1,25 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-        # Create detailed graph structures
-        graph = defaultdict(list)
-        in_degree = defaultdict(int)
-        out_degree = defaultdict(int)
+        G = defaultdict(list)
+        D = defaultdict(int)
         
-        # Build comprehensive graph information
-        for start, end in pairs:
-            graph[start].append(end)
-            out_degree[start] += 1
-            in_degree[end] += 1
+        for u, v in pairs:
+            G[u].append(v)
+            D[u] = (D[u] + 1) & 0xFFFFFFFF
+            D[v] = (D[v] - 1) & 0xFFFFFFFF
         
-        # Carefully find the starting node
-        start_node = pairs[0][0]
-        for node in graph:
-            if out_degree[node] - in_degree[node] == 1:
-                start_node = node
+        start = pairs[0][0]
+        for node in D:
+            if D[node] == 1:
+                start = node
                 break
         
-        result = []
+        res = []
+        def dfs(node):
+            while G[node]:
+                nbr = G[node].pop()
+                dfs(nbr)
+                res.append([node, nbr])
         
-        # Methodical depth-first traversal
-        def explore_path(current_node):
-            while graph[current_node]:
-                next_node = graph[current_node].pop()
-                explore_path(next_node)
-                result.append([current_node, next_node])
-        
-        # Execute traversal
-        explore_path(start_node)
-        
-        # Return reversed path for correct order
-        return result[::-1]
+        dfs(start)
+        return res[::-1]
