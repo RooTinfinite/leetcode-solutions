49cf53ecd9e2c42453f6987ae03bf8405d7f645f
Piotr M <filo74b@gmail.com>
2024-11-26 01:38:13 +0100

    Time: 0 ms (100.00%) | Memory: 93.1 MB (93.65%) - LeetSync

--- solutions/3189-find-champion-ii/find-champion-ii.cpp
+++ solutions/3189-find-champion-ii/find-champion-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<bool> isUndefeated(n, true);
+        vector<int> losses(n);
         
         for (const auto& edge : edges) {
-            int winner = edge[0];
-            int loser = edge[1];
-            isUndefeated[loser] = false;
+            losses[edge[1]]++;
         }
         
         int champion = -1;
-        int championCount = 0;
-        
-        for (int team = 0; team < n; team++) {
-            if (isUndefeated[team]) {
-                champion = team;
-                championCount++;
+        for (int i = 0; i < n; i++) {
+            if (losses[i] == 0) {
+                if (champion != -1) return -1;
+                champion = i;
             }
         }
         
-        return championCount == 1 ? champion : -1;
+        return champion;
     }
 };
