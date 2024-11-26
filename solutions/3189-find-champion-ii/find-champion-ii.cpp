a3861fc614cac2fd1aeeff9f2bba740fc2735af8
Piotr M <filo74b@gmail.com>
2024-11-26 03:21:09 +0100

    Time: 0 ms (100.00%) | Memory: 94 MB (66.24%) - LeetSync

--- solutions/3189-find-champion-ii/find-champion-ii.cpp
+++ solutions/3189-find-champion-ii/find-champion-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<int> incoming(n, 0);
+        bitset<10000> losses; 
         
         for (const auto& edge : edges) {
-            incoming[edge[1]]++;
+            losses.set(edge[1]);
         }
         
-        vector<int> champions;
+        int champion = -1;
         for (int i = 0; i < n; i++) {
-            if (incoming[i] == 0) {
-                champions.push_back(i);
+            if (!losses[i]) {
+                if (champion != -1) return -1;
+                champion = i;
             }
         }
         
-        if (champions.size() > 1) {
-            return -1;
-        }
-        
-        return champions[0];
+        return champion;
     }
 };
