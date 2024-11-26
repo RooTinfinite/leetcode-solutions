cfb7609846c30bd4d91d90e4af79efd771aba6f7
Piotr M <filo74b@gmail.com>
2024-11-26 02:10:11 +0100

    Time: 4 ms (53.85%) | Memory: 2.4 MB (33.33%) - LeetSync

--- solutions/3189-find-champion-ii/find-champion-ii.rs
+++ solutions/3189-find-champion-ii/find-champion-ii.rs
@@ -1,24 +1,23 @@
 impl Solution {
     pub fn find_champion(n: i32, edges: Vec<Vec<i32>>) -> i32 {
         let n = n as usize;
-        let mut is_undefeated = vec![true; n];
+        let mut incoming = vec![0; n];
         
         for edge in edges {
-            let winner = edge[0] as usize;
-            let loser = edge[1] as usize;
-            is_undefeated[loser] = false;
+            incoming[edge[1] as usize] += 1;
         }
         
-        let mut champion = -1;
-        let mut champion_count = 0;
-        
-        for team in 0..n {
-            if is_undefeated[team] {
-                champion = team as i32;
-                champion_count += 1;
-            }
+        let champions: Vec<i32> = incoming
+            .iter()
+            .enumerate()
+            .filter(|(_, &count)| count == 0)
+            .map(|(i, _)| i as i32)
+            .collect();
+            
+        if champions.len() > 1 {
+            -1
+        } else {
+            champions[0]
         }
-        
-        if champion_count == 1 { champion } else { -1 }
     }
 }
