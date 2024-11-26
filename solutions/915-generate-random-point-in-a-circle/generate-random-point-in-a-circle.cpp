d256643f315d299f79e1dbee76c008265d63fe77
Piotr M <filo74b@gmail.com>
2024-11-26 04:00:13 +0100

    Time: 11 ms (70.34%) | Memory: 33.5 MB (11.11%) - LeetSync

---
+++ solutions/915-generate-random-point-in-a-circle/generate-random-point-in-a-circle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double radius, x_center, y_center; 
+    Solution(double radius, double x_center, double y_center){
+        this->radius = radius;
+        this->x_center = x_center;
+        this->y_center = y_center;
+    } 
+    vector<double> randPoint() { 
+        double r = sqrt((double)rand() / RAND_MAX) * radius;  
+        double theta = ((double)rand() / RAND_MAX) * 2 * M_PI; 
+        double x = x_center + r * cos(theta);
+        double y = y_center + r * sin(theta); 
+        return {x, y};
+    }
+};
