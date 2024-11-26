1cee838180ee039bd21ba3dbb6c41863996a6337
Piotr M <filo74b@gmail.com>
2024-11-26 03:27:45 +0100

    Time: 0 ms (100.00%) | Memory: 92.7 MB (100.00%) - LeetSync

--- solutions/3189-find-champion-ii/find-champion-ii.cpp
+++ solutions/3189-find-champion-ii/find-champion-ii.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        bitset<100> losses; 
+        unsigned long long losses1 = 0;  // Bits 0-63
+        unsigned long long losses2 = 0;  // Bits 64-99
         
         for (const auto& edge : edges) {
-            losses.set(edge[1]);
+            if (edge[1] < 64) {
+                losses1 |= (1ULL << edge[1]);
+            } else {
+                losses2 |= (1ULL << (edge[1] - 64));
+            }
         }
         
         int champion = -1;
         for (int i = 0; i < n; i++) {
-            if (!losses[i]) {
+            bool hasLost = (i < 64) ? 
+                (losses1 & (1ULL << i)) : 
+                (losses2 & (1ULL << (i - 64)));
+                
+            if (!hasLost) {
                 if (champion != -1) return -1;
                 champion = i;
             }
