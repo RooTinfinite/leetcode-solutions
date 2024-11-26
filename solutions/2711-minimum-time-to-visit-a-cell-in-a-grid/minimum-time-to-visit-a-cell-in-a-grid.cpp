d0d0615eb1eb5003633f5abe97da2cf496448f20
Piotr M <filo74b@gmail.com>
2024-11-26 23:54:47 +0100

    Time: 134 ms (99.20%) | Memory: 51.3 MB (92.13%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -5,24 +5,21 @@ public:
         
         int rows = grid.size();
         int cols = grid[0].size();
-        int maxPos = rows * cols;
         
-        priority_queue<pair<int, int>, 
-                      vector<pair<int, int>>, 
-                      greater<pair<int, int>>> minHeap;
+        priority_queue<pair<int, pair<int, int>>, 
+                      vector<pair<int, pair<int, int>>>, 
+                      greater<pair<int, pair<int, int>>>> minHeap;
         
-        minHeap.push({0, 0});
+        minHeap.push({0, {0, 0}}); // time, {row, col}
         
-        vector<bool> seen(maxPos, false); 
-        seen[0] = true;
+        vector<vector<bool>> seen(rows, vector<bool>(cols, false));
+        seen[0][0] = true;
         
         const int moves[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         
         while (!minHeap.empty()) {
-            auto [currTime, pos] = minHeap.top();
-            int currRow = pos / cols;
-            int currCol = pos % cols;
-            
+            auto [currTime, coord] = minHeap.top();
+            auto [currRow, currCol] = coord;
             minHeap.pop();
             
             if (currRow == rows - 1 && currCol == cols - 1) 
@@ -31,17 +28,16 @@ public:
             for (const auto& move : moves) {
                 int nextRow = move[0] + currRow;
                 int nextCol = move[1] + currCol;
-                int nextPos = nextRow * cols + nextCol;
                 
                 if (nextRow >= 0 && nextCol >= 0 && 
                     nextRow < rows && nextCol < cols && 
-                    !seen[nextPos]) {
+                    !seen[nextRow][nextCol]) {
                     
                     int waitTime = ((grid[nextRow][nextCol] - currTime) % 2 == 0) ? 1 : 0;
                     int nextTime = max(currTime + 1, grid[nextRow][nextCol] + waitTime);
                     
-                    minHeap.push({nextTime, nextPos});
-                    seen[nextPos] = true;
+                    minHeap.push({nextTime, {nextRow, nextCol}});
+                    seen[nextRow][nextCol] = true;
                 }
             }
         }
