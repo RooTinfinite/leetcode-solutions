3b64784f44b2e405f1a2d3103b06f1c564b54cda
Piotr M <filo74b@gmail.com>
2024-11-27 01:21:58 +0100

    Time: 47 ms (100.00%) | Memory: 17.6 MB (8.31%) - LeetSync

--- solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
+++ solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.py
@@ -1,26 +1,27 @@
 class Solution:
+    def dfs(self, tree, c, dp):
+        d = dp[c] + 1
+        for x in tree[c]:
+            if dp[x] <= d:
+                continue
+            dp[x] = d
+            self.dfs(tree, x, dp)
+    
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        dp = [n-1-i for i in range(n)]
+        tree = [[] for _ in range(n)]
         
-        adj = [[] for _ in range(n)]
         for i in range(n-1):
-            adj[i].append(i+1)
-
-        res = []
-        for i,j in queries:
-            adj[i].append(j) 
-            res.append(self.bellman(adj))
+            tree[i+1].append(i)
+            
+        m = len(queries)
+        res = [0] * m
+        
+        for i, q in enumerate(queries):
+            a, b = q[0], q[1]
+            tree[b].append(a)
+            dp[a] = min(dp[a], dp[b] + 1)
+            self.dfs(tree, a, dp)
+            res[i] = dp[0]
             
         return res
-    
-    def bellman(self, adj):
-        n = len(adj)
-        dist = [float('inf')]*n
-        dist[0] = 0
-
-        for u in range(n):
-            for v in adj[u]:
-                if dist[u] + 1 < dist[v]:
-                    dist[v] = dist[u] + 1
-        
-        return dist[-1]
-
