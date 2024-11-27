0bca0761dbdcd3c6eb2795f84080c0df96623f3f
Piotr M <filo74b@gmail.com>
2024-11-27 01:28:57 +0100

    Time: 10 ms (99.03%) | Memory: 45.8 MB (15.95%) - LeetSync

--- solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
+++ solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.java
@@ -1,41 +1,44 @@
 class Solution {
-    private void dfs(List<List<Integer>> tree, int c, int[] dp) {
-        int d = dp[c] + 1;
-        for (int x : tree.get(c)) {
-            if (dp[x] <= d) continue;
-            dp[x] = d;
-            dfs(tree, x, dp);
+    private void updateDistances(List<List<Integer>> graph, int current, int[] distances) {
+        int newDist = distances[current] + 1;
+        
+        for (int neighbor : graph.get(current)) {
+            if (distances[neighbor] <= newDist) continue;
+            
+            distances[neighbor] = newDist;
+            updateDistances(graph, neighbor, distances);
         }
     }
     
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) {
-            dp[i] = n - 1 - i;
+        int[] distances = new int[n];
+        for (int i = 0; i < n; ++i) {
+            distances[i] = n - 1 - i;
         }
         
-        List<List<Integer>> tree = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            tree.add(new ArrayList<>());
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            graph.add(new ArrayList<>());
         }
         
-        for (int i = 0; i < n - 1; i++) {
-            tree.get(i + 1).add(i);
+        for (int i = 0; i + 1 < n; ++i) {
+            graph.get(i + 1).add(i);
         }
         
-        int m = queries.length;
-        int[] res = new int[m];
+        int[] answer = new int[queries.length];
+        int queryIdx = 0;
         
-        for (int i = 0; i < m; i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            tree.get(b).add(a);
-            dp[a] = Math.min(dp[a], dp[b] + 1);
-            dfs(tree, a, dp);
-            res[i] = dp[0];
+        for (int[] query : queries) {
+            int source = query[0];
+            int target = query[1];
+            
+            graph.get(target).add(source);
+            distances[source] = Math.min(distances[source], distances[target] + 1);
+            updateDistances(graph, source, distances);
+            
+            answer[queryIdx++] = distances[0];
         }
         
-        return res;
+        return answer;
     }
 }
-
