0b6552f6c5cc3e5bb120422854c86394c8f8aa9b
Piotr M <filo74b@gmail.com>
2024-11-27 01:49:58 +0100

    Time: 11 ms (18.18%) | Memory: 4.4 MB (36.36%) - LeetSync

--- solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.rs
+++ solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.rs
@@ -1,39 +1,36 @@
 impl Solution {
-    fn update_distances(graph: &Vec<Vec<usize>>, current: usize, distances: &mut Vec<usize>) {
-        let new_dist = distances[current] + 1;
-        
-        for &neighbor in &graph[current] {
-            if distances[neighbor] <= new_dist {
-                continue;
-            }
-            
-            distances[neighbor] = new_dist;
-            Self::update_distances(graph, neighbor, distances);
-        }
-    }
-
     pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let n = n as usize;
-        let mut distances: Vec<usize> = (0..n).map(|i| n - 1 - i).collect();
+        let mut distances = (0..n).collect::<Vec<_>>();
+        let mut successors = Vec::with_capacity(n);
         
-        let mut graph: Vec<Vec<usize>> = vec![Vec::new(); n];
-        for i in 0..n-1 {
-            graph[i + 1].push(i);
+        for i in 0..n {
+            let capacity = if i < n-1 { 1 } else { 0 };
+            successors.push(Vec::with_capacity(capacity + queries.len()));
+            if i < n-1 {
+                successors[i].push(i+1);
+            }
         }
         
-        let mut answer = Vec::new();
+        let mut result = Vec::with_capacity(queries.len());
+        let mut to_update = Vec::with_capacity(n);
         
-        for query in queries {
-            let source = query[0] as usize;
-            let target = query[1] as usize;
+        for query in &queries {
+            let (source, target) = (query[0] as usize, query[1] as usize);
+            successors[source].push(target);
+            to_update.push((source, target));
             
-            graph[target].push(source);
-            distances[source] = distances[source].min(distances[target] + 1);
-            Self::update_distances(&graph, source, &mut distances);
+            while let Some((u, v)) = to_update.pop() {
+                if distances[v] <= distances[u] + 1 {
+                    continue;
+                }
+                distances[v] = distances[u] + 1;
+                to_update.extend(successors[v].iter().map(|&w| (v, w)));
+            }
             
-            answer.push(distances[0] as i32);
+            result.push(distances[n-1] as i32);
         }
         
-        answer
+        result
     }
 }
