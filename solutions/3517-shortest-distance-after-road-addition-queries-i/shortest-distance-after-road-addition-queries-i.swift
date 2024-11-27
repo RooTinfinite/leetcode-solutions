63dfced5d68189a46071fadea5f5135aa6afca34
Piotr M <filo74b@gmail.com>
2024-11-27 02:36:21 +0100

    Time: 220 ms (100.00%) | Memory: 18.2 MB (100.00%) - LeetSync

--- solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.swift
+++ solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.swift
@@ -1,38 +1,40 @@
 class Solution {
-    func updateDistances(_ graph: [[Int]], _ current: Int, _ distances: inout [Int]) {
-        let newDist = distances[current] + 1
-        
-        for neighbor in graph[current] {
-            if distances[neighbor] <= newDist {
-                continue
-            }
-            
-            distances[neighbor] = newDist
-            updateDistances(graph, neighbor, &distances)
-        }
-    }
-    
     func shortestDistanceAfterQueries(_ n: Int, _ queries: [[Int]]) -> [Int] {
-        var distances = (0..<n).map { n - 1 - $0 }
-        
-        var graph = Array(repeating: [Int](), count: n)
-        for i in 0..<(n-1) {
-            graph[i + 1].append(i)
+        var adj = Array(repeating: [Int](), count: n)
+        for i in 0..<n {
+            adj[i] = [i + 1]
         }
         
-        var answer = [Int]()
+        func shortestPath() -> Int {
+            var queue = [(0, 0)] // node, length
+            var visit = Set<Int>()
+            visit.insert(0)
+            var queueIndex = 0
+            
+            while queueIndex < queue.count {
+                let (cur, length) = queue[queueIndex]
+                queueIndex += 1
+                
+                if cur == n - 1 {
+                    return length
+                }
+                
+                for nei in adj[cur] {
+                    if !visit.contains(nei) {
+                        queue.append((nei, length + 1))
+                        visit.insert(nei)
+                    }
+                }
+            }
+            return -1
+        }
         
+        var res = [Int]()
         for query in queries {
-            let source = query[0]
-            let target = query[1]
-            
-            graph[target].append(source)
-            distances[source] = min(distances[source], distances[target] + 1)
-            updateDistances(graph, source, &distances)
-            
-            answer.append(distances[0])
+            let src = query[0], dst = query[1]
+            adj[src].append(dst)
+            res.append(shortestPath())
         }
-        
-        return answer
+        return res
     }
 }
