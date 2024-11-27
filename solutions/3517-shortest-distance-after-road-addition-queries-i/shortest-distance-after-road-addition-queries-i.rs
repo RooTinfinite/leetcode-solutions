136ec96cc640220a09ecb9aef485221c1bd73a55
Piotr M <filo74b@gmail.com>
2024-11-27 02:37:10 +0100

    Time: 158 ms (9.09%) | Memory: 2.6 MB (36.36%) - LeetSync

--- solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.rs
+++ solutions/3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.rs
@@ -1,39 +1,37 @@
-impl Solution {
-    fn update_distances(graph: &Vec<Vec<usize>>, current: usize, distances: &mut Vec<usize>) {
-        let new_dist = distances[current] + 1;
-        
-        for &neighbor in &graph[current] {
-            if distances[neighbor] <= new_dist {
-                continue;
-            }
-            
-            distances[neighbor] = new_dist;
-            Self::update_distances(graph, neighbor, distances);
-        }
-    }
+use std::collections::{VecDeque, HashSet};
 
+impl Solution {
     pub fn shortest_distance_after_queries(n: i32, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let n = n as usize;
-        let mut distances: Vec<usize> = (0..n).map(|i| n - 1 - i).collect();
+        let mut adj: Vec<Vec<i32>> = (0..n).map(|i| vec![(i + 1) as i32]).collect();
         
-        let mut graph: Vec<Vec<usize>> = vec![Vec::new(); n];
-        for i in 0..n-1 {
-            graph[i + 1].push(i);
-        }
+        let shortest_path = |adj: &Vec<Vec<i32>>| -> i32 {
+            let mut q = VecDeque::new();
+            q.push_back((0, 0)); // node, length
+            let mut visit = HashSet::new();
+            visit.insert(0);
+            
+            while let Some((cur, length)) = q.pop_front() {
+                if cur == (n - 1) as i32 {
+                    return length;
+                }
+                
+                for &nei in &adj[cur as usize] {
+                    if !visit.contains(&nei) {
+                        q.push_back((nei, length + 1));
+                        visit.insert(nei);
+                    }
+                }
+            }
+            -1
+        };
         
-        let mut answer = Vec::new();
-        
+        let mut res = Vec::new();
         for query in queries {
-            let source = query[0] as usize;
-            let target = query[1] as usize;
-            
-            graph[target].push(source);
-            distances[source] = distances[source].min(distances[target] + 1);
-            Self::update_distances(&graph, source, &mut distances);
-            
-            answer.push(distances[0] as i32);
+            let (src, dst) = (query[0] as usize, query[1]);
+            adj[src].push(dst);
+            res.push(shortest_path(&adj));
         }
-        
-        answer
+        res
     }
 }
