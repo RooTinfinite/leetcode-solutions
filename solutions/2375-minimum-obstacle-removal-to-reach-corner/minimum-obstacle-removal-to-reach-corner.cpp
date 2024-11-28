339ae1c9293d89ef0832c36d225535545540f244
Piotr M <filo74b@gmail.com>
2024-11-28 14:02:53 +0100

    Time: 20 ms (100.00%) | Memory: 91.9 MB (100.00%) - LeetSync

--- solutions/2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ solutions/2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,65 +1,80 @@
-using int2 = pair<int, int>; 
-const static int d[5] = {0, 1, 0, -1, 0};
+#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
+#define INL __attribute__((always_inline))
+#define HOT __attribute__((hot))
+#define INL_ATTR noexcept INL NO_SAN HOT
+#define OUTL_ATTR noexcept NO_SAN HOT
 
-class Solution {
-public:
-    inline static bool isOutside(int i, int j, int n, int m) {
-        return i < 0 || i >= n || j < 0 || j >= m;
+template <class T, class S, S cap>
+struct ring_queue {
+    S start, end;
+    T elem[cap];
+
+    ring_queue() INL_ATTR: start(0), end(0) {}
+
+    void push(const T v) INL_ATTR {
+        elem[end++] = v;
+        if (end >= cap) end = 0;
+        // assert(!empty());
     }
-    inline static int idx(int i, int j, int m) {
-        return i * m + j;
+
+    T pop() INL_ATTR {
+        const T r = elem[start++];
+        if (start >= cap) start = 0;
+        return r;
     }
 
-    static int minimumObstacles(vector<vector<int>>& grid) {
-        const int n = grid.size(), m = grid[0].size(), N=100000;
-        unsigned dist[N];
-        fill(dist, dist + n*m, INT_MAX);
-
-        int2 q[N]; 
-        int back = N/2, front = N/2; 
-
-        q[back++] = {0, 0}; 
-        dist[0] = 0;
-
-        while (back != front) {
-            auto [currD, ij] = q[front]; 
-            front=(front==N-1)?0:front+1;
-            auto [i, j] = div(ij, m);
-
-            if (i == n - 1 && j == m - 1)
-                return currD;
-
-            for (int a = 0; a < 4; a++) {
-                int r = i + d[a], s = j + d[a + 1];
-                if (isOutside(r, s, n, m))
-                    continue;
-
-                int nextD = currD + grid[r][s];
-                int rs = idx(r, s, m);
-
-                if (nextD < dist[rs]) {
-                    dist[rs] = nextD;
-                    int2 next = {dist[rs], rs};
-
-                    if (grid[r][s] == 0) {
-                        front=(front==0)?N-1:front-1;
-                        q[front] = next; 
-                    } 
-                    else {
-                        q[back] = next; 
-                        back=(back==N-1)?0:back+1;
-                    }
-                }
-            }
-        }
-        return -1;
+    bool empty() INL_ATTR {
+        return start == end;
     }
 };
 
+class Solution {
+public:
+    static int minimumObstacles(vector<vector<int>>& grid) OUTL_ATTR {
+        const uint m = grid.size(), n = grid.front().size();
+        const uint8_t nord = 32 - countl_zero(n);
+        const uint cmask = (1u << nord) - 1u, endcoo = ((m - 1u) << nord) + n - 1u;
+        constexpr uint QCAP = 100'000;
+        ring_queue<uint, uint, QCAP> que[2];
+        que[0].push(0);
+        uint cost = 0;
+        bool a = false;
+        grid.front().front() = -1;
+        while (!que[a].empty()) {
+            while (!que[a].empty()) {
+                const uint coo = que[a].pop();
+                const uint i = coo >> nord, j = coo & cmask;
+                if (int b = !!i, &g = grid[i-b][j]; b & g >= 0) {
+                    que[a^g].push(coo - cmask - 1u);
+                    g = -1;
+                }
+                if (int b = i + 1u < m, &g = grid[i+b][j]; b & g >= 0) {
+                    if (coo + cmask + 1u == endcoo) [[unlikely]]
+                        return cost + g;
+                    que[a^g].push(coo + cmask + 1u);
+                    g = -1;
+                }
+                if (int b = !!j, &g = grid[i][j-b]; b & g >= 0) {
+                    que[a^g].push(coo - 1u);
+                    g = -1;
+                }
+                if (int b = j + 1u < n, &g = grid[i][j+b]; b & g >= 0) {
+                    if (coo + 1u == endcoo) [[unlikely]]
+                        return cost + g;
+                    que[a^g].push(coo + 1u);
+                    g = -1;
+                }
+            }
+            cost++;
+            a = !a;
+        }
+        return -1; // unreachable
+    }
+};
 
 auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'a';
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
 }();
