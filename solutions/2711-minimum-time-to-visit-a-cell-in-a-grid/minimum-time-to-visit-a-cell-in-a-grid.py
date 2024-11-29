7c864b822cbeaaff4f04908e1a2d0cdbc04df05a
Piotr M <filo74b@gmail.com>
2024-11-29 21:24:39 +0100

    Time: 729 ms (78.42%) | Memory: 31.6 MB (66.18%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -1,15 +1,34 @@
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
-        if min(grid[0][1],grid[1][0])>1: return -1
-        
-        m,n=len(grid),len(grid[0])
-        q, seen = [(0,0,0)], set([(0,0)])
+        if grid[0][1] > 1 and grid[1][0] > 1:
+            return -1        
+        # otherwise we can move between top left and one neighbour back and forth until
+        # we can move to the next cell
+        m, n = len(grid), len(grid[0])
+        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        heap = [(0, m + n, 0)]  # (t, distance to goal d = m - i + n - j, i) , j = m + n - d - i
+        min_time = [[math.inf]*n for _ in range(m)]
+        min_time[0][0] = 0
+        while True:
+            t, d, i = heappop(heap)
+            j = m + n - d - i
+            if i == m - 1 and j == n - 1:
+                return t           
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if ni < 0 or nj < 0 or ni >= m or nj >= n:
+                    continue
+                dt = grid[ni][nj] - t 
+                if dt <= 1:
+                    nt = t + 1
+                else:
+                    nt = t + dt + (dt % 2 == 0) # move back and forth
+                if min_time[ni][nj] <= nt:
+                    continue
+                min_time[ni][nj] = nt
+                heappush(heap, (nt, d + i + j - ni - nj, ni))
+        raise ValueError("Unreachable")
 
-        while q:
-            time,x,y=heapq.heappop(q)
-            if (x,y)==(m-1,n-1): return time
-            neighbours=[(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
-            for a,b in neighbours:
-                if (a,b) in seen or a<0 or a==m or b<0 or b==n: continue
-                seen.add((a,b))
-                heapq.heappush(q,(max(time+1,grid[a][b]+(1-(grid[a][b]-time)%2)),a,b))
+
+        
+        
