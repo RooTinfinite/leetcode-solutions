544b1cd9aa190a3f64bed99dc0b461fb9257cd11
Piotr M <filo74b@gmail.com>
2024-11-29 02:32:24 +0100

    Time: 793 ms (5.26%) | Memory: 18.7 MB (13.64%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.go
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.go
@@ -1,69 +1,39 @@
-type Cell struct {
-    time, row, col int
+package main
+
+import (
+    "container/heap"
+    "fmt"
+)
+
+type MinHeap [][]int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i][0] < h[j][0] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+    *h = append(*h, x.([]int))
 }
 
-type PriorityQueue []Cell
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-func (pq PriorityQueue) Less(i, j int) bool { return pq[i].time < pq[j].time }
-func (pq PriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
-func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(Cell)) }
-func (pq *PriorityQueue) Pop() interface{} {
-    old := *pq
+func (h *MinHeap) Pop() interface{} {
+    old := *h
     n := len(old)
-    item := old[n-1]
-    *pq = old[0 : n-1]
-    return item
+    x := old[n-1]
+    *h = old[0 : n-1]
+    return x
 }
 
-func minimumTime(grid [][]int) int {
-    if grid[0][1] > 1 && grid[1][0] > 1 {
-        return -1
+func abs(x int) int {
+    if x < 0 {
+        return -x
     }
-    
-    rows, cols := len(grid), len(grid[0])
-    minHeap := &PriorityQueue{}
-    heap.Init(minHeap)
-    heap.Push(minHeap, Cell{0, 0, 0}) // time, row, col
-    
-    seen := make([][]int, rows)
-    for i := range seen {
-        seen[i] = make([]int, cols)
+    return x
+}
+func min(a, b int) int {
+    if a < b {
+        return a
     }
-    seen[0][0] = 1
-    
-    moves := [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-    
-    for minHeap.Len() > 0 {
-        curr := heap.Pop(minHeap).(Cell)
-        currTime := curr.time
-        currRow := curr.row
-        currCol := curr.col
-        
-        if currRow == rows-1 && currCol == cols-1 {
-            return currTime
-        }
-        
-        for _, move := range moves {
-            nextRow := move[0] + currRow
-            nextCol := move[1] + currCol
-            
-            if nextRow >= 0 && nextCol >= 0 && 
-               nextRow < rows && nextCol < cols && 
-               seen[nextRow][nextCol] == 0 {
-                
-                waitTime := 0
-                if (grid[nextRow][nextCol]-currTime)%2 == 0 {
-                    waitTime = 1
-                }
-                nextTime := max(currTime+1, grid[nextRow][nextCol]+waitTime)
-                
-                heap.Push(minHeap, Cell{nextTime, nextRow, nextCol})
-                seen[nextRow][nextCol] = 1
-            }
-        }
-    }
-    return -1
+    return b
 }
 
 func max(a, b int) int {
@@ -71,4 +41,45 @@ func max(a, b int) int {
         return a
     }
     return b
+}
+
+func minimumTime(grid [][]int) int {
+    if min(grid[0][1], grid[1][0]) > 1 {
+        return -1
+    }
+    
+    rows, cols := len(grid), len(grid[0])
+    minHeap := &MinHeap{}
+    heap.Init(minHeap)
+    heap.Push(minHeap, []int{0, 0, 0}) // time, r, c
+    visited := make(map[string]bool)
+    
+    dirs := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+    
+    for minHeap.Len() > 0 {
+        curr := heap.Pop(minHeap).([]int)
+        t, r, c := curr[0], curr[1], curr[2]
+        
+        if r == rows-1 && c == cols-1 {
+            return t
+        }
+        
+        for _, dir := range dirs {
+            nr, nc := r+dir[0], c+dir[1]
+            key := fmt.Sprintf("%d,%d", nr, nc)
+            
+            if nr < 0 || nc < 0 || nr == rows || nc == cols || visited[key] {
+                continue
+            }
+            
+            wait := 0
+            if abs(grid[nr][nc]-t)%2 == 0 {
+                wait = 1
+            }
+            nTime := max(grid[nr][nc]+wait, t+1)
+            heap.Push(minHeap, []int{nTime, nr, nc})
+            visited[key] = true
+        }
+    }
+    return -1
 }
