47add7fabeb5931b72047da1dab98b84d91db9d1
Piotr M <filo74b@gmail.com>
2024-11-29 21:25:41 +0100

    Time: 62 ms (50.00%) | Memory: 6.6 MB (50.00%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.rs
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.rs
@@ -1,40 +1,38 @@
-use std::collections::{BinaryHeap,HashSet};
-
-type Grid = Vec<Vec<i32>>;
-type Queue = BinaryHeap<(i32,(isize,isize))>;
-type Visited = HashSet<(isize,isize)>;
+use std::collections::BTreeMap;
 
 impl Solution {
-    pub fn minimum_time(grid: Grid) -> i32 {
-        let mut queue:Queue = BinaryHeap::from_iter(vec![(0,(0,0))]);
-        let mut result = i32::MAX;
-        let mut visited:Visited = HashSet::from_iter(vec![(0,0)]);
-        if grid[0][1] > 1 && grid[1][0] > 1 { return -1;}
-        while !queue.is_empty() {
-            let (time, (row,col)) = queue.pop().unwrap();
-            let time = time*-1;
-            if time > result { continue; }
-            if row == grid.len() as isize - 1 && col == grid[0].len() as isize - 1 {
-                result = result.min(time);
-                continue;
-            }   
-            vec![(-1,0),(0,-1),(1,0),(0,1)].iter().for_each(|(add_r,add_c)| {
-                process_cell(row+add_r, col+add_c, time, &grid, &mut queue, &mut visited);
-            });       
-        }
-
-        fn process_cell(row:isize,col:isize,time:i32,grid:&Grid,queue: &mut Queue, visited:&mut Visited) {
-            if row >= 0 && col >= 0 && row < grid.len() as isize && col < grid[0].len() as isize && !visited.contains(&(row,col)) {                
-                if time+1 >= grid[row as usize][col as usize] {
-                    queue.push(((time+1)*-1,(row,col)));
-                } else {
-                    let mut new_time = grid[row as usize][col as usize];
-                    if (grid[row as usize][col as usize] - time) % 2 == 0 { new_time += 1;}                    
-                    queue.push((new_time*-1,(row,col)));
+    pub fn minimum_time(mut grid: Vec<Vec<i32>>) -> i32 {
+        let mut tree: BTreeMap<i32, Vec<(usize, usize)>> = BTreeMap::new();
+        tree.insert(0, vec![(0, 0)]);
+        grid[0][0] = -1;
+        let dir = [0, 1, 0, usize::MAX, 0];
+        while let Some(&step) = tree.keys().next() {
+            let v = tree.remove(&step).unwrap();
+            for &(i, j) in v.iter() {
+                for w in dir.windows(2) {
+                    let i2 = i.wrapping_add(w[0]);
+                    let j2 = j.wrapping_add(w[1]);
+                    if i2 < grid.len() && j2 < grid[0].len() {
+                        if grid[i2][j2] >= 0 {
+                            let step2 = if grid[i2][j2] > step + 1 {
+                                grid[i2][j2] + 1 - ((grid[i2][j2] + step) & 1)
+                            }
+                            else {
+                                step + 1
+                            };
+                            if i2 == grid.len() - 1 && j2 == grid[0].len() - 1 {
+                                return step2;
+                            }
+                            tree.entry(step2).or_default().push((i2, j2));
+                            grid[i2][j2] = !grid[i2][j2];
+                        }
+                    }
                 }
-                visited.insert((row,col));
-            };
+            }
+            if step == 0 && tree.get(&1).is_none() {
+                return -1;
+            }
         }
-        result
+        unreachable!()
     }
 }
