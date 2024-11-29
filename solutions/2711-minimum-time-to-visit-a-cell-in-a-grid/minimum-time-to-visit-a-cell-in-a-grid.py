6ad7c171ed732ed4b45739f51fe5fef76c5a3330
Piotr M <filo74b@gmail.com>
2024-11-29 02:22:50 +0100

    Time: 1003 ms (56.19%) | Memory: 42.6 MB (35.40%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -1,32 +1,25 @@
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
-        DIRS = ((-1, 0), (0, 1), (1, 0), (0, -1))
-        
-        if grid[0][1] > 1 and grid[1][0] > 1:
+        if min(grid[0][1], grid[1][0]) > 1:
             return -1
-        
-        rows, cols = len(grid), len(grid[0])
-        pq = [(0, 0, 0)]  # (time, row, col)
-        seen = [[False] * cols for _ in range(rows)]
-        seen[0][0] = True
-        
-        while pq:
-            time, row, col = heappop(pq)
-            
-            if row == rows - 1 and col == cols - 1:
-                return time
-            
-            for dr, dc in DIRS:
-                newRow, newCol = row + dr, col + dc
-                
-                if (0 <= newRow < rows and 
-                    0 <= newCol < cols and 
-                    not seen[newRow][newCol]):
-                    
-                    waitTime = 1 if (grid[newRow][newCol] - time) % 2 == 0 else 0
-                    newTime = max(time + 1, grid[newRow][newCol] + waitTime)
-                    
-                    seen[newRow][newCol] = True
-                    heappush(pq, (newTime, newRow, newCol))
-        
-        return -1
+
+        min_heap = [(0, 0, 0)]  # (time, r, c)
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+
+        while min_heap:
+            t, r, c = heapq.heappop(min_heap)
+            if (r, c) == (ROWS - 1, COLS - 1):
+                return t
+
+            nei = [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
+            for nr, nc in nei:
+                if nr < 0 or nc < 0 or nr == ROWS or nc == COLS or (nr, nc) in visit:
+                    continue
+
+                wait = 0
+                if abs(grid[nr][nc] - t) % 2 == 0:
+                    wait = 1
+                n_time = max(grid[nr][nc] + wait, t + 1)
+                heapq.heappush(min_heap, (n_time, nr, nc))
+                visit.add((nr, nc))
