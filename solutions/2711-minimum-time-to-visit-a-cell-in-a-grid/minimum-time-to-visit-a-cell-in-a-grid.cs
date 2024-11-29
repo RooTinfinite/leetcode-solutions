28e098b863d505689b15588c1db421c21fa0bdad
Piotr M <filo74b@gmail.com>
2024-11-29 21:22:59 +0100

    Time: 831 ms (14.29%) | Memory: 67.4 MB (71.43%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cs
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cs
@@ -1,37 +1,65 @@
-public class Solution {
-    public int MinimumTime(int[][] grid) {
-        if (Math.Min(grid[0][1], grid[1][0]) > 1) {
+public class Solution
+{
+    int[][] grid;
+    int[,] res;
+    int rows, cols;
+    PriorityQueue<(int, int), int> toExplore = new PriorityQueue<(int, int), int>();
+
+    public int MinimumTime(int[][] grid)
+    {
+        if(grid[0][1] > 1 && grid[1][0] > 1)
             return -1;
+        this.grid = grid;
+        rows = grid.Length;
+        cols = grid[0].Length;
+        res = new int[rows, cols];
+
+        for(int i = 0; i < rows; i++)
+            for(int j = 0; j < cols; j++)
+                res[i,j] = int.MaxValue;
+        res[0,0] = 0;
+
+        toExplore.Enqueue((0, 0), 0);
+        while(toExplore.Count > 0)
+        {
+            var cell = toExplore.Dequeue();
+            ExploreNeighbors(cell);
         }
-        
-        int ROWS = grid.Length, COLS = grid[0].Length;
-        var minHeap = new PriorityQueue<(int t, int r, int c), int>();
-        minHeap.Enqueue((0, 0, 0), 0); // (time, r, c)
-        var visit = new HashSet<string>();
-        
-        while (minHeap.Count > 0) {
-            var (t, r, c) = minHeap.Dequeue();
-            
-            if (r == ROWS - 1 && c == COLS - 1) {
-                return t;
-            }
-            
-            var dirs = new[] { (1, 0), (-1, 0), (0, 1), (0, -1) };
-            foreach (var (dr, dc) in dirs) {
-                int nr = r + dr, nc = c + dc;
-                string key = $"{nr},{nc}";
-                
-                if (nr < 0 || nc < 0 || nr == ROWS || nc == COLS || 
-                    visit.Contains(key)) {
-                    continue;
-                }
-                
-                int wait = (Math.Abs(grid[nr][nc] - t) % 2 == 0) ? 1 : 0;
-                int nTime = Math.Max(grid[nr][nc] + wait, t + 1);
-                minHeap.Enqueue((nTime, nr, nc), nTime);
-                visit.Add(key);
-            }
-        }
-        return -1;
+
+        return res[rows-1, cols-1];
     }
+
+    private void ExploreNeighbors((int row, int col) cell)
+    {
+        foreach(var neighbor in GetNeighbors(cell))
+        {
+            var newVal = MoveTo(cell, neighbor);
+            if(newVal < res[neighbor.row, neighbor.col])
+            {
+                res[neighbor.row, neighbor.col] = newVal;
+                toExplore.Enqueue(neighbor, newVal);
+            }
+        }
+    }
+
+    private int MoveTo((int row, int col) cell, (int row, int col) neighbor)
+    {
+        int step = res[cell.row, cell.col]+1;
+        int minVal = grid[neighbor.row][neighbor.col];
+        if(minVal <= step)
+            return step;
+        else if(AreSamePairity(minVal, step))
+            return minVal;
+        return minVal+1;
+    }
+
+    private IEnumerable<(int row, int col)> GetNeighbors((int row, int col) cell)
+    {
+        if(cell.col > 0) yield return (cell.row, cell.col-1);
+        if(cell.row > 0) yield return (cell.row-1, cell.col);
+        if(cell.col < cols-1) yield return (cell.row, cell.col+1);
+        if(cell.row < rows-1) yield return (cell.row+1, cell.col);
+    }
+
+    private bool AreSamePairity(int x, int y) => x%2 == y%2;
 }
