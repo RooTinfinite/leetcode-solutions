a716d2975828766b847133b6f9975fa33f0663a6
Piotr M <filo74b@gmail.com>
2024-11-29 02:30:00 +0100

    Time: 450 ms (5.15%) | Memory: 67.5 MB (6.47%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.java
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.java
@@ -1,41 +1,36 @@
 class Solution {
     public int minimumTime(int[][] grid) {
-        if (grid[0][1] > 1 && grid[1][0] > 1) return -1;
+        if (Math.min(grid[0][1], grid[1][0]) > 1) {
+            return -1;
+        }
         
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
+        int ROWS = grid.length, COLS = grid[0].length;
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        minHeap.offer(new int[]{0, 0, 0}); // time, row, col
-        
-        int[][] seen = new int[rows][cols];
-        seen[0][0] = 1;
-        
-        int[][] moves = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        minHeap.offer(new int[]{0, 0, 0}); // {time, r, c}
+        Set<String> visit = new HashSet<>();
         
         while (!minHeap.isEmpty()) {
             int[] curr = minHeap.poll();
-            int currTime = curr[0];
-            int currRow = curr[1];
-            int currCol = curr[2];
+            int t = curr[0], r = curr[1], c = curr[2];
             
-            if (currRow == rows - 1 && currCol == cols - 1) 
-                return currTime;
+            if (r == ROWS - 1 && c == COLS - 1) {
+                return t;
+            }
             
-            for (int[] move : moves) {
-                int nextRow = move[0] + currRow;
-                int nextCol = move[1] + currCol;
+            int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for (int[] dir : dirs) {
+                int nr = r + dir[0], nc = c + dir[1];
+                String key = nr + "," + nc;
                 
-                if (nextRow >= 0 && nextCol >= 0 && 
-                    nextRow < rows && nextCol < cols && 
-                    seen[nextRow][nextCol] == 0) {
-                    
-                    int waitTime = ((grid[nextRow][nextCol] - currTime) % 2 == 0) ? 1 : 0;
-                    int nextTime = Math.max(currTime + 1, grid[nextRow][nextCol] + waitTime);
-                    
-                    minHeap.offer(new int[]{nextTime, nextRow, nextCol});
-                    seen[nextRow][nextCol] = 1;
+                if (nr < 0 || nc < 0 || nr == ROWS || nc == COLS || 
+                    visit.contains(key)) {
+                    continue;
                 }
+                
+                int wait = (Math.abs(grid[nr][nc] - t) % 2 == 0) ? 1 : 0;
+                int nTime = Math.max(grid[nr][nc] + wait, t + 1);
+                minHeap.offer(new int[]{nTime, nr, nc});
+                visit.add(key);
             }
         }
         return -1;
