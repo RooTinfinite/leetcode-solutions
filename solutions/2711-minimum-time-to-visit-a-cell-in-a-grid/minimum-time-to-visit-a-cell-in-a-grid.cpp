51174f46a11ade65e7dadc02b49307defda00988
Piotr M <filo74b@gmail.com>
2024-11-29 22:08:17 +0100

    Time: 117 ms (98.60%) | Memory: 51.6 MB (89.35%) - LeetSync

--- solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ solutions/2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,13 +1,8 @@
 class Solution {
     static constexpr int d[5] = {0, 1, 0, -1, 0};
-    static constexpr int N = 10;
     
-    static inline bool isOutside(int i, int j, int n, int m) {
+    static inline bool isOutside(const int i, const int j, const int n, const int m) {
         return (unsigned)i >= (unsigned)n || (unsigned)j >= (unsigned)m;
-    }
-    
-    static inline int idx(int i, int j, int m) { 
-        return i * m + j; 
     }
 
 public:
@@ -16,22 +11,19 @@ public:
             return -1;
 
         const int n = grid.size(), m = grid[0].size();
-        vector<int> time(n * m, INT_MAX);
-        vector<uint64_t> pq;
-        pq.reserve(N);
+        const int nm = n * m;
+        vector<int> time(nm, INT_MAX);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 
-        pq.push_back(0);
         time[0] = 0;
+        pq.emplace(0, 0);
         
         while (!pq.empty()) {
-            pop_heap(pq.begin(), pq.end(), greater<>{});
-            uint64_t tij = pq.back();
-            pq.pop_back();
+            const auto [t, pos] = pq.top();
+            pq.pop();
             
-            const int t = tij >> 32;
-            const int ij = tij & 0x3FFFFFFF;
-            const int i = ij / m;
-            const int j = ij % m;
+            const int i = pos / m;
+            const int j = pos % m;
 
             if (i == n - 1 && j == m - 1)
                 return t;
@@ -45,12 +37,11 @@ public:
 
                 const int wait_time = ((grid[r][s] ^ t) & 1) ^ 1;
                 const int nextTime = max(t + 1, grid[r][s] + wait_time);
-                const int rs = idx(r, s, m);
+                const int rs = r * m + s;
 
                 if (nextTime < time[rs]) {
                     time[rs] = nextTime;
-                    pq.push_back(((uint64_t)nextTime << 32) | rs);
-                    push_heap(pq.begin(), pq.end(), greater<>{});
+                    pq.emplace(nextTime, rs);
                 }
             }
         }
@@ -59,7 +50,7 @@ public:
 };
 
 static const int fast_io = []() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     return 0;
 }();
