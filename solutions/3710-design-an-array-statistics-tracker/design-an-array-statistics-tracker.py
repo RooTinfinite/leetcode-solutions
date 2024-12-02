5264b8bcc1f8f7da83b8b17274feac50ca894756
Piotr M <filo74b@gmail.com>
2024-12-02 02:24:52 +0100

    Time: 635 ms (97.30%) | Memory: 75 MB (54.05%) - LeetSync

--- solutions/3710-design-an-array-statistics-tracker/design-an-array-statistics-tracker.py
+++ solutions/3710-design-an-array-statistics-tracker/design-an-array-statistics-tracker.py
@@ -3,67 +3,34 @@ from sortedcontainers import SortedList
 class StatisticsTracker:
 
     def __init__(self):
-        self.queue = []
-        self.freq = {}
-        self.freqfreq = {}
-        self.sl = SortedList([])
-        self.freqsl = SortedList([])
-        self.s = 0
+        self.a = deque()
         self.n = 0
-        
+        self.s = 0
+        self.ct = defaultdict(int)
+        self.pq = []
+        self.sd = SortedList()
 
     def addNumber(self, number: int) -> None:
-        self.queue.append(number)
-        if number not in self.freq:self.freq[number] = 0
-        self.freq[number] += 1
-        if self.freq[number] not in self.freqfreq:
-            self.freqfreq[self.freq[number]] = SortedList([])
-        if self.freq[number] > 1:
-            self.freqfreq[self.freq[number]-1].remove(number)
-            self.freqsl.remove(self.freq[number]-1)
-        self.freqsl.add(self.freq[number])
-        self.freqfreq[self.freq[number]].add(number)
-
-        self.sl.add(number)
+        self.sd.add(number)
+        self.ct[number] += 1
+        heappush(self.pq, (-self.ct[number], number))
         self.s += number
-        self.n += 1 
+        self.a.append(number)
+        self.n+= 1
         
-
     def removeFirstAddedNumber(self) -> None:
-        if not self.queue:return 
-        torem = self.queue[0]
-        self.queue.pop(0)
-        toremfreq = self.freq[torem]
-        self.freq[torem] -= 1
-        self.freqsl.remove(toremfreq)
-        self.freqfreq[toremfreq].remove(torem)
-        if toremfreq > 1:
-            self.freqsl.add(toremfreq-1)
-            self.freqfreq[toremfreq-1].add(torem)
-        self.sl.remove(torem)
-        self.s -= torem
-        self.n -= 1
-        
+        number = self.a.popleft()
+        self.sd.remove(number)
+        self.ct[number]-= 1
+        self.s-= number
+        self.n-= 1
 
     def getMean(self) -> int:
-        return self.s // self.n if self.n > 0 else 0
-        
+        return self.s // self.n
 
     def getMedian(self) -> int:
-        if self.n == 0:return 0
-        return self.sl[self.n//2]
-
+        return self.sd[self.n // 2]
 
     def getMode(self) -> int:
-        M = self.freqsl[-1]
-        return self.freqfreq[M][0]
-
-
-
-# Your StatisticsTracker object will be instantiated and called as such:
-# obj = StatisticsTracker()
-# obj.addNumber(number)
-# obj.removeFirstAddedNumber()
-# param_3 = obj.getMean()
-# param_4 = obj.getMedian()
-# param_5 = obj.getMode()
+        while self.pq and -self.pq[0][0] != self.ct[self.pq[0][1]]: heappop(self.pq)
+        return self.pq[0][1]
