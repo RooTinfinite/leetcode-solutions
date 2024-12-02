c5f628f982d20953cc6c800c8e1157804b712977
Piotr M <filo74b@gmail.com>
2024-12-02 02:04:20 +0100

    Time: 0 ms (100.00%) | Memory: 8.1 MB (7.80%) - LeetSync

--- solutions/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ solutions/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int isPrefixOfWord(string s, string w) {
-        stringstream ss(s);
+    int isPrefixOfWord(string sentence, string w) {
+        stringstream ss(sentence);
         string t;
         for(int i = 1; ss >> t; i++)
             if(t.substr(0, w.length()) == w) return i;
