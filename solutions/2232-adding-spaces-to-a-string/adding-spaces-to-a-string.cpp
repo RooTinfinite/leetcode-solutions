03ca17f900ea94fddccda2641ecffc6f03f8c4f1
Piotr M <filo74b@gmail.com>
2024-12-03 04:13:44 +0100

    Time: 6 ms (99.40%) | Memory: 82.2 MB (90.53%) - LeetSync

--- solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
         string result(s.length() + spaces.size(), 0);
-        int shift = 1;
-        int prev = 0;
+        int writePos = 0;
+        int readPos = 0;
         
-        memcpy(&result[0], &s[0], spaces[0]);
-        result[spaces[0]] = ' ';
-        
-        for (int i = 1; i < spaces.size(); ++i) {
-            int len = spaces[i] - spaces[i-1];
-            memcpy(&result[spaces[i-1] + shift], &s[spaces[i-1]], len);
-            result[spaces[i] + shift] = ' ';
-            shift++;
+        for (int spacePos : spaces) {
+            while (readPos < spacePos) {
+                result[writePos++] = s[readPos++];
+            }
+            result[writePos++] = ' ';
         }
         
-        if (!spaces.empty()) {
-            memcpy(&result[spaces.back() + shift], 
-                   &s[spaces.back()], 
-                   s.length() - spaces.back());
+        while (readPos < s.length()) {
+            result[writePos++] = s[readPos++];
         }
         
         return result;
