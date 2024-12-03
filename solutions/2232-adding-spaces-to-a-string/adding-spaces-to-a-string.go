a02ba5e193e42a2aa0111b8d35ea98dbc055d2d7
Piotr M <filo74b@gmail.com>
2024-12-03 04:33:03 +0100

    Time: 8 ms (85.71%) | Memory: 16.9 MB (90.24%) - LeetSync

--- solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.go
+++ solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.go
@@ -1,21 +1,23 @@
 func addSpaces(s string, spaces []int) string {
-    var res strings.Builder
-    res.Grow(len(s) + len(spaces))
+    result := make([]byte, len(s)+len(spaces))
+    writePos := 0
+    readPos := 0
     
-    i, j := 0, 0
-    for i < len(s) && j < len(spaces) {
-        if i < spaces[j] {
-            res.WriteByte(s[i])
-            i++
-        } else {
-            res.WriteByte(' ')
-            j++
+    for _, spacePos := range spaces {
+        for readPos < spacePos {
+            result[writePos] = s[readPos]
+            writePos++
+            readPos++
         }
+        result[writePos] = ' '
+        writePos++
     }
     
-    if i < len(s) {
-        res.WriteString(s[i:])
+    for readPos < len(s) {
+        result[writePos] = s[readPos]
+        writePos++
+        readPos++
     }
     
-    return res.String()
+    return string(result)
 }
