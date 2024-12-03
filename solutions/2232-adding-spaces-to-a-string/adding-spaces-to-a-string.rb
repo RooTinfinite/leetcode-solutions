03520d7a6689a14f1d71c3f2937f9655ccff600f
Piotr M <filo74b@gmail.com>
2024-12-03 04:34:02 +0100

    Time: 278 ms (100.00%) | Memory: 274.7 MB (50.00%) - LeetSync

--- solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.rb
+++ solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.rb
@@ -1,19 +1,23 @@
 def add_spaces(s, spaces)
-    i = 0
-    j = 0
-    res = []
+    result = Array.new(s.length + spaces.length)
+    write_pos = 0
+    read_pos = 0
     
-    while i < s.length && j < spaces.length
-        if i < spaces[j]
-            res.push(s[i])
-            i += 1
-        else
-            res.push(" ")
-            j += 1
+    spaces.each do |space_pos|
+        while read_pos < space_pos
+            result[write_pos] = s[read_pos]
+            write_pos += 1
+            read_pos += 1
         end
+        result[write_pos] = ' '
+        write_pos += 1
     end
     
-    res.push(s[i..-1]) if i < s.length
+    while read_pos < s.length
+        result[write_pos] = s[read_pos]
+        write_pos += 1
+        read_pos += 1
+    end
     
-    res.join
+    result.join
 end
