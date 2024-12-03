213e8fcf19a01ad5ca40aacbd6f2cba7f767423d
Piotr M <filo74b@gmail.com>
2024-12-03 03:44:57 +0100

    Time: 23 ms (37.65%) | Memory: 80.5 MB (93.30%) - LeetSync

--- solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ solutions/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     string addSpaces(string& s, vector<int>& spaces) {
-        const size_t finalSize = s.size() + spaces.size();
+        const size_t spaceCount = spaces.size();
+        const size_t strLength = s.size();
+        const size_t resultLength = strLength + spaceCount;
+        
         string result;
-        result.reserve(finalSize);
+        result.reserve(resultLength);
         
         size_t spaceIdx = 0;
         size_t currentPos = 0;
         
-        string_view sv(s);
-        
-        while (spaceIdx < spaces.size()) {
-            size_t nextSpace = spaces[spaceIdx];
-            result.append(sv.substr(currentPos, nextSpace - currentPos));
-            result += ' ';
-            currentPos = nextSpace;
-            ++spaceIdx;
+        while (currentPos + 4 <= strLength && spaceIdx < spaceCount) {
+            uint32_t chunk = *reinterpret_cast<const uint32_t*>(s.data() + currentPos);
+            
+            for (size_t i = 0; i < 4; ++i) {
+                if (spaceIdx < spaceCount && currentPos + i == static_cast<size_t>(spaces[spaceIdx])) {
+                    result.push_back(' ');
+                    ++spaceIdx;
+                }
+                result.push_back(static_cast<char>((chunk >> (i * 8)) & 0xFF));
+            }
+            currentPos += 4;
         }
         
-        if (currentPos < s.size()) {
-            result.append(sv.substr(currentPos));
+        while (currentPos < strLength) {
+            if (spaceIdx < spaceCount && currentPos == static_cast<size_t>(spaces[spaceIdx])) {
+                result.push_back(' ');
+                ++spaceIdx;
+            }
+            result.push_back(s[currentPos]);
+            ++currentPos;
+        }
+        
+        while (spaceIdx < spaceCount) {
+            result.push_back(' ');
+            ++spaceIdx;
         }
         
         return result;
