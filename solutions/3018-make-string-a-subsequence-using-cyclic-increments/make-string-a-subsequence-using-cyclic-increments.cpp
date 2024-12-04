a60b7dc6d2f34e23d9977c4663df6ccc95531739
Piotr M <filo74b@gmail.com>
2024-12-04 02:29:53 +0100

    Time: 12 ms (12.15%) | Memory: 14.9 MB (97.07%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    static bool canMakeSubsequence(string& str1, string& str2) {
-        const int n1=str1.size(), n2=str2.size();
-        char c2=str2[0];
-        int i, j;
-        for(i=0, j=0; i<n1 && j<n2; i++){
-            char c1=str1[i];
-            if (c1==c2 ||c1+1==c2 || (c1=='z' && c2=='a')) 
-                c2=str2[++j];
+    static bool canMakeSubsequence(string& source, string& target) {
+        const int srcLen = source.size(), tgtLen = target.size();
+        if (srcLen < tgtLen) return false;
+        
+        int tgtIdx = 0;
+        char targetChar = target[0];
+        
+        for(int srcIdx = 0; srcIdx < srcLen && tgtIdx < tgtLen; srcIdx++) {
+            int diff = ((target[tgtIdx] - source[srcIdx]) + 26) % 26;
+            if (diff == 0 || diff == 1) {
+                tgtIdx++;
+                if (tgtIdx < tgtLen) targetChar = target[tgtIdx];
+            }
         }
-        return j==n2; 
+        
+        return tgtIdx == tgtLen;
     }
 };
