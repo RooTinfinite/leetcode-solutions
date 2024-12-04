85f972e5abef9e51669d1f323242456b088a19f5
Piotr M <filo74b@gmail.com>
2024-12-04 01:38:13 +0100

    Time: 1 ms (79.01%) | Memory: 14.9 MB (97.91%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool canMakeSubsequence(string source, string target) {
-        int targetIdx = 0;
-        int targetLen = target.length();
-        for (char currChar : source) {
-            if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1) {
-                targetIdx++;
-            }
+    static bool canMakeSubsequence(string& str1, string& str2) {
+        const int n1=str1.size(), n2=str2.size();
+        char c2=str2[0];
+        int i, j;
+        for(i=0, j=0; i<n1 && j<n2; i++){
+            char c1=str1[i];
+            if (c1==c2 ||c1+1==c2 || (c1=='z' && c2=='a')) c2=str2[++j];
         }
-        return targetIdx == targetLen;
+        return j==n2; 
     }
 };
