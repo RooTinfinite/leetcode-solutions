4ad555a030c2194e0e769c93f9024524f594d82a
Piotr M <filo74b@gmail.com>
2024-12-04 01:28:01 +0100

    Time: 3 ms (100.00%) | Memory: 2.7 MB (100.00%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.rs
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.rs
@@ -1,19 +1,15 @@
 impl Solution {
     pub fn can_make_subsequence(source: String, target: String) -> bool {
-        let src_bytes = source.as_bytes();
-        let tgt_bytes = target.as_bytes();
-        let (mut src_idx, mut tgt_idx) = (0, 0);
+        let mut target_idx = 0;
+        let target_len = target.len();
+        let target_bytes = target.as_bytes();
         
-        while src_idx < src_bytes.len() && tgt_idx < tgt_bytes.len() {
-            if src_bytes[src_idx] == tgt_bytes[tgt_idx] || 
-               (src_bytes[src_idx] == b'z' && tgt_bytes[tgt_idx] == b'a') || 
-               (src_bytes[src_idx] + 1 == tgt_bytes[tgt_idx]) {
-                src_idx += 1;
-                tgt_idx += 1;
-            } else {
-                src_idx += 1;
+        for curr_char in source.bytes() {
+            if target_idx < target_len && 
+               (target_bytes[target_idx] as i32 - curr_char as i32 + 26) % 26 <= 1 {
+                target_idx += 1;
             }
         }
-        tgt_idx == tgt_bytes.len()
+        target_idx == target_len
     }
 }
