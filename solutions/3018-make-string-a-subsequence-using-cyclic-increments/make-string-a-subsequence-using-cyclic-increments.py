49903315fde5e489e60d945cf846f1602e739f56
Piotr M <filo74b@gmail.com>
2024-12-04 02:36:51 +0100

    Time: 63 ms (88.89%) | Memory: 17.6 MB (50.24%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -2,13 +2,19 @@ class Solution:
     @staticmethod
     def canMakeSubsequence(source: str, target: str) -> bool:
         src_len, tgt_len = len(source), len(target)
-        if src_len < tgt_len:
-            return False
+        target_char = target[0]
+        
+        src_idx = tgt_idx = 0
+        while src_idx < src_len and tgt_idx < tgt_len:
+            src_char = source[src_idx]
             
-        tgt_idx = 0
-        for c in source:
-            next_char = chr((ord(c) - ord('a') + 1) % 26 + ord('a'))
-            if tgt_idx < tgt_len and (c == target[tgt_idx] or next_char == target[tgt_idx]):
+            if (src_char == target_char or 
+                chr(ord(src_char) + 1) == target_char or 
+                (src_char == 'z' and target_char == 'a')):
                 tgt_idx += 1
-                
+                if tgt_idx < tgt_len:
+                    target_char = target[tgt_idx]
+            
+            src_idx += 1
+            
         return tgt_idx == tgt_len
