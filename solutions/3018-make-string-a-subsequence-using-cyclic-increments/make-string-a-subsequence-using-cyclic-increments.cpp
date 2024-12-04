9d7382f5f0dca51daeed558e97eba66141124948
Piotr M <filo74b@gmail.com>
2024-12-04 02:40:31 +0100

    Time: 11 ms (16.57%) | Memory: 17.1 MB (13.81%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    static bool canMakeSubsequence(string& source, string& target) {
-        const int srcLen = source.size(), tgtLen = target.size();
-        char targetChar = target[0];
+    bool canMakeSubsequence(string str1, string str2) {
+        if (str1.length() == str2.length()) {
+            for (int i = 0; i < str1.length(); i++) {
+                if (str2[i] == 'a' && str1[i] == 'z') {
+                    continue;
+                }
+                else if (str1[i] == str2[i]) {
+                    continue;
+                }
+                else if (str2[i] - str1[i] != 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
         
-        int srcIdx, tgtIdx;
-        for(srcIdx = 0, tgtIdx = 0; srcIdx < srcLen && tgtIdx < tgtLen; srcIdx++) {
-            char srcChar = source[srcIdx];
-            
-            if (srcChar == targetChar || 
-                srcChar + 1 == targetChar || 
-                (srcChar == 'z' && targetChar == 'a')) {
-                targetChar = target[++tgtIdx];
+        if (str2.length() > str1.length()) {
+            return false;
+        }
+        
+        vector<char> l;
+        for (char c : str2) {
+            if (str1.find(c) == string::npos) {
+                l.push_back(c);
             }
         }
         
-        return tgtIdx == tgtLen;
+        int count = 0;
+        for (char c : l) {
+            if (str1.find(char(c - 1)) != string::npos) {
+                count++;
+            }
+            if (c == 'a' && str1.find('z') != string::npos) {
+                count++;
+            }
+        }
+        
+        return count == l.size();
     }
 };
