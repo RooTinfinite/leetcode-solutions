52e7db5143f872ed6a67c5a623c47c9189e8ef71
Piotr M <filo74b@gmail.com>
2024-12-04 02:32:05 +0100

    Time: 111 ms (18.06%) | Memory: 17.7 MB (13.74%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -4,20 +4,11 @@ class Solution:
         src_len, tgt_len = len(source), len(target)
         if src_len < tgt_len:
             return False
-        
-        tgt_idx = 0
-        target_mask = 1 << (ord(target[0]) - ord('a'))
-        
-        for src_idx in range(src_len):
-            if tgt_idx >= tgt_len:
-                break
-                
-            src_mask = 1 << (ord(source[src_idx]) - ord('a'))
-            next_mask = 1 << ((ord(source[src_idx]) - ord('a') + 1) % 26)
             
-            if (target_mask & (src_mask | next_mask)) != 0:
+        tgt_idx = 0
+        for c in source:
+            next_char = chr((ord(c) - ord('a') + 1) % 26 + ord('a'))
+            if tgt_idx < tgt_len and (c == target[tgt_idx] or next_char == target[tgt_idx]):
                 tgt_idx += 1
-                if tgt_idx < tgt_len:
-                    target_mask = 1 << (ord(target[tgt_idx]) - ord('a'))
-        
+                
         return tgt_idx == tgt_len
