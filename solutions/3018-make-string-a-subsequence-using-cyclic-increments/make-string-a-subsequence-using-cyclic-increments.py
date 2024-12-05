840154b2b3785d56349d717d7ed762656d336a36
Piotr M <filo74b@gmail.com>
2024-12-05 01:09:44 +0100

    Time: 59 ms (91.22%) | Memory: 17.8 MB (12.68%) - LeetSync

--- solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ solutions/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,19 +1,20 @@
 class Solution:
     def canMakeSubsequence(self, source: str, target: str) -> bool:
-        src_len, tgt_len = len(source), len(target)
-        target_char = target[0]
+        s_len, t_len = len(source), len(target)
+        curr_t = target[0]
         
-        src_idx = tgt_idx = 0
-        while src_idx < src_len and tgt_idx < tgt_len:
-            src_char = source[src_idx]
+        s_pos = t_pos = 0
+        
+        while s_pos < s_len and t_pos < t_len:
+            curr_s = source[s_pos]
             
-            if (src_char == target_char or 
-                chr(ord(src_char) + 1) == target_char or 
-                (src_char == 'z' and target_char == 'a')):
-                tgt_idx += 1
-                if tgt_idx < tgt_len:
-                    target_char = target[tgt_idx]
+            if (curr_s == curr_t or 
+                chr(ord(curr_s) + 1) == curr_t or 
+                (curr_s == 'z' and curr_t == 'a')):
+                t_pos += 1
+                if t_pos < t_len:
+                    curr_t = target[t_pos]
             
-            src_idx += 1
+            s_pos += 1
             
-        return tgt_idx == tgt_len
+        return t_pos == t_len
