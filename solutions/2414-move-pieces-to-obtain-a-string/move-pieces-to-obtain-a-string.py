79696ea0b6abef53b20e8eae86bedab7ba962653
Piotr M <filo74b@gmail.com>
2024-12-05 01:15:11 +0100

    Time: 59 ms (99.19%) | Memory: 17.7 MB (46.11%) - LeetSync

--- solutions/2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
+++ solutions/2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -1,24 +1,25 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
-        if start==target:
+        if start == target:
             return True
-        need_l=0
-        store_r=0
-        for s,v in zip(start, target):
-            if s=='R':
-                if need_l>0:
+        pending_L = 0   
+        waiting_R = 0    
+
+        for curr, goal in zip(start, target):
+            if curr == 'R':
+                if pending_L > 0:
                     return False
-                store_r+=1
-            if v=='L':
-                if store_r>0:
+                waiting_R += 1  
+            if goal == 'L':
+                if waiting_R > 0:
                     return False
-                need_l+=1
-            if v=='R':
-                if store_r==0:
+                pending_L += 1
+            if goal == 'R':
+                if waiting_R == 0:
                     return False
-                store_r-=1
-            if s=="L":
-                if need_l==0:
+                waiting_R -= 1 
+            if curr == 'L':
+                if pending_L == 0:
                     return False
-                need_l-=1                    
-        return need_l==0 and store_r==0
+                pending_L -= 1    
+        return pending_L == 0 and waiting_R == 0
