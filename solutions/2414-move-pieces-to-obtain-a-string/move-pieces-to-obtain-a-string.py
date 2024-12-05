a303c7b6a24ede1022d878b34a6ba22f5a9617b7
Piotr M <filo74b@gmail.com>
2024-12-05 01:04:43 +0100

    Time: 59 ms (99.19%) | Memory: 17.6 MB (55.09%) - LeetSync

--- solutions/2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
+++ solutions/2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.py
@@ -1,31 +1,24 @@
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
-        # Queue to store characters and indices from both strings
-        start_queue = []
-        target_queue = []
-
-        # Record non-underscore characters and their indices
-        for i in range(len(start)):
-            if start[i] != "_":
-                start_queue.append((start[i], i))
-            if target[i] != "_":
-                target_queue.append((target[i], i))
-
-        # If number of pieces don't match, return false
-        if len(start_queue) != len(target_queue):
-            return False
-
-        # Compare each piece's type and position
-        while not len(start_queue) == 0:
-            start_char, start_index = start_queue.pop(0)
-            target_char, target_index = target_queue.pop(0)
-
-            # Check character match and movement rules
-            if (
-                start_char != target_char
-                or (start_char == "L" and start_index < target_index)
-                or (start_char == "R" and start_index > target_index)
-            ):
-                return False
-
-        return True
+        if start==target:
+            return True
+        need_l=0
+        store_r=0
+        for s,v in zip(start, target):
+            if s=='R':
+                if need_l>0:
+                    return False
+                store_r+=1
+            if v=='L':
+                if store_r>0:
+                    return False
+                need_l+=1
+            if v=='R':
+                if store_r==0:
+                    return False
+                store_r-=1
+            if s=="L":
+                if need_l==0:
+                    return False
+                need_l-=1                    
+        return need_l==0 and store_r==0
