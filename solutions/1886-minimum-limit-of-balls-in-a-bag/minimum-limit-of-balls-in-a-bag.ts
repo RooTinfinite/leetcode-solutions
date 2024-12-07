147c531dad81d93c89363cb466c29d37e55c3f40
Piotr M <filo74b@gmail.com>
2024-12-07 03:23:20 +0100

    Time: 25 ms (100.00%) | Memory: 62.1 MB (14.29%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.ts
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.ts
@@ -1,12 +1,17 @@
-function minimumSize(nums: number[], maxOps: number): number {
-    let low = 1;
-    let high = Math.max(...nums);
-    
-    while (low < high) {
-        const mid = Math.floor((low + high) / 2);
-        const ops = nums.reduce((sum, n) => sum + Math.floor((n - 1) / mid), 0);
-        if (ops <= maxOps) high = mid;
-        else low = mid + 1;
+function minimumSize(nums: number[], maxOperations: number): number {
+    let left = 1;
+    let right = Math.max(...nums);
+    while (left < right) {
+        const mid = (left + right) >> 1;
+        let cnt = 0;
+        for (const x of nums) {
+            cnt += ~~((x - 1) / mid);
+        }
+        if (cnt <= maxOperations) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
     }
-    return high;
+    return left;
 }
