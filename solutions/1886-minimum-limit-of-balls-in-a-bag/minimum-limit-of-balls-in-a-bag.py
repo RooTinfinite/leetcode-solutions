a4f6adbb5bce64369f9963562d7fc1fe1bc4e21a
Piotr M <filo74b@gmail.com>
2024-12-07 05:59:08 +0100

    Time: 563 ms (79.47%) | Memory: 29.3 MB (30.91%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
@@ -1,21 +1,8 @@
 class Solution:
-    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        def can_divide(max_balls):
-            ops = 0
-            for n in nums:
-                ops += ceil(n / max_balls) - 1
-                if ops > maxOperations:
-                    return False
-            return True
-
-        # O(n * logm)
-        l, r = 1, max(nums)
-        res = r
-        while l < r:
-            m = l + ((r - l) // 2)
-            if can_divide(m):
-                r = m
-                res = r
-            else:
-                l = m + 1
-        return res
+    def minimumSize(self, nums: List[int], maxOps: int) -> int:
+        low, high = 1, max(nums)
+        while low < high:
+            mid = (low + high) // 2
+            if sum((n - 1) // mid for n in nums) <= maxOps: high = mid
+            else: low = mid + 1
+        return high
