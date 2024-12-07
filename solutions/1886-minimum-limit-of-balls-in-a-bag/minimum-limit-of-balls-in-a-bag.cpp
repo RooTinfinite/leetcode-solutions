5ede01db2b6f2e28183cb6564988558ec4b743b4
Piotr M <filo74b@gmail.com>
2024-12-07 03:08:13 +0100

    Time: 27 ms (88.22%) | Memory: 59.7 MB (28.46%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -3,13 +3,9 @@ public:
     int minimumSize(vector<int>& nums, int k) {
         int l = 1, r = *max_element(nums.begin(), nums.end());
         while (l < r) {
-            int m = l + (r - l) / 2;
-            int cnt = 0;
-            for (int x : nums) {
-                cnt += (x - 1) / m;
-                if (cnt > k) break;
-            }
-            cnt <= k ? r = m : l = m + 1;
+            int m = l + (r - l) / 2, s = 0;
+            for (int x : nums) if ((s += (x - 1) / m) > k) break;
+            s <= k ? r = m : l = m + 1;
         }
         return r;
     }
