cc7360c77c5f8cd9e202e118f0330421f8834e44
Piotr M <filo74b@gmail.com>
2024-12-07 03:54:32 +0100

    Time: 38 ms (81.48%) | Memory: 60.7 MB (40.63%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.js
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.js
@@ -1,19 +1,25 @@
-// JavaScript
-
-function minimumSize(n, maxOps) {
-    let low = 1;
-    let high = Math.max(...n);
-    while (low < high) {
-        const mid = (low + high) >> 1;
-        let cnt = 0;
-        for (const x of n) {
-            cnt += ~~((x - 1) / mid);
+var minimumSize = function(nums, maxOperations) {
+    const canDivide = (maxBalls) => {
+        let ops = 0;
+        for (const n of nums) {
+            ops += Math.ceil(n / maxBalls) - 1;
+            if (ops > maxOperations) return false;
         }
-        if (cnt <= maxOps) {
-            high = mid;
+        return true;
+    };
+    
+    let left = 1;
+    let right = Math.max(...nums);
+    let res = right;
+    
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (canDivide(mid)) {
+            right = mid;
+            res = right;
         } else {
-            low = mid + 1;
+            left = mid + 1;
         }
     }
-    return low;
-}
+    return res;
+};
