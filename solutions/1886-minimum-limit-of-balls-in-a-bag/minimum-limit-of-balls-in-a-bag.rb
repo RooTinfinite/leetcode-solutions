f7b6cea724ec17a0b17dc3edd68b93dcc05dc378
Piotr M <filo74b@gmail.com>
2024-12-07 03:53:37 +0100

    Time: 1287 ms (100.00%) | Memory: 220.1 MB (100.00%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.rb
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.rb
@@ -1,9 +1,25 @@
-def minimum_size(nums, max_ops)
-    low, high = 1, nums.max
-    while low < high
-        mid = (low + high) / 2
-        ops = nums.sum { |n| (n - 1) / mid }
-        ops <= max_ops ? high = mid : low = mid + 1
+def can_divide(nums, max_balls, max_operations)
+    ops = 0
+    nums.each do |n|
+        ops += (n.to_f / max_balls).ceil - 1
+        return false if ops > max_operations
     end
-    high
+    true
+end
+
+def minimum_size(nums, max_operations)
+    left = 1
+    right = nums.max
+    res = right
+    
+    while left < right
+        mid = left + (right - left) / 2
+        if can_divide(nums, mid, max_operations)
+            right = mid
+            res = right
+        else
+            left = mid + 1
+        end
+    end
+    res
 end
