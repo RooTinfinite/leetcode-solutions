1aa63b0e32f15f91547342c529471673ae183eb1
Piotr M <filo74b@gmail.com>
2024-12-07 03:18:23 +0100

    Time: 557 ms (81.05%) | Memory: 29.4 MB (26.36%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
@@ -1,8 +1,8 @@
 class Solution:
     def minimumSize(self, nums: List[int], maxOps: int) -> int:
-        l, h = 1, max(nums)
-        while l < h:
-            m = (l + h) // 2
-            if sum((n - 1) // m for n in nums) <= maxOps: h = m
-            else: l = m + 1
-        return h
+        low, high = 1, max(nums)
+        while low < high:
+            mid = (low + high) // 2
+            if sum((n - 1) // mid for n in nums) <= maxOps: high = mid
+            else: low = mid + 1
+        return high
