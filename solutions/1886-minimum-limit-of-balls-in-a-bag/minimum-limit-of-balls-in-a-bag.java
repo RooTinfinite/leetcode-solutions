b10f9237cf9f8c2f3b9377933ce861139d283f47
Piotr M <filo74b@gmail.com>
2024-12-07 03:51:01 +0100

    Time: 29 ms (75.95%) | Memory: 60.3 MB (25.91%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.java
@@ -1,15 +1,26 @@
 class Solution {
-    public int minimumSize(int[] nums, int maxOps) {
-        int low = 1, high = Arrays.stream(nums).max().getAsInt();
-        while (low < high) {
-            int mid = (low + high) / 2;
-            int ops = 0;
-            for (int n : nums) {
-                ops += (n - 1) / mid;
-            }
-            if (ops <= maxOps) high = mid;
-            else low = mid + 1;
+    private boolean canDivide(int[] nums, int maxBalls, int maxOperations) {
+        int ops = 0;
+        for (int n : nums) {
+            ops += (n + maxBalls - 1) / maxBalls - 1;
+            if (ops > maxOperations) return false;
         }
-        return high;
+        return true;
+    }
+    
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt();
+        int res = right;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canDivide(nums, mid, maxOperations)) {
+                right = mid;
+                res = right;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
     }
 }
