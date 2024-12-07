17d9f05aceda0af1d098c5f4eb2109deba2eb73d
Piotr M <filo74b@gmail.com>
2024-12-07 03:53:17 +0100

    Time: 11 ms (100.00%) | Memory: 4.3 MB (100.00%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.rs
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.rs
@@ -1,12 +1,29 @@
 impl Solution {
-    pub fn minimum_size(nums: Vec<i32>, max_ops: i32) -> i32 {
-        let mut low = 1;
-        let mut high = *nums.iter().max().unwrap();
-        while low < high {
-            let mid = (low + high) / 2;
-            let ops = nums.iter().map(|&n| (n - 1) / mid).sum::<i32>();
-            if ops <= max_ops { high = mid } else { low = mid + 1 }
+    fn can_divide(nums: &Vec<i32>, max_balls: i32, max_operations: i32) -> bool {
+        let mut ops = 0;
+        for &n in nums {
+            ops += (n + max_balls - 1) / max_balls - 1;
+            if ops > max_operations {
+                return false;
+            }
         }
-        high
+        true
+    }
+    
+    pub fn minimum_size(nums: Vec<i32>, max_operations: i32) -> i32 {
+        let mut left = 1;
+        let mut right = *nums.iter().max().unwrap();
+        let mut res = right;
+        
+        while left < right {
+            let mid = left + (right - left) / 2;
+            if Self::can_divide(&nums, mid, max_operations) {
+                right = mid;
+                res = right;
+            } else {
+                left = mid + 1;
+            }
+        }
+        res
     }
 }
