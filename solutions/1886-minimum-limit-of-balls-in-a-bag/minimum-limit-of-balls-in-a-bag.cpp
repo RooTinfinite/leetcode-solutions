1e51ad799e54d658f7f52a3092330dcf1e6b0fb6
Piotr M <filo74b@gmail.com>
2024-12-08 19:31:26 +0100

    Time: 72 ms (7.46%) | Memory: 59.8 MB (22.57%) - LeetSync

--- solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ solutions/1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -1,44 +1,16 @@
-#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
-#define INL __attribute__((always_inline))
-#define HOT __attribute__((hot))
-#define INL_ATTR noexcept INL /*NO_SAN*/ HOT
-#define OUTL_ATTR noexcept /*NO_SAN*/ HOT
-#define LAM_ATTR INL /*NO_SAN*/ HOT noexcept
-
 class Solution {
-private:
-    static uint target(const vector<int> &nums, const uint m) INL_ATTR {
-        return transform_reduce(nums.cbegin(), nums.cend(), 0ull,
-            plus(), [m](const uint v) LAM_ATTR { return (v - 1u) / m; });
-    }
-
 public:
-    static int minimumSize(const vector<int>& nums, const uint tc) OUTL_ATTR {
-        constexpr uint MAXV = 1'000'000'000u;
-        const uint n = nums.size();
-        if (n == 1)
-            return (nums.front() + tc) / (tc + 1u);
-        uint64_t s = 0;
-        uint rb = 0;
-        for (const uint v : nums) {
-            s += v;
-            rb = max<uint64_t>(v, rb);
+    int minimumSize(vector<int>& nums, int maxOps) {
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int ops = 0;
+            for (int n : nums) {
+                ops += ceil((double)n / mid) - 1;
+                if (ops > maxOps) break;
+            }
+            ops <= maxOps ? high = mid : low = mid + 1;
         }
-        rb = min<uint>((s + tc - 1u) / tc, rb);
-        uint lb = max<uint>((uint64_t)rb * (tc < 190 ? 261u : 471u) / 512u, 1);
-        while (lb < rb) {
-            const uint mid = (lb + rb) / 2;
-            const uint64_t mc = target(nums, mid);
-            const bool b = mc > tc;
-            (b ? lb : rb) = mid + b;
-        }
-        return lb;
+        return high;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
