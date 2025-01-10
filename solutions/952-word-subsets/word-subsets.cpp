f59e1f5183cf51e8aef50ca4fbbbab51d854ca70
Piotr M <filo74b@gmail.com>
2025-01-10 01:30:57 +0100

    Time: 7 ms (95.89%) | Memory: 59.6 MB (99.54%) - LeetSync

--- solutions/952-word-subsets/word-subsets.cpp
+++ solutions/952-word-subsets/word-subsets.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    vector<string> wordSubsets(vector<string>& A, vector<string>& B) {
-        string unit = "";
-        for (auto& v : B) {
-            string tmp;
-            sort(v.begin(), v.end());
-            set_union(v.begin(), v.end(), unit.begin(), unit.end(), back_inserter(tmp));
-            swap(unit, tmp);
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        auto get_word_freqs {[](string& w) -> array<int, 26> {
+            array<int, 26> ret {};
+            for (auto c : w)
+                ++ret[c - 'a'];
+
+            return ret;
+        }};
+
+        array<int, 26> words2_max_freqs {};
+        for (auto& w : words2) {
+            const auto current {get_word_freqs(w)};
+            for (int i {0}; i < 26; ++i)
+                words2_max_freqs[i] = max (words2_max_freqs[i], current[i]);
         }
-        vector<string> res;
-        for (auto a : A) {
-            string t = a;
-            sort(t.begin(), t.end());
-            if (includes(t.begin(), t.end(), unit.begin(), unit.end())) {
-                res.push_back(a);
-            }
+
+        vector<string> ret {};
+        ret.reserve (words1.size());
+
+        for (auto& w : words1) {
+            const auto current {get_word_freqs(w)};
+
+            bool is_universal {true};
+            for (int i {0}; i < 26; ++i)
+                if (current[i] < words2_max_freqs[i]) {
+                    is_universal = false;
+                    break;
+                }     
+
+            if (is_universal)
+                ret.push_back (std::move(w));
         }
-        return res;
+
+        return ret;
     }
 };
