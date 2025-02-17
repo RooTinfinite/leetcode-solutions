58c497a87dc48ccaf5d92cd44517e98daf7dddb4
Piotr M <filo74b@gmail.com>
2025-02-17 03:44:11 +0100

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.58%) - LeetSync

---
+++ solutions/1086-divisor-game/divisor-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        int winner = 0;
+        while (n != 1) {
+            winner += 1;
+            n -= 1;
+        }
+
+        if (winner % 2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
