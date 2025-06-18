e3bcf72cf0a5cffdfdd7aa89b938f58c3f7e5dee
Piotr M <filo74b@gmail.com>
2025-06-18 07:44:31 +0200

    Time: 83 ms (88.89%) | Memory: 85 MB (77.78%) - LeetSync

---
+++ solutions/3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,11 @@
+var divideArray = function(nums, k) {
+    nums.sort((a, b) => a - b);
+    const ans = [];
+    for (let i = 0; i < nums.length; i += 3) {
+        if (nums[i + 2] - nums[i] > k) {
+            return [];
+        }
+        ans.push([nums[i], nums[i + 1], nums[i + 2]]);
+    }
+    return ans;
+};
