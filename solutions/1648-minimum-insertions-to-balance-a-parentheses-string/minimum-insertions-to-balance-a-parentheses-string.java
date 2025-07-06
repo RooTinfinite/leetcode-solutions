f032d259ca2127af3114338f16cdc4ac5c75055f
Piotr M <filo74b@gmail.com>
2025-07-06 22:05:29 +0200

    Time: 11 ms (69.12%) | Memory: 45.3 MB (82.88%) - LeetSync

---
+++ solutions/1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minInsertions(String s) {
+        int openCounts = 0; // Tracks unmatched '('
+        int insertions = 0; // Tracks required insertions
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                openCounts++;
+            } else if (i + 1 < s.length() && s.charAt(i + 1) == ')') {
+                if (openCounts > 0) openCounts--;
+                else insertions++;
+                i++; // Skip the next ')'
+            } else {
+                if (openCounts > 0) {
+                    openCounts--;
+                    insertions++;
+                } else {
+                    insertions += 2;
+                }
+            }
+        }
+
+        return insertions + 2 * openCounts;
+    }
+}
