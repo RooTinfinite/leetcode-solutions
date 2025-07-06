34359cb8f88a48b68e9f9ad412029621e155f4f3
Piotr M <filo74b@gmail.com>
2025-07-06 22:27:16 +0200

    Time: 0 ms (100.00%) | Memory: 13 MB (75.05%) - LeetSync

--- solutions/1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.cpp
+++ solutions/1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.cpp
@@ -1,10 +1,24 @@
+//C++ Code
+// Start Of Edwards310's Template...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(a, b, c) for (int a = b; a < c; a++)
+#define ALL(v) v.begin(), v.end()
+#define SORT(v) sort(ALL(v))
+
+// end of Edwards310's Template
 class Solution {
 public:
     double trimMean(vector<int>& arr) {
-        int n = arr.size(), skip = n / 20;
-        auto start = arr.begin() + skip, end = arr.begin() + n - skip;
-        nth_element(arr.begin(), start, arr.end());
-        nth_element(start, end, arr.end());
-        return accumulate(start, end, 0.0) / (n - skip * 2);
+        int sum = 0, cnt = 0, len = arr.size();
+        SORT(arr);
+        int percent5 = len / 20;
+        FOR(i, percent5, len - percent5){
+            sum += arr[i];
+            cnt++;
+        }
+        return (double)sum / cnt;
     }
 };
