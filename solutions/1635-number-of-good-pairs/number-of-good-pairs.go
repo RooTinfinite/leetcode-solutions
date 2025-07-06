2784b49a509a2cdc5789bedeb950e2cb99168ba4
Piotr M <filo74b@gmail.com>
2025-07-06 21:55:28 +0200

    Time: 0 ms (100.00%) | Memory: 4.1 MB (22.98%) - LeetSync

---
+++ solutions/1635-number-of-good-pairs/number-of-good-pairs.go
@@ -0,0 +1,13 @@
+package main
+
+func numIdenticalPairs(A []int) int {
+    ans := 0
+    cnt := make(map[int]int)
+    
+    for _, x := range A {
+        ans += cnt[x]
+        cnt[x]++
+    }
+    
+    return ans
+}
