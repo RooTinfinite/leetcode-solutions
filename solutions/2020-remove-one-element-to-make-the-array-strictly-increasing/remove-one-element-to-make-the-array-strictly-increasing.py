494f6ad3ce847b9790d33b8b2062a0165521df32
Piotr Maminski <filo74b@gmail.com>
2025-08-09 20:51:59 +0200

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.54%) - LeetSync

---
+++ solutions/2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canBeIncreasing(self, nums: List[int]) -> bool:
+        ct = 0
+        for i in range(len(nums) - 1):
+            if nums[i] >= nums[i + 1]:
+                ct += 1
+                if i > 0 and nums[i - 1] >= nums[i + 1]:
+                    if i + 2 < len(nums) and nums[i] >= nums[i + 2]:
+                        return False
+        return ct <= 1
