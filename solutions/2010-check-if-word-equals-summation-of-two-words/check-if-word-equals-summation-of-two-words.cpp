88a7a74b79f1bd275ed5ae8ff8623304fe6e65e3
Piotr Maminski <filo74b@gmail.com>
2025-08-09 20:49:00 +0200

    Time: 0 ms (100.00%) | Memory: 7.9 MB (31.30%) - LeetSync

---
+++ solutions/2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isSumEqual(string first, string second, string target) {
+    auto op = [](string &s) { for(auto &ch : s) ch -= 49; return s; };
+    return stoi(op(first)) + stoi(op(second)) == stoi(op(target));
+}
+};
