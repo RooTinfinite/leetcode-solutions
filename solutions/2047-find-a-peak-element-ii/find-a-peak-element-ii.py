8e18f9b68fc383a821e88c4498a5e3733227f486
Piotr Maminski <filo74b@gmail.com>
2025-08-09 20:51:22 +0200

    Time: 0 ms (100.00%) | Memory: 46 MB (90.96%) - LeetSync

--- solutions/2047-find-a-peak-element-ii/find-a-peak-element-ii.py
+++ solutions/2047-find-a-peak-element-ii/find-a-peak-element-ii.py
@@ -1,59 +1,20 @@
-from collections import deque, defaultdict, Counter, namedtuple
-from heapq import heappush, heappop
-
 class Solution:
-    def findPeakGrid(self, mat):
-        if not mat or not mat[0]:
-            return [-1, -1]
-
-        n, m = len(mat), len(mat[0])
-        visited = set()
-        max_heap = []
-        freq = Counter()
-        position_map = defaultdict(list)
-        path = deque()
-
-        Point = namedtuple("Point", ["value", "i", "j"])
-
-        # Push all elements into a max heap (why limit our search early?)
-        for i in range(n):
-            for j in range(m):
-                val = mat[i][j]
-                heappush(max_heap, (-val, Point(val, i, j)))
-                freq[val] += 1
-                position_map[val].append((i, j))
-
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        while max_heap:
-            _, point = heappop(max_heap)
-            i, j = point.i, point.j
-
-            if (i, j) in visited:
-                continue
-
-            visited.add((i, j))
-            path.append((i, j))
-
-            current = mat[i][j]
-            is_peak = True
-
-            for dx, dy in directions:
-                ni, nj = i + dx, j + dy
-                if 0 <= ni < n and 0 <= nj < m:
-                    if mat[ni][nj] >= current:
-                        is_peak = False
-                        break
-
-            if is_peak:
-                # diagnostics for later analysis
-                most_freq = freq.most_common(1)[0]
-                debug_info = {
-                    "visited_cells": len(visited),
-                    "most_common_value": most_freq,
-                    "sample_path": list(path)[:3]
-                }
-                print("Debug Info:", debug_info)
-                return [i, j]
-
-        return [-1, -1]
+    def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
+        rows=len(mat)
+        cols=len(mat[0])
+        low=0
+        high=cols-1
+        while low<=high:
+            mid=(low+high)//2
+            max_row=0
+            for row in range(rows):
+                if mat[row][mid] > mat[max_row][mid]:
+                    max_row = row
+            leftbig= (mid-1>=0 and mat[max_row][mid]<mat[max_row][mid-1])
+            rightbig=(mid<cols-1 and mat[max_row][mid]<mat[max_row][mid+1])
+            if not leftbig and not rightbig:
+                return [max_row,mid]
+            elif leftbig:
+                high=mid-1
+            else:
+                low=mid+1
