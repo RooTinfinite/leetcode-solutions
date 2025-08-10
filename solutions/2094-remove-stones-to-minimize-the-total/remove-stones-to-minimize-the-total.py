5a128cf71e4daa4feaea10208a6c691300db3aed
Piotr Maminski <filo74b@gmail.com>
2025-08-10 12:24:14 +0200

    Time: 555 ms (75.65%) | Memory: 29.3 MB (61.99%) - LeetSync

---
+++ solutions/2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -0,0 +1,11 @@
+import heapq
+
+class Solution:
+    def minStoneSum(self, piles: list[int], k: int) -> int:
+        h = [-p for p in piles]
+        heapq.heapify(h)
+        for _ in range(k):
+            x = -heapq.heappop(h)
+            x -= x // 2
+            heapq.heappush(h, -x)
+        return -sum(h)
