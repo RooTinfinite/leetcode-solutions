f1fb6f8efce9451f6d7fe5d8f44e9f77c90999ef
Piotr Maminski <filo74b@gmail.com>
2025-08-15 08:40:24 +0200

    Time: 4772 ms (29.12%) | Memory: 247.5 MB (30.80%) - LeetSync

---
+++ solutions/2114-minimum-number-of-work-sessions-to-finish-the-tasks/minimum-number-of-work-sessions-to-finish-the-tasks.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minSessions(self, tasks: List[int], sessionTime: int) -> int:
+        n = len(tasks)
+
+        def clearBit(x, k):
+            return ~(1 << k) & x
+
+        @lru_cache(None)
+        def dp(mask, remainTime):
+            if mask == 0: return 0
+
+            ans = n  # There is up to N work sessions
+            for i in range(n):
+                if (mask >> i) & 1:
+                    newMask = clearBit(mask, i)
+                    if tasks[i] <= remainTime:
+                        ans = min(ans, dp(newMask, remainTime - tasks[i]))  # Consume current session
+                    else:
+                        ans = min(ans, dp(newMask, sessionTime - tasks[i]) + 1)  # Create new session
+
+            return ans
+
+        return dp((1 << n) - 1, 0)
