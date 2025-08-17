1febf6378b629419599d5be5cfd77720e3babb6f
Piotr Maminski <filo74b@gmail.com>
2025-08-17 18:10:36 +0200

    Time: 7 ms (67.11%) | Memory: 17.8 MB (85.23%) - LeetSync

--- solutions/2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.py
+++ solutions/2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.py
@@ -1,39 +1,31 @@
 class Solution:
     def countValidWords(self, sentence: str) -> int:
-        def is_valid_word(token):
-            len_token = len(token)
-            
-            i = 0
-            while (i < len_token):
-                c = token[i]
-                if (ord(c) not in range(ord('a'), ord('z') + 1)
-                    and c != '-'
-                    and c not in ('!', '.', ',')):
-                    return False
-                elif (c == '-'
-                      and i > 1
-                      and not token[:i].isalpha()):
-                    return False
-                elif (c == '-'
-                      and i < len_token - 1
-                      and (not token[i+1:].isalpha() and not (token[i+1:-1].isalpha() and token[-1] in ('!', '.', ',')))):
-                    return False
-                elif (c == '-'
-                      and (i == 0 or i == len_token-1)):
-                    return False
-                elif (c in ('!', '.', ',')
-                      and i < len_token - 1):
-                    return False
-
-                i += 1
-
-            return True
-
-        tokens = [t for t in sentence.split(' ') if t != '']
-        count_valid_words = 0
-        for token in tokens:
-            print(token, is_valid_word(token))
-            if (is_valid_word(token)):
-                count_valid_words += 1
-        
-        return count_valid_words
+        count = 0
+        for word in sentence.split():
+            valid = True
+            hypen = 0
+            for i,w in enumerate(word):
+                if w.islower():
+                    continue
+                if w.isdigit():
+                    valid = False
+                    break
+                if w == '-':
+                    hypen += 1
+                    if hypen > 1:
+                        valid = False
+                        break
+                    elif i == 0 or i == len(word) - 1:
+                        valid = False
+                        break
+                    elif not word[i-1].islower() or not word[i+1].islower():
+                        valid = False
+                        break
+                if w in '!,.':
+                    if i != len(word)-1:
+                        valid = False
+                        break
+            count+=valid
+        return count
+                    
+                
