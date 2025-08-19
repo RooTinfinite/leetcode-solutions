e3ad8117e09b2dcbad98d8cccffba8fbf7682bd0
Piotr Maminski <filo74b@gmail.com>
2025-08-19 07:31:41 +0200

    Time: 0 ms (100.00%) | Memory: 42.9 MB (14.02%) - LeetSync

---
+++ solutions/1310-watering-plants/watering-plants.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+       int curr=capacity;
+       int steps=0;
+       for (int i=0;i<plants.length;i++){
+       if(curr<plants[i]){
+        steps+=2*i+1;
+        curr=capacity-plants[i];
+       }
+       else
+       {
+        steps+=1;
+        curr=curr-plants[i];
+       }
+       } 
+       return steps;
+    }
+}
