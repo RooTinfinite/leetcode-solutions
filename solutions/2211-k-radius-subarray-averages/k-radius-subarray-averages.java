0ced82cfce5aea812c1a841d81669cde23dfbb67
Piotr Maminski <filo74b@gmail.com>
2025-08-21 00:50:08 +0200

    Time: 5 ms (96.09%) | Memory: 60.1 MB (97.83%) - LeetSync

---
+++ solutions/2211-k-radius-subarray-averages/k-radius-subarray-averages.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        // When a single element is considered then its average will be the number itself only.
+        if (k == 0) {
+            return nums;
+        }
+
+        int windowSize = 2 * k + 1;
+        int n = nums.length;
+        int[] averages = new int[n];
+        Arrays.fill(averages, -1);
+
+        // Any index will not have 'k' elements in its left and right.
+        if (windowSize > n) {
+            return averages;
+        }
+
+        // First get the sum of first window of the 'nums' array.
+        long windowSum = 0;
+        for (int i = 0; i < windowSize; ++i) {
+            windowSum += nums[i];
+        }
+        averages[k] = (int) (windowSum / windowSize);
+
+        // Iterate on rest indices which have at least 'k' elements 
+        // on its left and right sides.
+        for (int i = windowSize; i < n; ++i) {
+            // We remove the discarded element and add the new element to get current window sum.
+            // 'i' is the index of new inserted element, and
+            // 'i - (window size)' is the index of the last removed element.
+            windowSum = windowSum - nums[i - windowSize] + nums[i];
+            averages[i - k] = (int) (windowSum / windowSize);
+        }
+
+        return averages;
+    }
+}
