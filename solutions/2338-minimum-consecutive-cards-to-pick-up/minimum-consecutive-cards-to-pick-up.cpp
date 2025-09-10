9b5538005f53381b145d43609e8e608490141065
Piotr Maminski <filo74b@gmail.com>
2025-09-10 03:22:06 +0200

    Time: 130 ms (62.50%) | Memory: 119.1 MB (64.62%) - LeetSync

---
+++ solutions/2338-minimum-consecutive-cards-to-pick-up/minimum-consecutive-cards-to-pick-up.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        int n = cards.size() ;  
+        unordered_map<int, int> mp ;
+        int res = INT_MAX ;
+
+        for(int i=0; i<n; i++){
+            if( mp.find(cards[i]) != mp.end() ){
+                res = min(res, i - mp[cards[i]] + 1 );
+            }
+            mp[cards[i]] = i ;
+        }
+        if(res==INT_MAX) return -1 ;
+        return res ;
+    }
+};
