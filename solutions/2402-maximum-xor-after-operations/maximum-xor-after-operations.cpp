91f878a6ef87668829f7512d6c24847ef7ce5c0e
Piotr Maminski <filo74b@gmail.com>
2025-09-18 09:50:06 +0200

    Time: 0 ms (100.00%) | Memory: 51.8 MB (38.84%) - LeetSync

--- solutions/2402-maximum-xor-after-operations/maximum-xor-after-operations.cpp
+++ solutions/2402-maximum-xor-after-operations/maximum-xor-after-operations.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
-        vector<int> bits(32,0);
-        for(int &i:nums) {
-            for(int j=31;j>=0;j--) {
-                if((i>>j)&1) {
-                    bits[j]++;
-                }
-            }
+        int result = 0;
+        for(int num: nums) {
+            result |= num;
         }
-        int res = 0;
-        for(int i=0;i<32;i++) {
-            if(bits[i]) {
-                res = res | 1<<i;
-            }
-        }
-        return res;
+
+        return result;
     }
 };
