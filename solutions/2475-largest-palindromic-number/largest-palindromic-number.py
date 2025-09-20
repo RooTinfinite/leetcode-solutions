cdb311e140543a4865c65aeb90b47a93f4df2ecb
Piotr Maminski <filo74b@gmail.com>
2025-09-20 08:54:46 +0200

    Time: 264 ms (5.06%) | Memory: 18.8 MB (71.90%) - LeetSync

---
+++ solutions/2475-largest-palindromic-number/largest-palindromic-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def largestPalindromic(self, num: str) -> str:
+        c = Counter(num)
+        if len(c)==1 and c['0']>=1:
+            return "0"
+        m = -1   #storing the mid of the number
+        res1 = ''
+        res2 = ''
+        for i in range(9,-1,-1):
+            while c[str(i)]:
+                if not res1 and i==0:   #to avoid adding 0 at first
+                    break 
+                if c[str(i)]>=2:
+
+                    res1 += str(i)
+                    res2 = str(i) + res2
+                    c[str(i)]-=2
+                if c[str(i)] == 1:
+                    m = max(m,i)  #updating medium to max
+                    c[str(i)]-=1
+                if c[str(i)]==0:
+                    del c[str(i)]
+        return res1+res2 if m==-1 else res1 + str(m) + res2
