8af275e7143de808519f9e3b2559654df476e7b0
Piotr Maminski <filo74b@gmail.com>
2025-09-21 16:10:26 +0200

    Time: 252 ms (31.48%) | Memory: 76.4 MB (5.02%) - LeetSync

---
+++ solutions/2590-maximum-star-sum-of-a-graph/maximum-star-sum-of-a-graph.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxStarSum(self, vals: List[int], edges: List[List[int]], k: int) -> int:
+        
+        graph = defaultdict(set)
+        for i,j in edges:
+            if vals[i] > 0 : graph[j].add(i)
+            if vals[j] > 0 : graph[i].add(j)
+            
+        stars = []
+        for i,v in enumerate(vals):
+            vv = [vals[j] for j in graph[i]]
+            vv.sort(reverse=True)
+            stars.append(v + sum(vv[0:k]))
+            
+        return max(stars)
