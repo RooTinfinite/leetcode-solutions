36b6fdf0ccaff891618820ab75916b527ba0cfe2
Piotr Maminski <filo74b@gmail.com>
2025-09-22 08:40:29 +0200

    Time: 213 ms (48.66%) | Memory: 33.6 MB (63.39%) - LeetSync

---
+++ solutions/2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxDivScore(vector<int>& nums, vector<int>& divisors) {
+
+        int n = divisors.size();
+        int maxcount=INT_MIN;
+        int minelement = INT_MAX;
+        
+        for(int i=0;i<n;i++){
+            
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+
+                if(nums[j]%divisors[i]==0){
+                    count++;
+
+                }
+            }
+           
+   // find max count, if count are equal check for the smallest divisor availbe         
+            if(count>maxcount || (count==maxcount && divisors[i]<minelement) ){
+                maxcount = count;
+                minelement=divisors[i];
+
+
+            }
+
+
+
+        }
+        return minelement;
+        
+    }
+};
