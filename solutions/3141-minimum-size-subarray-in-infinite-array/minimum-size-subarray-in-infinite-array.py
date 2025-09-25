86f0d0d959f9ca0f9c96ad428aab783b05608ca5
Piotr Maminski <filo74b@gmail.com>
2025-09-25 23:48:39 +0200

    Time: 79 ms (42.67%) | Memory: 27.3 MB (46.00%) - LeetSync

---
+++ solutions/3141-minimum-size-subarray-in-infinite-array/minimum-size-subarray-in-infinite-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:
+        q, target = divmod(target, sum(nums))
+        ans = q*len(nums)
+        cand = inf 
+        ii = 0 
+        for i, x in enumerate(nums + nums): 
+            target -= x
+            while target < 0: 
+                target += nums[ii % len(nums)]
+                ii += 1
+            if target == 0: cand = min(cand, i-ii+1)
+        return ans + cand if cand < inf else -1 
