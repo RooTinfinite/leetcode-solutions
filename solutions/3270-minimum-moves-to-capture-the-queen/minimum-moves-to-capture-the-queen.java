678d568b91acfb4cefc23e21a1730b7569f557a5
Piotr Maminski <filo74b@gmail.com>
2025-09-26 20:48:30 +0200

    Time: 1 ms (38.46%) | Memory: 41.8 MB (5.49%) - LeetSync

---
+++ solutions/3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        // horizontal and vertical four directions 
+        for (int[] direction : new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}) {
+            int dx = direction[0];
+            int dy = direction[1];
+            for (int i = 1; i < 8; ++i) {
+                // if we spot bishop just break, because the rook may be in a blind spot to queen. no need to check further
+                if (e + i * dx == c && f + i * dy == d) {
+                    break;
+                }
+                //  if we spot rook , we can capture queen in 1 move
+                if (e + i * dx == a && f + i * dy == b) {
+                    return 1;
+                }
+            }
+        }
+        // move in diagonal four directions 
+        for (int[] direction : new int[][]{{1, 1}, {-1, -1}, {-1, 1}, {1, -1}}) {
+            int dx = direction[0];
+            int dy = direction[1];
+            for (int i = 1; i < 8; ++i) {
+                //  if we spot rook just break, because the bishop may be in a blind spot. no need to check further
+                if (e + i * dx == a && f + i * dy == b) {
+                    break;
+                }
+                //  if we spot bishop , we can capture queen in 1 move
+                if (e + i * dx == c && f + i * dy == d) {
+                    return 1;
+                }
+            }
+        }
+
+        return 2;
+    }
+}
