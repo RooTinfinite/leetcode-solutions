2771b73e98d3f9e475de9ce9ff4e61332f3b2607
Piotr Maminski <filo74b@gmail.com>
2025-10-01 10:59:01 +0200

    Time: 1 ms (100.00%) | Memory: 44.7 MB (37.12%) - LeetSync

---
+++ solutions/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        for (int idx = 0; idx <= nums.size() - 2 * k; idx++) {
+            if (mono(nums, idx, k) && mono(nums, idx + k, k)) {
+                return true;}
+        }
+        return false;}
+
+    private boolean mono(List<Integer> nums, int idx, int k) {
+        for (int i = idx; i < idx + k - 1; i++) {
+            if (nums.get(i) >= nums.get(i + 1)) return false; }
+        return true;}
+}
