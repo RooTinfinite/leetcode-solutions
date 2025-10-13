33999ecc109f1b8544dd08a851894420e93d4583
Piotr Maminski <filo74b@gmail.com>
2025-10-13 15:10:18 +0200

    Time: 213 ms (89.03%) | Memory: 0B (100.00%) - LeetSync

--- solutions/3942-find-drivers-with-improved-fuel-efficiency/find-drivers-with-improved-fuel-efficiency.sql
+++ solutions/3942-find-drivers-with-improved-fuel-efficiency/find-drivers-with-improved-fuel-efficiency.sql
@@ -1,10 +1,10 @@
 WITH stat AS (
     SELECT 
         driver_id, 
-        SUM(CASE WHEN MONTH(trip_date) <= 6 THEN distance_km / fuel_consumed ELSE 0 END) AS frst_tot_fuel_ef,
-        SUM(CASE WHEN MONTH(trip_date) <= 6 THEN 1 ELSE 0 END) AS frst_tot_trips,
-        SUM(CASE WHEN MONTH(trip_date) > 6 THEN distance_km / fuel_consumed ELSE 0 END) AS sec_tot_fuel_ef,
-        SUM(CASE WHEN MONTH(trip_date) > 6 THEN 1 ELSE 0 END) AS sec_tot_trips
+        SUM(CASE WHEN EXTRACT(MONTH FROM trip_date) <= 6 THEN distance_km / fuel_consumed ELSE 0 END) AS frst_tot_fuel_ef,
+        SUM(CASE WHEN EXTRACT(MONTH FROM trip_date) <= 6 THEN 1 ELSE 0 END) AS frst_tot_trips,
+        SUM(CASE WHEN EXTRACT(MONTH FROM trip_date) > 6 THEN distance_km / fuel_consumed ELSE 0 END) AS sec_tot_fuel_ef,
+        SUM(CASE WHEN EXTRACT(MONTH FROM trip_date) > 6 THEN 1 ELSE 0 END) AS sec_tot_trips
     FROM trips
     GROUP BY driver_id
 ), summary AS (
@@ -22,9 +22,9 @@ WITH stat AS (
 SELECT 
     driver_id, 
     driver_name,
-    ROUND(frst_half_avg, 2) AS first_half_avg,
-    ROUND(sec_half_avg, 2) AS second_half_avg,
-    ROUND(sec_half_avg - frst_half_avg, 2) AS efficiency_improvement 
+    ROUND(frst_half_avg::numeric, 2) AS first_half_avg,
+    ROUND(sec_half_avg::numeric, 2) AS second_half_avg,
+    ROUND((sec_half_avg - frst_half_avg)::numeric, 2) AS efficiency_improvement 
 FROM summary
 WHERE sec_half_avg > frst_half_avg
   AND frst_half_avg IS NOT NULL
