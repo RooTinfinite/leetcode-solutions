053d0b548192e4aa5c45715cd77142f951553da1
Piotr Maminski <filo74b@gmail.com>
2025-10-13 15:09:43 +0200

    Time: 487 ms (81.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ solutions/3942-find-drivers-with-improved-fuel-efficiency/find-drivers-with-improved-fuel-efficiency.sql
@@ -0,0 +1,24 @@
+WITH stat AS (
+    SELECT driver_id, 
+        SUM(CASE WHEN MONTH(trip_date) <= 6 THEN distance_km/fuel_consumed ELSE 0 END) AS frst_tot_fuel_ef,
+        SUM(CASE WHEN MONTH(trip_date) <= 6 THEN 1 ELSE 0 END) AS frst_tot_trips,
+        SUM(CASE WHEN MONTH(trip_date) > 6 THEN distance_km/fuel_consumed ELSE 0 END) AS sec_tot_fuel_ef,
+        SUM(CASE WHEN MONTH(trip_date) > 6 THEN 1 ELSE 0 END) AS sec_tot_trips
+    FROM trips
+    GROUP BY 1
+), summary AS (
+    SELECT driver_id, driver_name,
+        frst_tot_fuel_ef/frst_tot_trips AS frst_half_avg,
+        sec_tot_fuel_ef/sec_tot_trips AS sec_half_avg
+    FROM stat
+    INNER JOIN drivers USING(driver_id)
+    WHERE stat.frst_tot_trips IS NOT NULL AND stat.sec_tot_trips IS NOT NULL
+)
+
+SELECT driver_id, driver_name,
+    ROUND(frst_half_avg, 2) AS first_half_avg,
+    ROUND(sec_half_avg, 2) AS second_half_avg,
+    ROUND(sec_half_avg - frst_half_avg, 2) AS efficiency_improvement 
+FROM summary
+WHERE sec_half_avg > frst_half_avg
+ORDER BY 5 DESC, 2;
