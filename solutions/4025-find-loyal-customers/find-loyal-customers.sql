66d30a5a494891f17c9c0a1e337f34a2f03881b1
Piotr Maminski <filo74b@gmail.com>
2025-10-18 02:09:20 +0200

    Time: 215 ms (74.17%) | Memory: 0B (100.00%) - LeetSync

--- solutions/4025-find-loyal-customers/find-loyal-customers.sql
+++ solutions/4025-find-loyal-customers/find-loyal-customers.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 
 SELECT customer_id
 FROM customer_transactions 
 GROUP BY customer_id 
-HAVING COUNT( IF(transaction_type = 'purchase', 1,  NULL) ) > 2 
-AND DATEDIFF(MAX(transaction_date) , MIN(transaction_date)) > 29 
-AND ( COUNT( IF(transaction_type = 'refund' , 1, NULL) ) / COUNT(1) ) < 0.2
+HAVING COUNT( CASE WHEN transaction_type = 'purchase' THEN 1 ELSE NULL END ) > 2 
+AND MAX(transaction_date) - MIN(transaction_date) > 29 
+AND ( COUNT( CASE WHEN transaction_type = 'refund' THEN 1 ELSE NULL END) / COUNT(1) :: FLOAT ) < 0.2
 ORDER BY customer_id
