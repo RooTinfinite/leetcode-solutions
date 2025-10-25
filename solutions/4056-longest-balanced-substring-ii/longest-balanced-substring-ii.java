b95b7580fc1335c8bbb2e0f5e42988d00badc974
Piotr Maminski <filo74b@gmail.com>
2025-10-25 11:59:53 +0200

    Time: 800 ms (11.84%) | Memory: 144.4 MB (5.24%) - LeetSync

---
+++ solutions/4056-longest-balanced-substring-ii/longest-balanced-substring-ii.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    private int n;
+
+    // key function: converts 3 counts into a unique number
+    private long key(long[] cnt) {
+        return cnt[0] + cnt[1] * (n + 1L) + cnt[2] * (n + 1L) * (n + 1L);
+    }
+
+    public int longestBalanced(String s) {
+        n = s.length();
+        long[] cnt = new long[3];      // counts of 'a','b','c'
+        long[] cur = new long[3];
+        int ans = 0;
+
+        // create 8 maps for all subsets
+        Map<Long, Integer>[] mp = new HashMap[8];
+        for (int j = 0; j < 8; j++) {
+            mp[j] = new HashMap<>();
+            mp[j].put(0L, -1);          // initialize with 0 -> -1
+        }
+
+        for (int i = 0; i < n; i++) {
+            int x = s.charAt(i) - 'a';
+            cnt[x]++;
+
+            // iterate over all non-empty subsets (1..7)
+            for (int m = 7; m > 0; m--) {
+                long mind = n;
+                for (int b = 0; b < 3; b++) {
+                    int bit = 1 << b;
+                    if ((bit & m) != 0) mind = Math.min(mind, cnt[b]);
+                    cur[b] = cnt[b];
+                }
+
+                for (int b = 0; b < 3; b++) {
+                    int bit = 1 << b;
+                    if ((bit & m) != 0) cur[b] -= mind;
+                }
+
+                long k = key(cur);
+                if (mp[m].containsKey(k)) {
+                    ans = Math.max(ans, i - mp[m].get(k));
+                } else {
+                    mp[m].put(k, i);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
