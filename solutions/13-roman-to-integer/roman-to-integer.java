8fbdbc81e9b03a0f4889abbb6f0e7cd6669b66ea
Piotr Maminski <filo74b@gmail.com>
2025-10-27 11:09:11 +0100

    Time: 3 ms (82.27%) | Memory: 44.8 MB (70.73%) - LeetSync

--- solutions/13-roman-to-integer/roman-to-integer.java
+++ solutions/13-roman-to-integer/roman-to-integer.java
@@ -1,25 +1,31 @@
 class Solution {
+    static Map<String, Integer> values = new HashMap<>();
+
+    static {
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+    }
+
     public int romanToInt(String s) {
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
-        
-        int n = s.length();
-        int result = roman.get(s.charAt(n-1));
-        
-        for(int i = n-2; i >= 0; i--) {
-            if(roman.get(s.charAt(i)) < roman.get(s.charAt(i+1))) {
-                result -= roman.get(s.charAt(i));
+        String lastSymbol = s.substring(s.length() - 1);
+        int lastValue = values.get(lastSymbol);
+        int total = lastValue;
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            String currentSymbol = s.substring(i, i + 1);
+            int currentValue = values.get(currentSymbol);
+            if (currentValue < lastValue) {
+                total -= currentValue;
             } else {
-                result += roman.get(s.charAt(i));
+                total += currentValue;
             }
+            lastValue = currentValue;
         }
-        
-        return result;
+        return total;
     }
 }
