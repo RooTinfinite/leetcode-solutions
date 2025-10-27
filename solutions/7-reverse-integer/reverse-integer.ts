1e60e0da3ebef4125e0850cc9f302cdc5318e2e6
Piotr Maminski <filo74b@gmail.com>
2025-10-27 11:05:30 +0100

    Time: 63 ms (12.40%) | Memory: 58.2 MB (13.29%) - LeetSync

---
+++ solutions/7-reverse-integer/reverse-integer.ts
@@ -0,0 +1,19 @@
+function reverse(x: number): number {
+    let rev: number = 0;
+    while (x !== 0) {
+        let pop: number = x % 10;
+        x = (x - pop) / 10;
+        if (
+            rev > Math.pow(2, 31) / 10 ||
+            (rev === Math.pow(2, 31) / 10 && pop > 7)
+        )
+            return 0;
+        if (
+            rev < Math.pow(-2, 31) / 10 ||
+            (rev === Math.pow(-2, 31) / 10 && pop < -8)
+        )
+            return 0;
+        rev = rev * 10 + pop;
+    }
+    return rev;
+}
