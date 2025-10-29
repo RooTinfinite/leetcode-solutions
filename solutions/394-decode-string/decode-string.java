4f5f3c695889898230e7937154efad574db70dc8
Piotr Maminski <filo74b@gmail.com>
2025-10-29 10:54:49 +0100

    Time: 0 ms (100.00%) | Memory: 41.4 MB (91.70%) - LeetSync

--- solutions/394-decode-string/decode-string.java
+++ solutions/394-decode-string/decode-string.java
@@ -1,30 +1,25 @@
 class Solution {
-    public String decodeString(String s) {
-        Stack<Integer> st = new Stack<>();
-        Stack<StringBuilder> st1 = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        int n = 0;
-
-        for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                n = n * 10 + (c - '0');
-            } else if (c == '[') {
-                st.push(n);
-                n = 0;
-                st1.push(sb);
-                sb = new StringBuilder();
-            } else if (c == ']') {
-                int k = st.pop();
-                StringBuilder temp = sb;
-                sb = st1.pop();
-                while (k-- > 0) {
-                    sb.append(temp);
-                }
-            } else {
-                sb.append(c);
+    int index = 0;
+    String decodeString(String s) {
+        StringBuilder result = new StringBuilder();
+        while (index < s.length() && s.charAt(index) != ']') {
+            if (!Character.isDigit(s.charAt(index)))
+                result.append(s.charAt(index++));
+            else {
+                int k = 0;
+                // build k while next character is a digit
+                while (index < s.length() && Character.isDigit(s.charAt(index)))
+                    k = k * 10 + s.charAt(index++) - '0';
+                // ignore the opening bracket '['    
+                index++;
+                String decodedString = decodeString(s);
+                // ignore the closing bracket ']'
+                index++;
+                // build k[decodedString] and append to the result
+                while (k-- > 0)
+                    result.append(decodedString);
             }
         }
-
-        return sb.toString();
+        return new String(result);
     }
 }
