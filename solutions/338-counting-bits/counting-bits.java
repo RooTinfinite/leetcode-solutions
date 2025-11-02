343b86e881a63db98259aa9fe7056d94714c99f3
Piotr Maminski <filo74b@gmail.com>
2025-11-02 01:19:02 +0100

    Time: 2 ms (94.49%) | Memory: 48.7 MB (6.90%) - LeetSync

--- solutions/338-counting-bits/counting-bits.java
+++ solutions/338-counting-bits/counting-bits.java
@@ -1,8 +1,8 @@
 public class Solution {
-    public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            ans[i] = ans[i >> 1] + (i & 1);
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int x = 1; x <= num; ++x) {
+            ans[x] = ans[x & (x - 1)] + 1;
         }
         return ans;
     }
