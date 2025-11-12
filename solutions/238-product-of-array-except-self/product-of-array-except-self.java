459087cca0882edfa45e7a73c92eec2556bbbf95
Piotr Maminski <filo74b@gmail.com>
2025-11-13 00:14:16 +0100

    Time: 2 ms (88.95%) | Memory: 72.1 MB (6.71%) - LeetSync

--- solutions/238-product-of-array-except-self/product-of-array-except-self.java
+++ solutions/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,34 @@
 class Solution {
+
     public int[] productExceptSelf(int[] nums) {
-        int numsLength = nums.length;
-        int prefixProduct = 1;
-        int suffixProduct = 1;
-        int[] result = new int[numsLength];
-        for(int i = 0; i < numsLength; i++) {
-            result[i] = prefixProduct;
-            prefixProduct *= nums[i];
+        // The length of the input array
+        int length = nums.length;
+
+        // Final answer array to be returned
+        int[] answer = new int[length];
+
+        // answer[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so the answer[0] would be 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            // answer[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            answer[i] = nums[i - 1] * answer[i - 1];
         }
-        for(int i = numsLength-1; i >= 0; i--) {
-            result[i] *= suffixProduct;
-            suffixProduct *= nums[i];
+
+        // R contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R would be 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // For the index 'i', R would contain the
+            // product of all elements to the right. We update R accordingly
+            answer[i] = answer[i] * R;
+            R *= nums[i];
         }
-        return result;
+
+        return answer;
     }
 }
