6efd30210e3f70c3508444f5fc8d5165245fc1a8
Piotr Maminski <filo74b@gmail.com>
2025-11-18 18:39:32 +0100

    Time: 0 ms (100.00%) | Memory: 4.7 MB (55.73%) - LeetSync

--- solutions/46-permutations/permutations.go
+++ solutions/46-permutations/permutations.go
@@ -1,52 +1,28 @@
-// permute generates all possible permutations of the input slice nums
-func permute(nums []int) [][]int {
-    // res stores all the permutations we'll generate
-    var res [][]int
-    
-    // permutation will store the current permutation we're building
-    // initialized with same length as input slice
-    permutation := make([]int, len(nums))
-    
-    // visit keeps track of which numbers we've used in current permutation
-    // using a boolean array where true means the number at that index is used
-    visit := make([]bool, len(nums))
-    
-    // backtrack is our recursive helper function
-    // index represents the current position we're filling in permutation
-    var backtrack func(int)
-    backtrack = func(index int) {
-        // Base case: if we've filled all positions (index == len(nums))
-        // we've completed one permutation
-        if index == len(nums) {
-            // Create a deep copy of current permutation
-            // We need to copy because permutation slice will be modified
-            copiedPermutation := make([]int, len(nums))
-            copy(copiedPermutation, permutation)
-            
-            // Add the completed permutation to our results
-            res = append(res, copiedPermutation)
-            
-            return
-        }   
-        
-        // Try placing each number at the current index
-        for i := 0; i < len(nums); i++ {
-            // If this number hasn't been used in current permutation
-            if visit[i] == false {
-                // Mark this number as used
-                visit[i] = true
-                // Place this number at current index
-                permutation[index] = nums[i]
-                // Recursively fill the next position
-                backtrack(index+1)   
-                // Backtrack: mark number as unused for next iteration
-                visit[i] = false
+func backtrack(nums []int, cur []int, ans *[][]int) {
+    if len(cur) == len(nums) {
+        copied := make([]int, len(cur))
+        copy(copied, cur)
+        *ans = append(*ans, copied)
+        return
+    }
+    for _, num := range nums {
+        exists := false
+        for _, val := range cur {
+            if val == num {
+                exists = true
+                break
             }
         }
+        if !exists {
+            cur = append(cur, num)
+            backtrack(nums, cur, ans)
+            cur = cur[:len(cur)-1]
+        }
     }
-    
-    // Start the backtracking process from index 0
-    backtrack(0)
-    
-    return res
+}
+
+func permute(nums []int) [][]int {
+    ans := make([][]int, 0)
+    backtrack(nums, []int{}, &ans)
+    return ans
 }
