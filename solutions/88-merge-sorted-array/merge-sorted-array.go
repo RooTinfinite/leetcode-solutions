bf09c3dd2c576fdf3a13ddf49363457870bd70ca
Piotr Maminski <filo74b@gmail.com>
2025-11-20 20:03:29 +0100

    Time: 0 ms (100.00%) | Memory: 4.3 MB (37.82%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.go
+++ solutions/88-merge-sorted-array/merge-sorted-array.go
@@ -1,16 +1,19 @@
 func merge(nums1 []int, m int, nums2 []int, n int) {
-    k := m + n - 1
-    i := m - 1
-    j := n - 1
-    
-    for j >= 0 {
-        if i >= 0 && nums1[i] > nums2[j] {
-            nums1[k] = nums1[i]
-            i--
-        } else {
-            nums1[k] = nums2[j]
-            j--
+    // Set p1 and p2 to point to the end of their respective arrays.
+    p1 := m - 1
+    p2 := n - 1
+    // And move p backward through the array, each time writing
+    // the largest value pointed at by p1 or p2.
+    for p := m + n - 1; p >= 0; p-- {
+        if p2 < 0 {
+            break
         }
-        k--
+        if p1 >= 0 && nums1[p1] > nums2[p2] {
+            nums1[p] = nums1[p1]
+            p1--
+        } else {
+            nums1[p] = nums2[p2]
+            p2--
+        }
     }
 }
