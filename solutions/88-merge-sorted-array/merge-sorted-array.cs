8bc3ed4b9729325b8c6f1067003e1e58044afde5
Piotr Maminski <filo74b@gmail.com>
2025-11-20 20:03:11 +0100

    Time: 0 ms (100.00%) | Memory: 47.3 MB (27.56%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.cs
+++ solutions/88-merge-sorted-array/merge-sorted-array.cs
@@ -1,8 +1,20 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++, i++) {
-            nums1[i] = nums2[j];
+        // Set p1 and p2 to point to the end of their respective arrays.
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // And move p backward through the array, each time writing
+        // the largest value pointed at by p1 or p2.
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) {
+                break;
+            }
+
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
+            } else {
+                nums1[p] = nums2[p2--];
+            }
         }
-        Array.Sort(nums1);
     }
 }
