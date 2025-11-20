560d468ae87ab4c26c1cf548ec747b1fb5ddd02f
Piotr Maminski <filo74b@gmail.com>
2025-11-20 20:03:46 +0100

    Time: 0 ms (100.00%) | Memory: 56.5 MB (22.78%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.ts
+++ solutions/88-merge-sorted-array/merge-sorted-array.ts
@@ -1,16 +1,17 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let k: number = m + n - 1;
-    let i: number = m - 1;
-    let j: number = n - 1;
-    
-    while (j >= 0) {
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
+    // Set p1 and p2 to point to the end of their respective arrays.
+    let p1 = m - 1;
+    let p2 = n - 1;
+    // And move p backward through the array, each time writing
+    // the largest value pointed at by p1 or p2.
+    for (let p = m + n - 1; p >= 0; p--) {
+        if (p2 < 0) {
+            break;
         }
-        k--;
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1--];
+        } else {
+            nums1[p] = nums2[p2--];
+        }
     }
 }
