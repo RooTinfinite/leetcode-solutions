572e7240a8a2888d63898c692d5550290df20047
Piotr Maminski <filo74b@gmail.com>
2025-11-27 06:07:13 +0100

    Time: 9 ms (40.00%) | Memory: 79.6 MB (0.00%) - LeetSync

---
+++ solutions/3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.ts
@@ -0,0 +1,13 @@
+function maxSubarraySum(nums: number[], k: number): number {
+    let n = nums.length;
+    let prefixSum = 0;
+    let maxSum = -Number.MAX_SAFE_INTEGER;
+    let kSum: number[] = Array(k).fill(Number.MAX_SAFE_INTEGER / 2);
+    kSum[k - 1] = 0;
+    for (let i = 0; i < n; i++) {
+        prefixSum += nums[i];
+        maxSum = Math.max(maxSum, prefixSum - kSum[i % k]);
+        kSum[i % k] = Math.min(kSum[i % k], prefixSum);
+    }
+    return maxSum;
+}
