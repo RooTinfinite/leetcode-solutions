25a89544756a3fd75b3ff33317cfd1f6bdb3d7cb
Piotr Maminski <filo74b@gmail.com>
2025-11-29 00:42:07 +0100

    Time: 131 ms (100.00%) | Memory: 25.8 MB (22.95%) - LeetSync

---
+++ solutions/2326-sum-of-scores-of-built-strings/sum-of-scores-of-built-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumScores(self, s: str) -> int:
+        n = len(s)
+        dp, ans = [0] * n , [1] * n 
+        i, j = 1, 0 
+        while i < n:
+            if s[i] == s[j]:
+                ans[i] += ans[j]
+                dp[i] = j + 1
+                i += 1
+                j += 1
+            else:
+                if j:
+                    j = dp[j - 1]
+                else:
+                    i += 1
+        return sum(ans)
+        
