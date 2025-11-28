c3ba2013bed8d2b01b50ab83a95dc728f049ffba
Piotr Maminski <filo74b@gmail.com>
2025-11-29 00:36:32 +0100

    Time: 0 ms (100.00%) | Memory: 43.7 MB (30.81%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.java
+++ solutions/88-merge-sorted-array/merge-sorted-array.java
@@ -1,20 +1,24 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // Set p1 and p2 to point to the end of their respective arrays.
+        // Initialize pointers
         int p1 = m - 1;
         int p2 = n - 1;
+        int p = m + n - 1;
 
-        // And move p backward through the array, each time writing
-        // the largest value pointed at by p1 or p2.
-        for (int p = m + n - 1; p >= 0; p--) {
-            if (p2 < 0) {
-                break;
-            }
+        // While there are elements in nums2 to process
+        while (p2 >= 0) {
+            // If nums1 still has elements AND nums1's element is larger
             if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1--];
+                nums1[p] = nums1[p1];
+                p1--;
             } else {
-                nums1[p] = nums2[p2--];
+                // Otherwise take from nums2
+                nums1[p] = nums2[p2];
+                p2--;
             }
+            p--;
         }
+        // Note: No need to copy remaining elements from nums1 
+        // because they are already in place.
     }
 }
