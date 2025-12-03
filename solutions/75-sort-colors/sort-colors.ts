aa1915191a50b71dd6e51d40e96e469895214cc9
Piotr Maminski <filo74b@gmail.com>
2025-12-03 03:37:10 +0100

    Time: 0 ms (100.00%) | Memory: 54.3 MB (88.07%) - LeetSync

---
+++ solutions/75-sort-colors/sort-colors.ts
@@ -0,0 +1,15 @@
+function sortColors(nums: number[]): void {
+    /*
+    Dutch National Flag problem solution.
+    */
+    let p0 = 0,
+        curr = 0;
+    let p2 = nums.length - 1;
+    while (curr <= p2) {
+        if (nums[curr] == 0) {
+            [nums[curr++], nums[p0++]] = [nums[p0], nums[curr]];
+        } else if (nums[curr] == 2) {
+            [nums[curr], nums[p2--]] = [nums[p2], nums[curr]];
+        } else curr++;
+    }
+}
