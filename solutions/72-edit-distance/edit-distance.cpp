92658ea8024e731adf6c23f617a76147ed1c7f4c
Piotr Maminski <filo74b@gmail.com>
2025-12-03 03:31:43 +0100

    Time: 3 ms (91.93%) | Memory: 12.8 MB (83.93%) - LeetSync

--- solutions/72-edit-distance/edit-distance.cpp
+++ solutions/72-edit-distance/edit-distance.cpp
@@ -1,24 +1,38 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();//first word length
-    const int n = word2.length();//second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+public:
+    vector<vector<int>> memo;
+    int minDistance(string word1, string word2) {
+        int word1Length = word1.size();
+        int word2Length = word2.size();
+        if (word1Length == 0) {
+            return word2Length;
+        }
+        if (word2Length == 0) {
+            return word1Length;
+        }
+        vector<vector<int>> dp(word1.length() + 1,
+                               vector<int>(word2.length() + 1, 0));
+        for (int word1Index = 1; word1Index <= word1Length; word1Index++) {
+            dp[word1Index][0] = word1Index;
+        }
+        for (int word2Index = 1; word2Index <= word2Length; word2Index++) {
+            dp[0][word2Index] = word2Index;
+        }
+        for (int word1Index = 1; word1Index <= word1Length; word1Index++) {
+            for (int word2Index = 1; word2Index <= word2Length; word2Index++) {
+                if (word2[word2Index - 1] == word1[word1Index - 1]) {
+                    dp[word1Index][word2Index] =
+                        dp[word1Index - 1][word2Index - 1];
+                } else {
+                    dp[word1Index][word2Index] =
+                        min(dp[word1Index - 1][word2Index],
+                            min(dp[word1Index][word2Index - 1],
+                                dp[word1Index - 1][word2Index - 1])) +
+                        1;
+                }
+            }
+        }
 
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[m][n];
-  }
+        return dp[word1Length][word2Length];
+    }
 };
