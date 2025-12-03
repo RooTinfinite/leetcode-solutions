7977b39b338b527f25cab63afc5f4107c13e590e
Piotr Maminski <filo74b@gmail.com>
2025-12-03 03:26:58 +0100

    Time: 822 ms (100.00%) | Memory: 124.1 MB (33.33%) - LeetSync

---
+++ solutions/3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.js
@@ -0,0 +1,69 @@
+var countTrapezoids = function (points) {
+    const n = points.length;
+    const inf = 1e9 + 7;
+    const slopeToIntercept = new Map();
+    const midToSlope = new Map();
+    let ans = 0;
+
+    for (let i = 0; i < n; i++) {
+        const [x1, y1] = points[i];
+        for (let j = i + 1; j < n; j++) {
+            const [x2, y2] = points[j];
+            const dx = x1 - x2;
+            const dy = y1 - y2;
+
+            let k, b;
+            if (x2 === x1) {
+                k = inf;
+                b = x1;
+            } else {
+                k = (y2 - y1) / (x2 - x1);
+                b = (y1 * dx - x1 * dy) / dx;
+            }
+
+            const mid = (x1 + x2) * 10000 + (y1 + y2);
+            if (!slopeToIntercept.has(k)) {
+                slopeToIntercept.set(k, []);
+            }
+            if (!midToSlope.has(mid)) {
+                midToSlope.set(mid, []);
+            }
+            slopeToIntercept.get(k).push(b);
+            midToSlope.get(mid).push(k);
+        }
+    }
+
+    for (const sti of slopeToIntercept.values()) {
+        if (sti.length === 1) {
+            continue;
+        }
+        const cnt = new Map();
+        for (const bVal of sti) {
+            cnt.set(bVal, (cnt.get(bVal) || 0) + 1);
+        }
+
+        let totalSum = 0;
+        for (const count of cnt.values()) {
+            ans += totalSum * count;
+            totalSum += count;
+        }
+    }
+
+    for (const mts of midToSlope.values()) {
+        if (mts.length === 1) {
+            continue;
+        }
+        const cnt = new Map();
+        for (const kVal of mts) {
+            cnt.set(kVal, (cnt.get(kVal) || 0) + 1);
+        }
+
+        let totalSum = 0;
+        for (const count of cnt.values()) {
+            ans -= totalSum * count;
+            totalSum += count;
+        }
+    }
+
+    return ans;
+};
