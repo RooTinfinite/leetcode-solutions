3fc1d5d0ebc9fdab0612016b712d1e2774199151
Piotr Maminski <filo74b@gmail.com>
2025-12-04 01:24:49 +0100

    Time: 87 ms (66.92%) | Memory: 18.2 MB (70.26%) - LeetSync

--- solutions/2317-count-collisions-on-a-road/count-collisions-on-a-road.py
+++ solutions/2317-count-collisions-on-a-road/count-collisions-on-a-road.py
@@ -1,17 +1,20 @@
-class Solution(object):
-    def countCollisions(self, directions):
-        n = len(directions)
-        left, right = 0, n - 1
-        stationCount = 0
-        if n == 1:
-            return 0
-        while left < n and directions[left] == "L":
-            left += 1
-        while right > 0 and directions[right] == "R":
-            right -= 1
-        for i in range(left, right + 1):
-            car = directions[i]
-            if car == "S":
-                stationCount += 1                
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        res = 0
+        flag = -1
 
-        return right + 1 - left - stationCount
+        for c in directions:
+            if c == "L":
+                if flag >= 0:
+                    res += flag + 1
+                    flag = 0
+            elif c == "S":
+                if flag > 0:
+                    res += flag
+                flag = 0
+            else:
+                if flag >= 0:
+                    flag += 1
+                else:
+                    flag = 1
+        return res
