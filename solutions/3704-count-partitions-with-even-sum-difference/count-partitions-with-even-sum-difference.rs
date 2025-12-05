21794bbf4c99d3562ace1cb52ad02b582a4bbba6
Piotr Maminski <filo74b@gmail.com>
2025-12-05 17:33:27 +0100

    Time: 0 ms (100.00%) | Memory: 2.2 MB (44.44%) - LeetSync

---
+++ solutions/3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn count_partitions(nums: Vec<i32>) -> i32 {
+        let total_sum: i32 = nums.iter().sum();
+        if total_sum % 2 == 0 {
+            nums.len() as i32 - 1
+        } else {
+            0
+        }
+    }
+}
