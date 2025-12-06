9565843dad8baf0f03108d3e8374499401ffcdfd
Piotr Maminski <filo74b@gmail.com>
2025-12-06 23:26:53 +0100

    Time: 9 ms (100.00%) | Memory: 47.5 MB (100.00%) - LeetSync

---
+++ solutions/4099-minimum-operations-to-make-binary-palindrome/minimum-operations-to-make-binary-palindrome.java
@@ -0,0 +1,44 @@
+class Solution {
+    static List<Integer> palindromes = new ArrayList<>();
+    static{
+        for(int i = 1; i <= 5000; i++){
+            if(isbinpalindrome(i)) palindromes.add(i);
+        }
+    }
+    
+    //binary palindrome pre compute
+    private static boolean isbinpalindrome(int n){
+        String s = Integer.toBinaryString(n);
+        int l = 0, r = s.length() - 1;
+        while (l < r){
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+    
+    public int[] minOperations(int[] nums) {
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            result[i] = nearestpal(nums[i]);
+        }
+        return result;
+    }
+
+    private int nearestpal(int value){
+        int left = 0, right = palindromes.size() - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            int curr = palindromes.get(mid);
+
+            ans = Math.min(ans, Math.abs(curr - value));
+
+            if (curr == value) return 0;
+            else if (curr < value) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return ans;
+    }
+}
