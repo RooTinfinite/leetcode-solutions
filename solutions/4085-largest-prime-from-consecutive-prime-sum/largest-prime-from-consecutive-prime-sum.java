cc197f0fd23605c9ec994e9150fff7748e62a1fa
Piotr Maminski <filo74b@gmail.com>
2025-12-08 01:12:32 +0100

    Time: 205 ms (53.57%) | Memory: 46.6 MB (54.29%) - LeetSync

---
+++ solutions/4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int largestPrime(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) isPrime[i] = true;
+        isPrime[0] = isPrime[1] = false;
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) isPrime[j] = false;
+            }
+        }
+        
+        int sum = 0, ans = 0;
+        for (int i = 2; i <= n && sum <= n; i++) {
+            if (isPrime[i]) {
+                sum += i;
+                if (sum <= n && isPrime[sum]) ans = sum;
+            }
+        }
+        return ans;
+    }
+}
