691fd72b79290be253d25c9f1df05ed157d0b66a
Piotr Maminski <filo74b@gmail.com>
2025-12-14 01:03:59 +0100

    Time: 42 ms (0.00%) | Memory: 15.8 MB (0.00%) - LeetSync

---
+++ solutions/2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.c
@@ -0,0 +1,28 @@
+int numberOfWays(char * corridor){
+    // Store 1000000007 in a variable for convenience
+    const int MOD = 1e9 + 7;
+
+    // Initialize the array to store the result of each sub-problem
+    int count[strlen(corridor) + 1][3];
+
+    // Base cases
+    count[strlen(corridor)][0] = 0;
+    count[strlen(corridor)][1] = 0;
+    count[strlen(corridor)][2] = 1;
+
+    // Fill the array in a bottom-up fashion
+    for (int index = strlen(corridor) - 1; index >= 0; index--) {
+        if (corridor[index] == 'S') {
+            count[index][0] = count[index + 1][1];
+            count[index][1] = count[index + 1][2];
+            count[index][2] = count[index + 1][1];
+        } else {
+            count[index][0] = count[index + 1][0];
+            count[index][1] = count[index + 1][1];
+            count[index][2] = (count[index + 1][0] + count[index + 1][2]) % MOD;
+        }
+    }
+
+    // Return the result
+    return count[0][0];
+}
