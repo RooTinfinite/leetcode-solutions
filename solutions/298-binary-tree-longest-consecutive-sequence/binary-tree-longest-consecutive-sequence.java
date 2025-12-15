65f68fcb68a777f809fa0624ee202cf671943e7c
Piotr Maminski <filo74b@gmail.com>
2025-12-15 20:21:28 +0100

    Time: 1 ms (87.12%) | Memory: 47.1 MB (35.62%) - LeetSync

--- solutions/298-binary-tree-longest-consecutive-sequence/binary-tree-longest-consecutive-sequence.java
+++ solutions/298-binary-tree-longest-consecutive-sequence/binary-tree-longest-consecutive-sequence.java
@@ -1,71 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int maxLen =0;
+public class Solution {
+    private int max = 0;
     public int longestConsecutive(TreeNode root) {
-        
-        if (root == null) return 0;
-
-        if (root.left == null && root.right==null) return 1;
-
-        conSeq(root);
-
-        return maxLen;
+        if(root == null) return 0;
+        helper(root, 0, root.val);
+        return max;
     }
-
-    private int conSeq(TreeNode root)
-    {
-        if (root == null) return 0;
-
-        if (root.left == null && root.right == null) return 1;
-
-        int left=0, right=0;
-        int leftLen =0;
-        int rightLen =0;
-        int currLen =0;
-        if (root.left != null)
-        {
-            left = conSeq(root.left);
-
-            if (root.left.val - root.val == 1)
-            {
-                leftLen = left + 1;
-            }else
-            {
-                leftLen = 1;
-            }
-        }
-
-        if (root.right != null)
-        {
-            right = conSeq(root.right);
-
-            if (root.right.val - root.val == 1)
-            {
-                rightLen = right + 1;
-            }else
-            {
-                rightLen = 1;
-            }
-        }
-
-        currLen = Math.max(currLen, Math.max(rightLen, leftLen));
-
-        maxLen = Math.max(maxLen, currLen);
-
-        return currLen;
+    
+    public void helper(TreeNode root, int cur, int target){
+        if(root == null) return;
+        if(root.val == target) cur++;
+        else cur = 1;
+        max = Math.max(cur, max);
+        helper(root.left, cur, root.val + 1);
+        helper(root.right, cur, root.val + 1);
     }
 }
