fe200f7dcf9495b01f4927790bfeed8ec5340b53
Piotr Maminski <filo74b@gmail.com>
2025-12-20 04:49:07 +0100

    Time: 1 ms (78.05%) | Memory: 57.7 MB (46.34%) - LeetSync

---
+++ solutions/3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.ts
@@ -0,0 +1,6 @@
+function maxSum(nums: number[]): number {
+    const positiveNumsSet = new Set(nums.filter((num) => num > 0));
+    return positiveNumsSet.size === 0
+        ? Math.max(...nums)
+        : [...positiveNumsSet].reduce((a, b) => a + b, 0);
+}
