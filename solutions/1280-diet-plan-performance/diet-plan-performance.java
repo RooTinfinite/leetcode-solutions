0339e56982cf3c3abadede55da9fb281fa5a85b7
Piotr Maminski <filo74b@gmail.com>
2025-12-29 02:46:21 +0100

    Time: 2 ms (97.52%) | Memory: 51.7 MB (14.88%) - LeetSync

---
+++ solutions/1280-diet-plan-performance/diet-plan-performance.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int dietPlanPerformance(
+        int[] calories,
+        int k,
+        int lower,
+        int upper
+    ) {
+        int sum = 0;
+        int score = 0;
+        for (int i = 0; i < k; i++) {
+            sum += calories[i];
+        }
+        for (int i = k; i < calories.length; i++) {
+            if (sum < lower) {
+                score--;
+            } else if (sum > upper) {
+                score++;
+            }
+            sum += calories[i] - calories[i - k];
+        }
+        if (sum < lower) {
+            score--;
+        } else if (sum > upper) {
+            score++;
+        }
+        return score;
+    }
+}
