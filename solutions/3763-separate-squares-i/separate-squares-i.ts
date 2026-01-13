e65419b9b235b994ac60b9f0cd8f78cb997012a6
Piotr Maminski <filo74b@gmail.com>
2026-01-13 12:27:30 +0100

    Time: 333 ms (50.00%) | Memory: 89.8 MB (100.00%) - LeetSync

---
+++ solutions/3763-separate-squares-i/separate-squares-i.ts
@@ -0,0 +1,31 @@
+function separateSquares(squares: number[][]): number {
+    let max_y = 0,
+        total_area = 0;
+    for (const [x, y, l] of squares) {
+        total_area += l * l;
+        max_y = Math.max(max_y, y + l);
+    }
+
+    const check = (limit_y: number): boolean => {
+        let area = 0;
+        for (const [x, y, l] of squares) {
+            if (y < limit_y) {
+                area += l * Math.min(limit_y - y, l);
+            }
+        }
+        return area >= total_area / 2;
+    };
+
+    let lo = 0,
+        hi = max_y;
+    const eps = 1e-5;
+    while (Math.abs(hi - lo) > eps) {
+        const mid = (hi + lo) / 2;
+        if (check(mid)) {
+            hi = mid;
+        } else {
+            lo = mid;
+        }
+    }
+    return hi;
+}
