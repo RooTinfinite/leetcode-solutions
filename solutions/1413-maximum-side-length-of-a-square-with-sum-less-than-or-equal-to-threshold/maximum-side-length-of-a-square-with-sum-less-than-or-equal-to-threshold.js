5b1aa1b08e7045e2814bbe96e070106c63841521
Piotr Maminski <filo74b@gmail.com>
2026-01-19 02:37:09 +0100

    Time: 11 ms (81.82%) | Memory: 62.6 MB (54.55%) - LeetSync

---
+++ solutions/1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.js
@@ -0,0 +1,35 @@
+var maxSideLength = function (mat, threshold) {
+    const m = mat.length,
+        n = mat[0].length;
+    const P = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            P[i][j] =
+                P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + mat[i - 1][j - 1];
+        }
+    }
+
+    const getRect = (x1, y1, x2, y2) => {
+        return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1];
+    };
+
+    const r = Math.min(m, n);
+    let ans = 0;
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            for (let c = ans + 1; c <= r; c++) {
+                if (
+                    i + c - 1 <= m &&
+                    j + c - 1 <= n &&
+                    getRect(i, j, i + c - 1, j + c - 1) <= threshold
+                ) {
+                    ans = c;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return ans;
+};
