8ca5bf3dc1b126aa5f53fe1ff6e5258898a29595
Piotr Maminski <filo74b@gmail.com>
2026-01-21 23:33:48 +0100

    Time: 0 ms (100.00%) | Memory: 5.2 MB (100.00%) - LeetSync

---
+++ solutions/3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.go
@@ -0,0 +1,12 @@
+func minBitwiseArray(nums []int) []int {
+	for i, x := range nums {
+		res := -1
+		d := 1
+		for (x & d) != 0 {
+			res = x - d
+			d <<= 1
+		}
+		nums[i] = res
+	}
+	return nums
+}
