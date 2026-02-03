a2f70870fe322235a3f718a8277e07f68f3a54b0
Piotr Maminski <filo74b@gmail.com>
2026-02-03 02:22:59 +0100

    Time: 0 ms (100.00%) | Memory: 4.6 MB (80.00%) - LeetSync

---
+++ solutions/3952-trionic-array-i/trionic-array-i.go
@@ -0,0 +1,20 @@
+func isTrionic(nums []int) bool {
+	n := len(nums)
+	i := 1
+	for i < n && nums[i-1] < nums[i] {
+		i++
+	}
+	p := i - 1
+
+	for i < n && nums[i-1] > nums[i] {
+		i++
+	}
+	q := i - 1
+
+	for i < n && nums[i-1] < nums[i] {
+		i++
+	}
+	flag := i - 1
+
+	return p != 0 && q != p && flag == n-1 && flag != q
+}
