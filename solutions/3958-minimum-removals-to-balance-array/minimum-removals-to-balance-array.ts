e5787a5f3b4bc2ec445c6bee8c931ff6aaac4e9f
Piotr Maminski <filo74b@gmail.com>
2026-02-06 16:14:42 +0100

    Time: 84 ms (25.00%) | Memory: 72.8 MB (25.00%) - LeetSync

---
+++ solutions/3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.ts
@@ -0,0 +1,15 @@
+function minRemoval(nums: number[], k: number): number {
+    const n = nums.length;
+    nums.sort((a, b) => a - b);
+
+    let ans = n;
+    let right = 0;
+    for (let left = 0; left < n; left++) {
+        while (right < n && nums[right] <= nums[left] * k) {
+            right++;
+        }
+        ans = Math.min(ans, n - (right - left));
+    }
+
+    return ans;
+}
