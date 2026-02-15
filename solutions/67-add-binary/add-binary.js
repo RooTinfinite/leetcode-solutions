24a841e80f0b7246d1139e578d4a3667d9ed2616
Piotr Maminski <filo74b@gmail.com>
2026-02-15 04:33:16 +0100

    Time: 0 ms (100.00%) | Memory: 55.3 MB (58.73%) - LeetSync

--- solutions/67-add-binary/add-binary.js
+++ solutions/67-add-binary/add-binary.js
@@ -1,11 +1,13 @@
+// Bit Manipulation is used to perform operations using bit wise XOR, AND and shift left.
 var addBinary = function (a, b) {
-    // Convert binary strings to BigInt
-    let numA = BigInt("0b" + a);
-    let numB = BigInt("0b" + b);
-
-    // Sum the BigInt numbers
-    let sum = numA + numB;
-
-    // Convert the sum back to a binary string
-    return sum.toString(2);
+    let x = BigInt(`0b${a}`);
+    let y = BigInt(`0b${b}`);
+    let zero = BigInt(0);
+    while (y != zero) {
+        let answer = x ^ y;
+        let carry = (x & y) << BigInt(1);
+        x = answer;
+        y = carry;
+    }
+    return x.toString(2);
 };
