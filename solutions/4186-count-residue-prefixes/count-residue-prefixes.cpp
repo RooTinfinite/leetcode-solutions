1a8195a9b9fdf5f267ba7d478e83fdc63ef9a7f8
Piotr Maminski <filo74b@gmail.com>
2026-02-23 01:31:53 +0100

    Time: 7 ms (9.98%) | Memory: 10 MB (64.82%) - LeetSync

--- solutions/4186-count-residue-prefixes/count-residue-prefixes.cpp
+++ solutions/4186-count-residue-prefixes/count-residue-prefixes.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int residuePrefixes(string s) {
-        set<int> st;
-        int n = s.size();
+        unordered_set<char> st{};
         int ans = 0;
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < s.length(); ++i)
+        {
             st.insert(s[i]);
-            if (st.size() == (i + 1) % 3) ans++;
-            if (st.size() >= 3) return ans;
+            if(st.size() == (i+1) % 3)
+                ++ans;
         }
         return ans;
     }
