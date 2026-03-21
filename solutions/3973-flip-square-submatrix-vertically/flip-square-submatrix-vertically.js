15b54f23721242fe66fccc1879729094116c28e1
Piotr Maminski <filo74b@gmail.com>
2026-03-21 02:07:31 +0100

    Time: 3 ms (25.00%) | Memory: 64.3 MB (75.00%) - LeetSync

---
+++ solutions/3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.js
@@ -0,0 +1,8 @@
+var reverseSubmatrix = function (grid, x, y, k) {
+    for (let i0 = x, i1 = x + k - 1; i0 < i1; i0++, i1--) {
+        for (let j = y; j < y + k; j++) {
+            [grid[i0][j], grid[i1][j]] = [grid[i1][j], grid[i0][j]];
+        }
+    }
+    return grid;
+};
