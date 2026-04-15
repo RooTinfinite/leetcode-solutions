ce7b4408ecd5d08dd685462ef4d4ed0ba7319b56
Piotr Maminski <filo74b@gmail.com>
2026-04-15 16:46:40 +0200

    Time: 0 ms (100.00%) | Memory: 53.2 MB (81.82%) - LeetSync

---
+++ solutions/163-missing-ranges/missing-ranges.js
@@ -0,0 +1,28 @@
+var findMissingRanges = function (nums, lower, upper) {
+    let n = nums.length;
+    let missingRanges = [];
+    if (n === 0) {
+        missingRanges.push([lower, upper]);
+        return missingRanges;
+    }
+
+    // Check for any missing numbers between the lower bound and nums[0].
+    if (lower < nums[0]) {
+        missingRanges.push([lower, nums[0] - 1]);
+    }
+
+    // Check for any missing numbers between successive elements of nums.
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i + 1] - nums[i] <= 1) {
+            continue;
+        }
+        missingRanges.push([nums[i] + 1, nums[i + 1] - 1]);
+    }
+
+    // Check for any missing numbers between the last element of nums and the upper bound.
+    if (upper > nums[n - 1]) {
+        missingRanges.push([nums[n - 1] + 1, upper]);
+    }
+
+    return missingRanges;
+};
