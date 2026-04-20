156f64f0996f437d99e818f43d4dc48b4b884d35
Piotr Maminski <filo74b@gmail.com>
2026-04-20 22:06:10 +0200

    Time: 2 ms (12.00%) | Memory: 55.8 MB (48.00%) - LeetSync

---
+++ solutions/2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.ts
@@ -0,0 +1,13 @@
+function maxDistance(colors: number[]): number {
+    const n = colors.length;
+    let res = 0; // the maximum distance between two houses of different colors
+    // traverse the indices of two houses and maintain the maximum distance
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (colors[i] !== colors[j]) {
+                res = Math.max(res, j - i);
+            }
+        }
+    }
+    return res;
+}
