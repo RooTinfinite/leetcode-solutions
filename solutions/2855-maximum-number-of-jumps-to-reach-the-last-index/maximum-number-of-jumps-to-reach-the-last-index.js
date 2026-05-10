c391c076506a9eb18c35f9a2a3692c8a9d63e699
Piotr Maminski <filo74b@gmail.com>
2026-05-10 08:47:42 +0200

    Time: 14 ms (94.44%) | Memory: 59.4 MB (83.33%) - LeetSync

---
+++ solutions/2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.js
@@ -0,0 +1,15 @@
+var maximumJumps = function (nums, target) {
+    const n = nums.length;
+    const dp = new Array(n).fill(Number.MIN_SAFE_INTEGER);
+    dp[0] = 0;
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (Math.abs(nums[j] - nums[i]) <= target) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return dp[n - 1] < 0 ? -1 : dp[n - 1];
+};
