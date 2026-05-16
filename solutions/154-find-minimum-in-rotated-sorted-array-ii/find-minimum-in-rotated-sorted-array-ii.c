af86cfef77a6bce67aca1b853f65d098ec2cfee8
Piotr Maminski <filo74b@gmail.com>
2026-05-16 07:52:59 +0200

    Time: 0 ms (100.00%) | Memory: 9.1 MB (82.67%) - LeetSync

---
+++ solutions/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.c
@@ -0,0 +1,14 @@
+int findMin(int* nums, int numsSize) {
+    int low = 0, high = numsSize - 1;
+
+    while (low < high) {
+        int pivot = low + (high - low) / 2;
+        if (nums[pivot] < nums[high])
+            high = pivot;
+        else if (nums[pivot] > nums[high])
+            low = pivot + 1;
+        else
+            high -= 1;
+    }
+    return nums[low];
+}
