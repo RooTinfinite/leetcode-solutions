2db7d38a926aa092102bd3cbaa579dd2094ee714
Piotr Maminski <filo74b@gmail.com>
2026-05-22 10:03:59 +0200

    Time: 0 ms (100.00%) | Memory: 54 MB (47.41%) - LeetSync

---
+++ solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,21 @@
+var search = function (nums, target) {
+    let n = nums.length;
+    let left = 0,
+        right = n - 1;
+    while (left <= right) {
+        let mid = left + (((right - left) / 2) | 0);
+        // Case 1: find target
+        if (nums[mid] == target) return mid;
+        // Case 2: Subarray on mid's left is sorted
+        else if (nums[mid] >= nums[left]) {
+            if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+        // Case 3: Subarray on mid's right is sorted
+        else {
+            if (target <= nums[right] && target > nums[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
+    }
+    return -1;
+};
