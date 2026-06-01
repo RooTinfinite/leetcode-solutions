c06fc14171767e024b3afdeefc52a880644d3da1
Piotr Maminski <filo74b@gmail.com>
2026-06-01 10:51:27 +0200

    Time: 2 ms (53.33%) | Memory: 55.5 MB (100.00%) - LeetSync

---
+++ solutions/2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.ts
@@ -0,0 +1,11 @@
+function minimumCost(cost: number[]): number {
+    cost.sort((a, b) => b - a);
+    let res = 0;
+    const n = cost.length;
+    for (let i = 0; i < n; ++i) {
+        if (i % 3 !== 2) {
+            res += cost[i];
+        }
+    }
+    return res;
+}
