41fceaf5cece1b2c0b1d7bd073667d7f3574bcc4
Piotr Maminski <filo74b@gmail.com>
2026-07-06 10:11:58 +0200

    Time: 9 ms (12.77%) | Memory: 58.8 MB (12.77%) - LeetSync

---
+++ solutions/1222-remove-covered-intervals/remove-covered-intervals.js
@@ -0,0 +1,14 @@
+var removeCoveredIntervals = function (intervals) {
+  intervals.sort(([a, b], [c, d]) => a - c || d - b);
+
+  let res = 0;
+  let end = Number.MIN_SAFE_INTEGER;
+
+  intervals.forEach(([, b]) => {
+    if (b > end) res++;
+
+    end = Math.max(end, b);
+  });
+
+  return res;
+};
