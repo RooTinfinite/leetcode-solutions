c2bb745eb364de9fee236a9bfb7fa612d7b75f4d
Piotr Maminski <filo74b@gmail.com>
2026-07-09 02:22:06 +0200

    Time: 52 ms (7.14%) | Memory: 200 MB (7.14%) - LeetSync

---
+++ solutions/4296-zigzag-level-sum-of-binary-tree/zigzag-level-sum-of-binary-tree.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Long> zigzagLevelSum(TreeNode root) {
+        List<List<TreeNode>> list = new ArrayList<>();
+
+        goDFS(root, 0, list);
+
+        List<Long> result = new ArrayList<>();
+        int l = 0;
+        long sum = 0;
+        for (List<TreeNode> lvl: list) {
+            if (l++ % 2 == 0) {
+                for (TreeNode node: lvl) {
+                    if (node.left == null) 
+                        break;
+                    sum += node.val;
+                }
+            } else {
+                for (int i = lvl.size() - 1; i >= 0; i--) {
+                    TreeNode node = lvl.get(i);
+                    if (node.right == null) 
+                        break;
+                    sum += node.val;
+                }
+            }
+            result.add(sum);
+            sum = 0;
+        }
+        return result;
+    }
+
+    private void goDFS(TreeNode node, int lvl, List<List<TreeNode>> list) {
+        if (node == null) return;
+
+        if (lvl >= list.size()) {
+            list.add(new ArrayList<>());
+        }
+
+        list.get(lvl).add(node);
+
+        goDFS(node.left, lvl + 1, list);
+        goDFS(node.right, lvl + 1, list);
+    }
+}
