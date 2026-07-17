8d7a85f0fbe0dfc6ffc6c6a22a0321619b859484
Piotr Maminski <filo74b@gmail.com>
2026-07-17 08:03:17 +0200

    Time: 535 ms (52.50%) | Memory: 40.1 MB (62.50%) - LeetSync

--- solutions/3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
+++ solutions/3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.py
@@ -1,31 +1,22 @@
-from itertools import accumulate
-from bisect import bisect_right
-from typing import List
-
 class Solution:
     def gcdValues(self, nums: List[int], queries: List[int]) -> List[int]:
-        maxv = max(nums)
-        co = [0] * (maxv + 1)
-        lcount = [0] * (maxv + 1)
-        
-        # Count the occurrence of each number in nums
-        for v in nums:
-            co[v] += 1
-        
-        # Calculate lcount for each divisor
-        for i in range(1, maxv + 1):
-            x = 0
-            for mul in range(i, maxv + 1, i):
-                x += co[mul]
-            lcount[i] = x * (x - 1) // 2
-        
-        # Adjust lcount to subtract over-counted GCDs
-        for i in range(maxv, 0, -1):
-            for mul in range(2 * i, maxv + 1, i):
-                lcount[i] -= lcount[mul]
-        
-        # Accumulate counts for answering queries efficiently
-        acc = list(accumulate(lcount))
-        
-        # Answer each query by finding the appropriate GCD count using binary search
-        return [bisect_right(acc, q) for q in queries]
+        m = max(nums)
+        cnt = [0] * (m + 1)
+        for num in nums:
+            cnt[num] += 1
+        for i in range(1, m + 1):
+            for j in range(i * 2, m + 1, i):
+                cnt[i] += cnt[j]
+        for i in range(1, m + 1):
+            cnt[i] = cnt[i] * (cnt[i] - 1) // 2
+        for i in range(m, 0, -1):
+            for j in range(i * 2, m + 1, i):
+                cnt[i] -= cnt[j]
+        for i in range(1, m + 1):
+            cnt[i] += cnt[i - 1]
+        ans = []
+        for q in queries:
+            q += 1
+            pos = bisect_left(cnt, q)
+            ans.append(pos)
+        return ans
