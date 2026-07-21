fc5695bf5a78eacb5676d2ef37a295d1333c8119
Piotr Maminski <filo74b@gmail.com>
2026-07-21 09:59:04 +0200

    Time: 137 ms (57.14%) | Memory: 70.8 MB (92.86%) - LeetSync

---
+++ solutions/3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.js
@@ -0,0 +1,28 @@
+var maxActiveSectionsAfterTrade = function (s) {
+    const n = s.length;
+    let cnt1 = 0;
+    for (let c of s) {
+        if (c === "1") cnt1++;
+    }
+
+    let i = 0;
+    let bestGain = 0;
+    let prev = -Infinity;
+    let cur = 0;
+
+    while (i < n) {
+        const start = i;
+        while (i < n && s[i] === s[start]) {
+            i++;
+        }
+        if (s[start] === "0") {
+            cur = i - start;
+            if (prev !== -Infinity) {
+                bestGain = Math.max(bestGain, prev + cur);
+            }
+            prev = cur;
+        }
+    }
+
+    return cnt1 + bestGain;
+};
