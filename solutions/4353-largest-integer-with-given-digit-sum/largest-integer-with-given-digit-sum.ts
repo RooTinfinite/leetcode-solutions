c2c2276098a1e66203335716b323cd3ab707beaf
Piotr Maminski <filo74b@gmail.com>
2026-08-09 20:22:56 +0200

    Time: 0 ms (100.00%) | Memory: 55.8 MB (57.58%) - LeetSync

---
+++ solutions/4353-largest-integer-with-given-digit-sum/largest-integer-with-given-digit-sum.ts
@@ -0,0 +1,8 @@
+function largestInteger(n: number, s: number) {
+    let res = 0;
+    for (const min = Math.min; s; s -= min(s, 9)) {
+        if (!n--) return -1;
+        res = res * 10 + min(s, 9);
+    }
+    return res * 10 ** n;
+}
