d8907d2df156948dee2c13728538ce672a88bad0
Piotr Maminski <filo74b@gmail.com>
2026-08-12 17:42:24 +0200

    Time: 5 ms (39.92%) | Memory: 41.7 MB (90.90%) - LeetSync

---
+++ solutions/20-valid-parentheses/valid-parentheses.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    private Dictionary<char, char> mappings;
+
+    public Solution() {
+        mappings = new Dictionary<char, char> {
+            { ')', '(' }, { '}', '{' }, { ']', '[' }
+        };
+    }
+
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        foreach (var c in s) {
+            if (mappings.ContainsKey(c)) {
+                char topElement = stack.Count == 0 ? '#' : stack.Pop();
+                if (topElement != mappings[c]) {
+                    return false;
+                }
+            } else {
+                stack.Push(c);
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
