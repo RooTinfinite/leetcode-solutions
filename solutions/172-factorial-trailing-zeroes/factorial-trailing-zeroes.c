c65825cd41aa8c3913bbf55ab55799e14963e92c
Piotr Maminski <filo74b@gmail.com>
2026-08-28 22:31:25 +0200

    Time: 0 ms (100.00%) | Memory: 9 MB (4.76%) - LeetSync

---
+++ solutions/172-factorial-trailing-zeroes/factorial-trailing-zeroes.c
@@ -0,0 +1,9 @@
+int trailingZeroes(int n) {
+    int zeroCount = 0;
+    long currentMultiple = 5;
+    while (n >= currentMultiple) {
+        zeroCount += (n / currentMultiple);
+        currentMultiple *= 5;
+    }
+    return zeroCount;
+}
