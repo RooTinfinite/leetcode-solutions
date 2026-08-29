1e768b3d80c944336c976bd9be6954325f07d99a
Piotr Maminski <filo74b@gmail.com>
2026-08-29 23:05:49 +0200

    Time: 0 ms (100.00%) | Memory: 55.8 MB (100.00%) - LeetSync

---
+++ solutions/4390-minimum-bishop-moves-to-reach-target/minimum-bishop-moves-to-reach-target.js
@@ -0,0 +1,11 @@
+var minBishopMoves = function(x, y) {
+    if (Math.abs(x[0] - y[0]) === Math.abs(y[1] - x[1])) {
+        return 1;
+    }
+
+    if ((x[0] + x[1]) % 2 === (y[0] + y[1]) % 2) {
+        return 2;
+    }
+
+    return -1;
+};
