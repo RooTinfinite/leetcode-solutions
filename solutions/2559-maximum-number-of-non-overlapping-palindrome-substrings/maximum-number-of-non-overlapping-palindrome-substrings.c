67afb0146253cb5392a7cd45f670624583b6d2ef
Piotr Maminski <filo74b@gmail.com>
2026-09-15 18:46:02 +0200

    Time: 0 ms (100.00%) | Memory: 8.8 MB (97.06%) - LeetSync

---
+++ solutions/2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.c
@@ -0,0 +1,30 @@
+bool check(char* s, int l, int r) {
+    while (l < r) {
+        if (s[l++] != s[r--]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int maxPalindromes(char* s, int k) {
+    int n = strlen(s);
+    int ans = 0, start = 0;
+
+    for (int r = k - 1; r < n; ++r) {
+        int l = r - k + 1;
+        if (l >= start && check(s, l, r)) {
+            ++ans;
+            start = r + 1;
+            continue;
+        }
+
+        l = r - k;
+        if (l >= start && check(s, l, r)) {
+            ++ans;
+            start = r + 1;
+        }
+    }
+
+    return ans;
+}
