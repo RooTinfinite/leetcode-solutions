f50487f13001091a8a528deb3455cd1081198e27
Piotr Maminski <filo74b@gmail.com>
2026-09-16 08:39:09 +0200

    Time: 27 ms (68.22%) | Memory: 9.7 MB (82.71%) - LeetSync

--- solutions/1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ solutions/1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,40 +1,22 @@
+const int MOD = 1000000007;
+
 class Solution {
 public:
-#define deb(x) cout<<#x<<" "<<x<<"\n";
-    const int mod=1e9+7;
-    
-    // x1+x2+... xr=n 
-    long long power(long long a,long long b,int mod)
-    {
-        long long res=1;
-        while(b>0)
-        {
-            if(b&1)
-            {
-                res*=a;
-                res%=mod;
-            }
-            a=(a*a)%mod;
-            b/=2;
-        }
-        return res;
-    }    
-
-
     int numberOfSets(int n, int k) {
-        
-        // g1+x1+g2+x2+...+gk+xk+gk1=n-1 ,xi>=1
-        // g1+x1+g2+x2+...+gk+xk+gk1=n-1-k ,xi>=0k,gi>=0
-        // n-1-k+2k+1-1C2k = n+k-1C2k 
-        int N=n+k-1,r=2*k;
-        int curr=1;
-        
-        for(int i=1;i<=r;i++)
-        {
-            curr=(1LL*curr*(N-i+1))%mod;
-            curr=(1LL*curr*power(i,mod-2,mod))%mod;
+        vector<int> dp(n), prefixSums(n + 1);
+        for (int j = 0; j < n; j++) {
+            dp[j] = 1;
+            prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
         }
-
-        return curr;
+        for (int i = 1; i <= k; i++) {
+            dp[0] = 0;
+            for (int j = 1; j < n; j++) {
+                dp[j] = (dp[j - 1] + prefixSums[j]) % MOD;
+            }
+            for (int j = 0; j < n; j++) {
+                prefixSums[j + 1] = (prefixSums[j] + dp[j]) % MOD;
+            }
+        }
+        return dp[n - 1];
     }
 };
